27f0dfb8b5f7ac882f707a919c823ba567e2093c
bekacru <bekacru@gmail.com>
2024-05-21 02:52:41 +0300

    chore: cleanup git history

--- examples/nextjs/.next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/bekacru/Desktop/Development/better-auth/better-auth/examples/nextjs/src/app/layout\":[\"static/media/c9a5bc6a7c948fb0-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
--- examples/nextjs/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Ossp4SWUPfpKrkab00KsGHZNlcvxlyfxFby7whRGn1k=\"\n}"
