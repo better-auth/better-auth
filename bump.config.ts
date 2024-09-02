ef61af24d93954f5d961c411a761ab2a836731dc
Bereket Engida <bekacru@gmail.com>
2024-09-02 23:53:24 +0300

    chore: remove bump config

--- bump.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "bumpp";
-import fg from "fast-glob";
-
-export default defineConfig({
-	files: fg.sync(["./packages/*/package.json"], {
-		ignore: ["./packages/*-extension/package.json"],
-	}),
-});
