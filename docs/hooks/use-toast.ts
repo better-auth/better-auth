80259764151b94c182b755b351590c9c259aee40
Bereket Engida <bekacru@gmail.com>
2024-09-02 22:58:31 +0300

    chore: formatting

--- docs/hooks/use-toast.ts
+++ docs/hooks/use-toast.ts
@@ -95,9 +95,9 @@ export const reducer = (state: State, action: Action): State => {
 			if (toastId) {
 				addToRemoveQueue(toastId);
 			} else {
-				state.toasts.forEach((toast) => {
+				for (const toast of state.toasts) {
 					addToRemoveQueue(toast.id);
-				});
+				}
 			}
 
 			return {
@@ -132,9 +132,9 @@ let memoryState: State = { toasts: [] };
 
 function dispatch(action: Action) {
 	memoryState = reducer(memoryState, action);
-	listeners.forEach((listener) => {
+	for (const listener of listeners) {
 		listener(memoryState);
-	});
+	}
 }
 
 type Toast = Omit<ToasterToast, "id">;
