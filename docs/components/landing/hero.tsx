d3e0924b69353cb13fe95b5d1204cb6296f59574
Bereket Engida <bekacru@gmail.com>
2024-09-04 14:36:14 +0300

    docs: change hero text

--- docs/components/landing/hero.tsx
+++ docs/components/landing/hero.tsx
@@ -106,9 +106,8 @@ export default function Hero() {
 							</div>
 
 							<p className="mt-3 md:text-2xl font-mono tracking-tight dark:text-zinc-300 text-zinc-800">
-								The authentication library for modern web applications.
+								The most comprehensive authentication library for typescript.
 							</p>
-
 							<div className="mt-8 flex gap-4 font-sans md:justify-center lg:justify-start flex-col md:flex-row">
 								<button className="px-4 md:px-8 py-0.5  border-2 border-black dark:border-white uppercase bg-white text-black transition duration-200 text-sm shadow-[1px_1px_rgba(0,0,0),2px_2px_rgba(0,0,0),3px_3px_rgba(0,0,0),4px_4px_rgba(0,0,0),5px_5px_0px_0px_rgba(0,0,0)] dark:shadow-[1px_1px_rgba(255,255,255),2px_2px_rgba(255,255,255),3px_3px_rgba(255,255,255),4px_4px_rgba(255,255,255),5px_5px_0px_0px_rgba(255,255,255)] ">
 									Get Started
--- packages/better-auth/src/init.test.ts
+++ packages/better-auth/src/init.test.ts
@@ -1,6 +1,5 @@
 import { describe, expect, it } from "vitest";
 import { init } from "./init";
-import { getTestInstance } from "./test-utils/test-instance";
 
 describe("init", async () => {
 	it("should match config", () => {
