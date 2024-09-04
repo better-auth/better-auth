52ffb1104145b2a7f203a26e8be820635ad5ab2b
Bereket Engida <bekacru@gmail.com>
2024-09-04 13:49:14 +0300

    chore: bun still not working

--- dev/bun/index.ts
+++ dev/bun/index.ts
@@ -1,8 +1,15 @@
-import { betterAuth } from "better-auth";
+import { Argon2id } from "oslo/password";
+import { xchacha20poly1305 } from "@noble/ciphers/chacha";
+import { bytesToHex, hexToBytes, utf8ToBytes } from "@noble/ciphers/utils";
+import { managedNonce } from "@noble/ciphers/webcrypto";
+import { sha256 } from "@noble/hashes/sha256";
+import { auth } from "./auth";
+import { symmetricDecrypt, symmetricEncrypt } from "./ec";
+const a2Id = new Argon2id();
 
-export const auth = betterAuth({
-	database: {
-		provider: "sqlite",
-		url: "bun-auth.db",
+Bun.serve({
+	fetch(request, server) {
+		auth;
+		return new Response("Hello, World!");
 	},
 });
