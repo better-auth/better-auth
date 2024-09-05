0c296f896a130243738434f0345fabdcd90e15a5
Bereket Engida <bekacru@gmail.com>
2024-09-06 00:28:25 +0300

    fix: type errors

--- packages/better-auth/src/adapters/utils.ts
+++ packages/better-auth/src/adapters/utils.ts
@@ -8,23 +8,20 @@ export function getAdapter(options: BetterAuthOptions): Adapter {
 	if (!options.database) {
 		throw new BetterAuthError("Database configuration is required");
 	}
-	if ("provider" in options.database) {
-		const db = createKyselyAdapter(options);
-		if (!db) {
-			throw new BetterAuthError("Failed to initialize database adapter");
-		}
-		const tables = getAuthTables(options);
-		return kyselyAdapter(db, {
-			transform: {
-				schema: {
-					[tables.user.tableName]: tables.user.fields,
-					[tables.session.tableName]: tables.session.fields,
-					[tables.account.tableName]: tables.account.fields,
-				},
-				date: true,
-				boolean: getDatabaseType(options) === "sqlite",
-			},
-		});
+	const db = createKyselyAdapter(options);
+	if (!db) {
+		throw new BetterAuthError("Failed to initialize database adapter");
 	}
-	return options.database;
+	const tables = getAuthTables(options);
+	return kyselyAdapter(db, {
+		transform: {
+			schema: {
+				[tables.user.tableName]: tables.user.fields,
+				[tables.session.tableName]: tables.session.fields,
+				[tables.account.tableName]: tables.account.fields,
+			},
+			date: true,
+			boolean: getDatabaseType(options) === "sqlite",
+		},
+	});
 }
