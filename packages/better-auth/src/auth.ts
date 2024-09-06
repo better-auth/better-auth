52f4afb93ecbeae808ce4d7f38048adf8ab2f20b
Bereket Engida <bekacru@gmail.com>
2024-09-06 23:34:04 +0300

    fix: example attempt #2

--- packages/better-auth/src/auth.ts
+++ packages/better-auth/src/auth.ts
@@ -7,13 +7,19 @@ export const betterAuth = <O extends BetterAuthOptions>(options: O) => {
 	const { api } = getEndpoints(authContext, options);
 	return {
 		handler: async (request: Request) => {
+			const basePath = authContext.options.basePath;
 			if (!authContext.options.baseURL) {
 				const baseURL = `${new URL(request.url).origin}/api/auth`;
 				authContext.options.baseURL = baseURL;
 				authContext.baseURL = baseURL;
 			}
 			if (!authContext.options.baseURL) {
-				return new Response("Base URL not set", { status: 200 });
+				return new Response("Base URL not set", { status: 400 });
+			}
+
+			const url = new URL(request.url);
+			if (url.pathname === basePath || url.pathname === `${basePath}/`) {
+				return new Response("Welcome to BetterAuth", { status: 200 });
 			}
 			const { handler } = router(authContext, options);
 			return handler(request);
