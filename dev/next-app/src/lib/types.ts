f1961a079c4b3e36c8b5f956b07ad6eef36da66e
Bereket Engida <bekacru@gmail.com>
2024-09-06 23:09:34 +0300

    example: session

--- dev/next-app/src/lib/types.ts
+++
@@ -1,5 +0,0 @@
-import type { InferSession, InferUser } from "better-auth/types";
-import type { auth } from "./auth";
-
-export type User = InferUser<typeof auth>;
-export type Session = InferSession<typeof auth>;
--- examples/next-js/src/lib/auth-client.ts
+++ examples/next-js/src/lib/auth-client.ts
@@ -12,9 +12,7 @@ export const authClient = createAuthClient({
 		baseURL: "http://localhost:3000/api/auth",
 	},
 	plugins: [
-		organizationClient({
-			ac: ac,
-		}),
+		organizationClient(),
 		twoFactorClient({
 			twoFactorPage: "/two-factor",
 		}),
