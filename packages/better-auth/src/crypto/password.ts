76290855fdb469f916e8da9a092bfba323a29720
Bereket Engida <bekacru@gmail.com>
2024-09-08 00:12:34 +0300

    mv to scrypt

--- packages/better-auth/src/crypto/password.ts
+++ packages/better-auth/src/crypto/password.ts
@@ -1,13 +1,49 @@
-import { verify, hash } from "argon2";
+import { scrypt } from "node:crypto";
+import { decodeHex, encodeHex } from "oslo/encoding";
+import { constantTimeEqual } from "oslo/crypto";
+
+const config = {
+	N: 16384,
+	r: 16,
+	p: 1,
+	dkLen: 64,
+};
+
+async function generateKey(
+	password: string,
+	salt: string,
+): Promise<ArrayBuffer> {
+	return await new Promise<ArrayBuffer>((resolve, reject) => {
+		scrypt(
+			password.normalize("NFKC"),
+			salt!,
+			config.dkLen,
+			{
+				N: config.N,
+				p: config.p,
+				r: config.r,
+				// errors when 128 * N * r > `maxmem` (approximately)
+				maxmem: 128 * config.N * config.r * 2,
+			},
+			(err, buff) => {
+				if (err) return reject(err);
+				// @ts-ignore
+				return resolve(buff);
+			},
+		);
+	});
+}
 
 export function getPasswordHasher(secret: string) {
-	const hashPassword = async (password: string) =>
-		await hash(password.normalize("NFKC"), {
-			secret: Buffer.from(secret),
-		});
-	const verifyPassword = async (password: string, hash: string) =>
-		await verify(hash, password.normalize("NFKC"), {
-			secret: Buffer.from(secret),
-		});
+	const hashPassword = async (password: string) => {
+		const salt = encodeHex(crypto.getRandomValues(new Uint8Array(16)));
+		const key = await generateKey(password, salt);
+		return `${salt}:${encodeHex(key)}`;
+	};
+	const verifyPassword = async (password: string, hash: string) => {
+		const [salt, key] = hash.split(":");
+		const targetKey = await generateKey(password, salt!);
+		return constantTimeEqual(targetKey, decodeHex(key!));
+	};
 	return { hashPassword, verifyPassword };
 }
