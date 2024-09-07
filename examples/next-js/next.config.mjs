f7cb4beead319629f06d63800116f55fc23903f9
Bereket Engida <bekacru@gmail.com>
2024-09-08 00:12:47 +0300

    mv config

--- examples/next-js/next.config.mjs
+++ examples/next-js/next.config.mjs
@@ -1,9 +1,4 @@
 /** @type {import("next").NextConfig} */
-const nextConfig = {
-	webpack: (config) => {
-		config.externals.push("argon2");
-		return config;
-	}
-}
+const nextConfig = {}
 
 export default nextConfig;
