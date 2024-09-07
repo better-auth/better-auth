b147a880d3f5a7399bc4f9be46f32eb567e70073
Bereket Engida <bekacru@gmail.com>
2024-09-07 12:42:24 +0300

    fix: config

--- examples/next-js/next.config.ts
+++ examples/next-js/next.config.ts
@@ -1,9 +1,10 @@
-/** @type {import('next').NextConfig} */
+import { NextConfig } from "next";
+
 const nextConfig = {
 	webpack: (config) => {
 		config.externals.push("argon2");
 		return config;
-	}
-};
+	},
+} satisfies NextConfig;
 
 export default nextConfig;
