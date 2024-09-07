95139c6a2d21f436670c48f4264d551248d711ec
Bereket Engida <bekacru@gmail.com>
2024-09-08 00:00:32 +0300

    mv to argon2

--- examples/next-js/next.config.mjs
+++ examples/next-js/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import("next").NextConfig} */
 const nextConfig = {
 	webpack: (config) => {
-		config.externals.push("@node-rs/argon2", "@node-rs/bcrypt");
+		config.externals.push("argon2");
 		return config;
 	}
 }
