0fcf52798ecc5c305c2cf393d63045e695599d2c
Bereket Engida <bekacru@gmail.com>
2024-09-14 21:11:18 +0300

    fix: config file reader

--- packages/better-auth/src/cli/get-config.ts
+++ packages/better-auth/src/cli/get-config.ts
@@ -36,35 +36,40 @@ export async function getConfig({
 			configFile = config.auth.options;
 		}
 
-		for (const possiblePath of possiblePaths) {
-			try {
-				const config = (await jiti(path.join(cwd, possiblePath)).import(
-					path.join(cwd, possiblePath),
-					{},
-				)) as {
-					auth?: {
-						options: BetterAuthOptions;
+		if (!configFile) {
+			for (const possiblePath of possiblePaths) {
+				try {
+					const config = (await jiti(path.join(cwd, possiblePath)).import(
+						path.join(cwd, possiblePath),
+						{},
+					)) as {
+						auth?: {
+							options: BetterAuthOptions;
+						};
+						default?: {
+							options: BetterAuthOptions;
+						};
 					};
-					default?: {
-						options: BetterAuthOptions;
-					};
-				};
-				if (config) {
-					configFile = config.auth?.options || config.default?.options || null;
-					if (!configFile) {
-						logger.error("[#better-auth]: Couldn't read your auth config.");
-						logger.break();
-						logger.info(
-							"[#better-auth]: Make sure to default export your auth instance or to export as a variable named auth.",
-						);
+					if (config) {
+						configFile =
+							config.auth?.options || config.default?.options || null;
+						if (!configFile) {
+							logger.error("[#better-auth]: Couldn't read your auth config.");
+							logger.break();
+							logger.info(
+								"[#better-auth]: Make sure to default export your auth instance or to export as a variable named auth.",
+							);
+							process.exit(1);
+						}
+						break;
+					}
+				} catch (e) {
+					if (
+						!(e instanceof Error && e.message.includes("Cannot find module"))
+					) {
+						logger.error(e);
 						process.exit(1);
 					}
-					break;
-				}
-			} catch (e) {
-				if (!(e instanceof Error && e.message.includes("Cannot find module"))) {
-					logger.error(e);
-					process.exit(1);
 				}
 			}
 		}
