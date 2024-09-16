163d01b9abdc5063c74d36666c0378b8f1ce7a03
Bereket Engida <bekacru@gmail.com>
2024-09-16 22:59:18 +0300

    fix: remove uncessary files from demo

--- demo/nextjs/lib/client.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from "better-call/client";
-import { router } from "./router";
-
-export const client = createClient<typeof router>({
-	baseURL: "http://localhost:3000/api/v1",
-});
