e210e543044bd99ee15445eb93593d26bda1d399
Bereket Engida <bekacru@gmail.com>
2024-09-28 02:54:30 +0300

    fix: magic link client export

--- packages/better-auth/src/client/plugins/index.ts
+++ packages/better-auth/src/client/plugins/index.ts
@@ -3,3 +3,4 @@ export * from "../../plugins/username/client";
 export * from "../../plugins/passkey/client";
 export * from "../../plugins/two-factor/client";
 export * from "../../plugins/passkey/client";
+export * from "../../plugins/magic-link/client";
