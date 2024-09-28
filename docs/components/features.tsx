79ddba4cca6cc51c482d6e09eedfc9c956ae80bc
Kinfe Michael Tariku <65047246+Kinfe123@users.noreply.github.com>
2024-09-28 03:19:28 +0300

    fix: feature grid  (#6)

--- docs/components/features.tsx
+++ docs/components/features.tsx
@@ -19,7 +19,7 @@ import { Ripple } from "./ripple";
 
 export default function Features() {
 	return (
-		<div className="md:w-10/12 overflow-x-hidden mt-10 mx-auto font-geist relative md:border-l-0 md:border-[1.2px] rounded-none -pr-2">
+		<div className="md:w-10/12 overflow-hidden mt-10 mx-auto font-geist relative md:border-l-0 md:border-[1.2px] rounded-none -pr-2">
 			<Plus className="absolute top-[-17px] left-[-17px] text-black/20 dark:text-white/30  w-8 h-8" />
 			<div className="grid grid-cols-1 md:grid-cols-3 md:mx-0 grid-rows-4 md:grid-rows-4 w-full">
 				<div className="relative items-start justify-start border-l-[1.2px] border-t-[1.2px] md:border-t-0  transform-gpu  flex flex-col p-10 overflow-clip">
@@ -144,7 +144,6 @@ export default function Features() {
 					</div>
 				</div>
 				<div className="items-start justify-start  border-l-[1.2px] border-t-[1.2px] transform-gpu relative  flex flex-col p-10  ">
-					<Plus className="absolute bottom-[-15px] right-[-15px] text-black/20 dark:text-white/40 w-8 h-8" />
 					<div className="flex gap-2 items-center my-1">
 						<PlugIcon className="w-4 h-4" />
 						<p className="text-gray-600 dark:text-gray-400">
@@ -169,8 +168,6 @@ export default function Features() {
 					</div>
 				</div>
 				<div className="relative md:grid md:col-span-3 grid-cols-2 row-span-2 border-t-[1.2px] border-l-[1.2px]  md:border-b-[1.2px] dark:border-b-0  h-full py-20 ">
-					<Plus className="absolute top-[-15px] left-[-15px] text-black/20 dark:text-black/20  w-8 h-8 " />
-					<Plus className="absolute top-[-15px] right-[-15px] text-black/20 dark:text-black/20  w-8 h-8 " />
 					<div className="p-16 pt-10 md:px-10 h-full md:absolute top-0 left-0  w-full">
 						<div className="flex flex-col gap-3 justify-center h-full items-center w-full">
 							<div className="flex gap-2 items-center">
