a3275a17d3e44991a7cf34385bc054154364d60f
Kinfe123 <kinfishtech@gmail.com>
2024-09-29 23:49:40 +0300

    fix: lint as always

--- docs/app/page.tsx
+++ docs/app/page.tsx
@@ -2,40 +2,40 @@ import Section from "@/components/landing/section";
 import Hero from "@/components/landing/hero";
 import Features from "@/components/features";
 async function getGitHubStars() {
-  try {
-    const response = await fetch(
-      "https://api.github.com/repos/better-auth/better-auth",
-      {
-        next: {
-          revalidate: 60,
-        },
-      }
-    );
-    if (!response?.ok) {
-      return null;
-    }
-    const json = await response.json();
-    const stars = parseInt(json.stargazers_count).toLocaleString();
-    return stars;
-  } catch {
-    return null;
-  }
+	try {
+		const response = await fetch(
+			"https://api.github.com/repos/better-auth/better-auth",
+			{
+				next: {
+					revalidate: 60,
+				},
+			},
+		);
+		if (!response?.ok) {
+			return null;
+		}
+		const json = await response.json();
+		const stars = parseInt(json.stargazers_count).toLocaleString();
+		return stars;
+	} catch {
+		return null;
+	}
 }
 export default async function HomePage() {
-  const stars = await getGitHubStars();
-  return (
-    <main className="h-min mx-auto ">
-      <Section
-        className="-z-1 mb-1  overflow-y-clip"
-        crosses
-        crossesOffset="lg:translate-y-[5.25rem]"
-        customPaddings
-        id="hero"
-      >
-        <Hero />
-        <Features stars={stars} />
-        <hr className="h-px bg-gray-200" />
-      </Section>
-    </main>
-  );
+	const stars = await getGitHubStars();
+	return (
+		<main className="h-min mx-auto ">
+			<Section
+				className="-z-1 mb-1  overflow-y-clip"
+				crosses
+				crossesOffset="lg:translate-y-[5.25rem]"
+				customPaddings
+				id="hero"
+			>
+				<Hero />
+				<Features stars={stars} />
+				<hr className="h-px bg-gray-200" />
+			</Section>
+		</main>
+	);
 }
--- docs/components/features.tsx
+++ docs/components/features.tsx
@@ -1,14 +1,14 @@
 "use client";
 import {
-  CheckIcon,
-  Globe2Icon,
-  PlugIcon,
-  PlugZap2Icon,
-  Plus,
-  RabbitIcon,
-  ShieldCheckIcon,
-  Webhook,
-  XIcon,
+	CheckIcon,
+	Globe2Icon,
+	PlugIcon,
+	PlugZap2Icon,
+	Plus,
+	RabbitIcon,
+	ShieldCheckIcon,
+	Webhook,
+	XIcon,
 } from "lucide-react";
 import { GitHubLogoIcon, LockClosedIcon } from "@radix-ui/react-icons";
 import Link from "next/link";
@@ -19,196 +19,196 @@ import { Ripple } from "./ripple";
 import { GithubStat } from "./github-stat";
 
 export default function Features({ stars }: { stars: string | null }) {
-  return (
-    <div className="md:w-10/12 overflow-hidden mt-10 mx-auto font-geist relative md:border-l-0 md:border-[1.2px] rounded-none -pr-2">
-      <Plus className="absolute top-[-17px] left-[-17px] text-black/20 dark:text-white/30  w-8 h-8" />
-      <div className="grid grid-cols-1 md:grid-cols-3 md:mx-0 grid-rows-4 md:grid-rows-4 w-full">
-        <div className="relative items-start justify-start border-l-[1.2px] border-t-[1.2px] md:border-t-0  transform-gpu  flex flex-col p-10 overflow-clip">
-          <Plus className="absolute bottom-[-17px] left-[-17px] text-black/20 dark:text-white/30  w-8 h-8" />
+	return (
+		<div className="md:w-10/12 overflow-hidden mt-10 mx-auto font-geist relative md:border-l-0 md:border-[1.2px] rounded-none -pr-2">
+			<Plus className="absolute top-[-17px] left-[-17px] text-black/20 dark:text-white/30  w-8 h-8" />
+			<div className="grid grid-cols-1 md:grid-cols-3 md:mx-0 grid-rows-4 md:grid-rows-4 w-full">
+				<div className="relative items-start justify-start border-l-[1.2px] border-t-[1.2px] md:border-t-0  transform-gpu  flex flex-col p-10 overflow-clip">
+					<Plus className="absolute bottom-[-17px] left-[-17px] text-black/20 dark:text-white/30  w-8 h-8" />
 
-          <div className="flex gap-2 items-center my-1">
-            <PlugZap2Icon className="w-4 h-4" />
-            <p className="text-gray-600 dark:text-gray-400">
-              Framework Agnostic{" "}
-            </p>
-          </div>
-          <div className="mt-2">
-            <div className="max-w-full">
-              <div className="flex gap-3 ">
-                <p className="text-xl md:text-2xl tracking-tighter font-normal max-w-lg">
-                  Supports popular <strong>frameworks</strong>
-                </p>
-              </div>
-            </div>
-            <p className="text-left text-sm mt-2 text-muted-foreground">
-              Supports your favorite frontend, backend and meta frameworks,
-              including React, Vue, Svelte, Astro, Solid, Next.js, Nuxt.js,
-              Hono, and more{" "}
-              <a className="text-gray-50" href="/docs" target="_blank">
-                Learn more
-              </a>
-            </p>
-          </div>
-        </div>
-        <div className="relative items-start justify-start border-l-[1.2px] border-t-[1.2px] md:border-t-0  transform-gpu  flex flex-col p-10">
-          <Plus className="absolute bottom-[-17px] left-[-17px] text-black/20 dark:text-white/30  w-8 h-8" />
+					<div className="flex gap-2 items-center my-1">
+						<PlugZap2Icon className="w-4 h-4" />
+						<p className="text-gray-600 dark:text-gray-400">
+							Framework Agnostic{" "}
+						</p>
+					</div>
+					<div className="mt-2">
+						<div className="max-w-full">
+							<div className="flex gap-3 ">
+								<p className="text-xl md:text-2xl tracking-tighter font-normal max-w-lg">
+									Supports popular <strong>frameworks</strong>
+								</p>
+							</div>
+						</div>
+						<p className="text-left text-sm mt-2 text-muted-foreground">
+							Supports your favorite frontend, backend and meta frameworks,
+							including React, Vue, Svelte, Astro, Solid, Next.js, Nuxt.js,
+							Hono, and more{" "}
+							<a className="text-gray-50" href="/docs" target="_blank">
+								Learn more
+							</a>
+						</p>
+					</div>
+				</div>
+				<div className="relative items-start justify-start border-l-[1.2px] border-t-[1.2px] md:border-t-0  transform-gpu  flex flex-col p-10">
+					<Plus className="absolute bottom-[-17px] left-[-17px] text-black/20 dark:text-white/30  w-8 h-8" />
 
-          <div className="flex gap-2 items-center my-1">
-            <LockClosedIcon className="w-4 h-4" />
-            <p className="text-gray-600 dark:text-gray-400">Authentication</p>
-          </div>
-          <div className="mt-2">
-            <div className="max-w-full">
-              <div className="flex gap-3 ">
-                <p className="text-2xl tracking-tighter font-normal max-w-lg">
-                  Email & Password <strong>Authentication</strong>
-                </p>
-              </div>
-            </div>
-            <p className="text-left text-sm mt-2 text-muted-foreground">
-              Builtin support for email and password authentication, with secure
-              password hashing and account management features{" "}
-              <a className="text-gray-50" href="/docs" target="_blank">
-                Learn more
-              </a>
-            </p>
-          </div>
-        </div>
-        <div className="relative items-start justify-start md:border-l-[0.2px] border-t-[1.2px] md:border-t-0  flex flex-col p-10">
-          <Plus className="absolute bottom-[-17px] left-[-17px] text-black/20 dark:text-white/30  w-8 h-8" />
+					<div className="flex gap-2 items-center my-1">
+						<LockClosedIcon className="w-4 h-4" />
+						<p className="text-gray-600 dark:text-gray-400">Authentication</p>
+					</div>
+					<div className="mt-2">
+						<div className="max-w-full">
+							<div className="flex gap-3 ">
+								<p className="text-2xl tracking-tighter font-normal max-w-lg">
+									Email & Password <strong>Authentication</strong>
+								</p>
+							</div>
+						</div>
+						<p className="text-left text-sm mt-2 text-muted-foreground">
+							Builtin support for email and password authentication, with secure
+							password hashing and account management features{" "}
+							<a className="text-gray-50" href="/docs" target="_blank">
+								Learn more
+							</a>
+						</p>
+					</div>
+				</div>
+				<div className="relative items-start justify-start md:border-l-[0.2px] border-t-[1.2px] md:border-t-0  flex flex-col p-10">
+					<Plus className="absolute bottom-[-17px] left-[-17px] text-black/20 dark:text-white/30  w-8 h-8" />
 
-          <div className="flex gap-2 items-center my-1">
-            <Webhook className="w-4 h-4" />
-            <p className="text-gray-600 dark:text-gray-400">Social Sign-on</p>
-          </div>
-          <div className="mt-2">
-            <div className="max-w-full">
-              <div className="flex gap-3 ">
-                <p className="text-2xl tracking-tighter font-normal max-w-lg">
-                  Support multiple <strong>OAuth providers.</strong>
-                </p>
-              </div>
-            </div>
-            <p className="text-left text-sm mt-2 text-muted-foreground">
-              Allow users to sign in with their accounts, including Github,
-              Google, Discord, Twitter, and more.{" "}
-              <a className="text-gray-50" href="#" target="_blank">
-                Learn more
-              </a>
-            </p>
-          </div>
-        </div>
-        <div className="items-start justify-start  border-l-[1.2px] border-t-[1.2px] flex flex-col p-10  ">
-          <div className="flex gap-2 items-center my-1">
-            <ShieldCheckIcon className="w-4 h-4" />
-            <p className="text-gray-600 dark:text-gray-400">Two Factor</p>
-          </div>
-          <div className="mt-2">
-            <div className="max-w-full">
-              <div className="flex gap-3 ">
-                <p className="text-2xl tracking-tighter font-normal max-w-lg">
-                  Two Factor <strong>Authentication</strong>
-                </p>
-              </div>
-            </div>
-            <p className="text-left text-sm mt-2 text-muted-foreground">
-              With our built-in two factor authentication plugin, you can add an
-              extra layer of security to your account.{" "}
-              <Link className="text-gray-50" href="/docs" target="_blank">
-                Learn more
-              </Link>
-            </p>
-          </div>
-        </div>
-        <div className="items-start justify-staart  border-l-[1.2px] border-t-[1.2px] flex flex-col p-10  ">
-          <div className="flex gap-2 items-center my-1">
-            <RabbitIcon className="w-4 h-4" />
-            <p className="text-gray-600 dark:text-gray-400">
-              Organization & Access Control{" "}
-            </p>
-          </div>
-          <div className="mt-2">
-            <div className="max-w-full">
-              <div className="flex gap-3 ">
-                <p className="text-2xl tracking-tighter font-normal max-w-lg">
-                  Gain and manage <strong>access.</strong>
-                </p>
-              </div>
-            </div>
-            <p className="text-left text-sm mt-2 text-muted-foreground">
-              Manage users and their access to resources within your
-              application.{" "}
-              <a className="text-gray-50" href="/docs" target="_blank">
-                Learn more
-              </a>
-            </p>
-          </div>
-        </div>
-        <div className="items-start justify-start  border-l-[1.2px] border-t-[1.2px] transform-gpu relative  flex flex-col p-10  ">
-          <div className="flex gap-2 items-center my-1">
-            <PlugIcon className="w-4 h-4" />
-            <p className="text-gray-600 dark:text-gray-400">
-              Plugin Ecosystem{" "}
-            </p>
-          </div>
-          <div className="max-w-full">
-            <div className="flex gap-3 ">
-              <p className="text-2xl tracking-tighter font-normal max-w-lg">
-                Pluggable with <strong>custom.</strong>
-              </p>
-            </div>
-          </div>
-          <div className="mt-2">
-            <p className="text-left text-sm mt-2 text-muted-foreground">
-              Enhance your application with our official plugins and those
-              created by the community.{" "}
-              <a className="text-gray-50" href="/docs" target="_blank">
-                Learn more
-              </a>
-            </p>
-          </div>
-        </div>
-        <div className="relative md:grid md:col-span-3 grid-cols-2 row-span-2 border-t-[1.2px] border-l-[1.2px]  md:border-b-[1.2px] dark:border-b-0  h-full py-20 ">
-          <div className="p-16 pt-10 md:px-10 h-full md:absolute top-0 left-0  w-full">
-            <div className="flex flex-col gap-3 justify-center h-full items-center w-full">
-              <div className="flex gap-2 items-center">
-                <Globe2Icon className="w-4 h-4" />
-                <p className="text-gray-600 dark:text-gray-400">
-                  Own your auth
-                </p>
-              </div>
-              <p className="text-4xl md:text-4xl mt-4 tracking-tighter font-normal max-w-md mx-auto text-center">
-                <strong>Roll your own auth with confidence in minutes!</strong>
-              </p>
-              <div className="flex mt-[10px] z-20 justify-center items-start">
-                <TechStackDisplay
-                  skills={[
-                    "nextJs",
-                    "nuxt",
-                    "svelteKit",
-                    "astro",
-                    "solidStart",
-                    "react",
-                    "hono",
-                  ]}
-                />
-              </div>
-              <div className="flex items-center gap-2">
-                <Link href="/docs" className="z-50">
-                  <Button
-                    className="mt-4 rounded-none p-5 ml-auto"
-                    variant="secondary"
-                  >
-                    Start now
-                  </Button>
-                </Link>
-                <GithubStat stars={stars} />
-              </div>
-              <Ripple />
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+					<div className="flex gap-2 items-center my-1">
+						<Webhook className="w-4 h-4" />
+						<p className="text-gray-600 dark:text-gray-400">Social Sign-on</p>
+					</div>
+					<div className="mt-2">
+						<div className="max-w-full">
+							<div className="flex gap-3 ">
+								<p className="text-2xl tracking-tighter font-normal max-w-lg">
+									Support multiple <strong>OAuth providers.</strong>
+								</p>
+							</div>
+						</div>
+						<p className="text-left text-sm mt-2 text-muted-foreground">
+							Allow users to sign in with their accounts, including Github,
+							Google, Discord, Twitter, and more.{" "}
+							<a className="text-gray-50" href="#" target="_blank">
+								Learn more
+							</a>
+						</p>
+					</div>
+				</div>
+				<div className="items-start justify-start  border-l-[1.2px] border-t-[1.2px] flex flex-col p-10  ">
+					<div className="flex gap-2 items-center my-1">
+						<ShieldCheckIcon className="w-4 h-4" />
+						<p className="text-gray-600 dark:text-gray-400">Two Factor</p>
+					</div>
+					<div className="mt-2">
+						<div className="max-w-full">
+							<div className="flex gap-3 ">
+								<p className="text-2xl tracking-tighter font-normal max-w-lg">
+									Two Factor <strong>Authentication</strong>
+								</p>
+							</div>
+						</div>
+						<p className="text-left text-sm mt-2 text-muted-foreground">
+							With our built-in two factor authentication plugin, you can add an
+							extra layer of security to your account.{" "}
+							<Link className="text-gray-50" href="/docs" target="_blank">
+								Learn more
+							</Link>
+						</p>
+					</div>
+				</div>
+				<div className="items-start justify-staart  border-l-[1.2px] border-t-[1.2px] flex flex-col p-10  ">
+					<div className="flex gap-2 items-center my-1">
+						<RabbitIcon className="w-4 h-4" />
+						<p className="text-gray-600 dark:text-gray-400">
+							Organization & Access Control{" "}
+						</p>
+					</div>
+					<div className="mt-2">
+						<div className="max-w-full">
+							<div className="flex gap-3 ">
+								<p className="text-2xl tracking-tighter font-normal max-w-lg">
+									Gain and manage <strong>access.</strong>
+								</p>
+							</div>
+						</div>
+						<p className="text-left text-sm mt-2 text-muted-foreground">
+							Manage users and their access to resources within your
+							application.{" "}
+							<a className="text-gray-50" href="/docs" target="_blank">
+								Learn more
+							</a>
+						</p>
+					</div>
+				</div>
+				<div className="items-start justify-start  border-l-[1.2px] border-t-[1.2px] transform-gpu relative  flex flex-col p-10  ">
+					<div className="flex gap-2 items-center my-1">
+						<PlugIcon className="w-4 h-4" />
+						<p className="text-gray-600 dark:text-gray-400">
+							Plugin Ecosystem{" "}
+						</p>
+					</div>
+					<div className="max-w-full">
+						<div className="flex gap-3 ">
+							<p className="text-2xl tracking-tighter font-normal max-w-lg">
+								Pluggable with <strong>custom.</strong>
+							</p>
+						</div>
+					</div>
+					<div className="mt-2">
+						<p className="text-left text-sm mt-2 text-muted-foreground">
+							Enhance your application with our official plugins and those
+							created by the community.{" "}
+							<a className="text-gray-50" href="/docs" target="_blank">
+								Learn more
+							</a>
+						</p>
+					</div>
+				</div>
+				<div className="relative md:grid md:col-span-3 grid-cols-2 row-span-2 border-t-[1.2px] border-l-[1.2px]  md:border-b-[1.2px] dark:border-b-0  h-full py-20 ">
+					<div className="p-16 pt-10 md:px-10 h-full md:absolute top-0 left-0  w-full">
+						<div className="flex flex-col gap-3 justify-center h-full items-center w-full">
+							<div className="flex gap-2 items-center">
+								<Globe2Icon className="w-4 h-4" />
+								<p className="text-gray-600 dark:text-gray-400">
+									Own your auth
+								</p>
+							</div>
+							<p className="text-4xl md:text-4xl mt-4 tracking-tighter font-normal max-w-md mx-auto text-center">
+								<strong>Roll your own auth with confidence in minutes!</strong>
+							</p>
+							<div className="flex mt-[10px] z-20 justify-center items-start">
+								<TechStackDisplay
+									skills={[
+										"nextJs",
+										"nuxt",
+										"svelteKit",
+										"astro",
+										"solidStart",
+										"react",
+										"hono",
+									]}
+								/>
+							</div>
+							<div className="flex items-center gap-2">
+								<Link href="/docs" className="z-50">
+									<Button
+										className="mt-4 rounded-none p-5 ml-auto"
+										variant="secondary"
+									>
+										Start now
+									</Button>
+								</Link>
+								<GithubStat stars={stars} />
+							</div>
+							<Ripple />
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
