c4da64db0d7b304115b5d767959e6e3a927ce088
Kinfe123 <kinfishtech@gmail.com>
2024-09-29 23:48:38 +0300

    feat: github stat

--- docs/app/page.tsx
+++ docs/app/page.tsx
@@ -1,20 +1,41 @@
 import Section from "@/components/landing/section";
 import Hero from "@/components/landing/hero";
 import Features from "@/components/features";
-export default function HomePage() {
-	return (
-		<main className="h-min mx-auto ">
-			<Section
-				className="-z-1 mb-1  overflow-y-clip"
-				crosses
-				crossesOffset="lg:translate-y-[5.25rem]"
-				customPaddings
-				id="hero"
-			>
-				<Hero />
-				<Features />
-				<hr className="h-px bg-gray-200" />
-			</Section>
-		</main>
-	);
+async function getGitHubStars() {
+  try {
+    const response = await fetch(
+      "https://api.github.com/repos/better-auth/better-auth",
+      {
+        next: {
+          revalidate: 60,
+        },
+      }
+    );
+    if (!response?.ok) {
+      return null;
+    }
+    const json = await response.json();
+    const stars = parseInt(json.stargazers_count).toLocaleString();
+    return stars;
+  } catch {
+    return null;
+  }
+}
+export default async function HomePage() {
+  const stars = await getGitHubStars();
+  return (
+    <main className="h-min mx-auto ">
+      <Section
+        className="-z-1 mb-1  overflow-y-clip"
+        crosses
+        crossesOffset="lg:translate-y-[5.25rem]"
+        customPaddings
+        id="hero"
+      >
+        <Hero />
+        <Features stars={stars} />
+        <hr className="h-px bg-gray-200" />
+      </Section>
+    </main>
+  );
 }
