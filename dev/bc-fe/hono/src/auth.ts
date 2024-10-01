9249c2806aec2bf28955bb23b4025f76dfb4aa1f
Bereket Engida <bekacru@gmail.com>
2024-10-01 17:11:57 +0300

    doc: fix issues

--- dev/bc-fe/hono/src/auth.ts
+++ dev/bc-fe/hono/src/auth.ts
@@ -1,18 +1,17 @@
 import Database from "better-sqlite3";
 import { betterAuth } from "better-auth";
 import { organization, twoFactor, username } from "better-auth/plugins";
-import { github } from "better-auth/social-providers";
 
 export const auth = betterAuth({
 	baseURL: "http://localhost:3000",
 	basePath: "/auth",
 	database: new Database("./db.sqlite"),
-	socialProviders: [
-		github({
-			clientId: process.env.GITHUB_CLIENT_ID as string,
-			clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
-		}),
-	],
+	socialProviders: {
+		github: {
+			clientId: process.env.GITHUB_CLIENT_ID || "",
+			clientSecret: process.env.GITHUB_CLIENT_SECRET || "",
+		},
+	},
 	plugins: [
 		twoFactor({
 			issuer: "BetterAuth",
