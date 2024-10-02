8f2c7e3536461dc951d576d9792bfbc7d75077c4
Bereket Engida <bekacru@gmail.com>
2024-10-02 21:18:17 +0300

    chore: fix test

--- packages/better-auth/src/adapters/prisma-adapter/test/adapter.prisma.test.ts
+++ packages/better-auth/src/adapters/prisma-adapter/test/adapter.prisma.test.ts
@@ -5,6 +5,7 @@ import { runAdapterTest } from "../../test";
 import { twoFactor } from "../../../plugins";
 import path from "path";
 import Database from "better-sqlite3";
+import { generatePrismaSchema } from "../generate-cli";
 
 const db = new PrismaClient();
 describe("adapter test", async () => {
@@ -16,16 +17,22 @@ describe("adapter test", async () => {
 	});
 	const database = new Database(path.join(__dirname, "test.db"));
 	it("should create schema", async () => {
-		const res = await adapter.createSchema!({
-			database,
+		const res = await generatePrismaSchema({
+			provider: "sqlite",
+			options: {
+				database,
+			},
 		});
 		expect(res.code).toMatchSnapshot("__snapshots__/adapter.prisma");
 	});
 
 	it("should work with plugins", async () => {
-		const res = await adapter.createSchema!({
-			database,
-			plugins: [twoFactor()],
+		const res = await generatePrismaSchema({
+			provider: "sqlite",
+			options: {
+				database,
+				plugins: [twoFactor()],
+			},
 		});
 		expect(res.code).toMatchSnapshot(
 			"__snapshots__/adapter-with-plugins.prisma",
