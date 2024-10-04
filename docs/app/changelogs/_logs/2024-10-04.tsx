6473f468c0da9a1c2a474e7655992f12dba0fb0b
Bereket Engida <bekacru@gmail.com>
2024-10-04 21:22:43 +0300

    chore: changelog

---
+++ docs/app/changelogs/_logs/2024-10-04.tsx
@@ -0,0 +1,54 @@
+import { Badge } from "@/components/ui/badge";
+import { AnimatePresence } from "@/components/ui/fade-in";
+import Link from "next/link";
+
+const listOfFeatures = [
+	"Database Schema Generation",
+	"Better type Inference",
+	"Custom table/model names",
+	"Phone Number Plugin",
+	"Removed flat db config",
+	"new core table for verification use cases",
+	"moved passkey challenge keys to be stored on db ",
+	"and many bug fixes and improvements",
+];
+
+const ChangelogThree = () => {
+	return (
+		<AnimatePresence>
+			<div className="flex flex-col gap-4 items-start justify-center max-w-full md:max-w-2xl">
+				<div className="flex flex-col gap-2">
+					<h2 className="text-2xl font-bold tracking-tighter">v0.3.0</h2>
+					<p>
+						Database Schema Generation, Better type Inference, Custom table
+						names, Phone Number Plugin and many more.
+					</p>
+				</div>
+				<p className="text-gray-600 dark:text-gray-300 text-[0.855rem]"></p>
+				<div className="flex flex-col gap-2">
+					<h4 className="text-xl tracking-tighter">Features & Changes</h4>
+				</div>
+				<ul className="list-disc ml-10 text-[0.855rem] text-gray-600 dark:text-gray-300">
+					{listOfFeatures.map((change, i) => (
+						<li key={i}>{change}</li>
+					))}
+				</ul>
+				<p>
+					see{" "}
+					<Link
+						href="https://github.com/better-auth/better-auth/releases/tag/v0.3.0"
+						className="underline"
+					>
+						release note{" "}
+					</Link>
+					for full list of changes.
+				</p>
+				<Badge variant="secondary">
+					🚧 We still in beta and we don't recommend to use better-auth in
+					production just yet.
+				</Badge>
+			</div>
+		</AnimatePresence>
+	);
+};
+export default ChangelogThree;
