0176a25186de9edcd71f3396832d268aa94da63d
Bereket Engida <bekacru@gmail.com>
2024-10-04 11:08:33 +0300

    chore:lint

--- examples/remix-example/vite.config.ts
+++ examples/remix-example/vite.config.ts
@@ -3,14 +3,14 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [
-    remix({
-      future: {
-        v3_fetcherPersist: true,
-        v3_relativeSplatPath: true,
-        v3_throwAbortReason: true,
-      },
-    }),
-    tsconfigPaths(),
-  ],
+	plugins: [
+		remix({
+			future: {
+				v3_fetcherPersist: true,
+				v3_relativeSplatPath: true,
+				v3_throwAbortReason: true,
+			},
+		}),
+		tsconfigPaths(),
+	],
 });
