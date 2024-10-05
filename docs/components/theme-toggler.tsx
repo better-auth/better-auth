a0e94b328f96db0d767ac64a421cb0e3219d970f
Bereket Engida <bekacru@gmail.com>
2024-10-05 16:20:14 +0300

    chore: lint

--- docs/components/theme-toggler.tsx
+++ docs/components/theme-toggler.tsx
@@ -5,74 +5,74 @@ import { useTheme } from "next-themes";
 import * as React from "react";
 import { Button } from "@/components/ui/button";
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 
 export function ThemeToggle() {
-  const { setTheme } = useTheme();
+	const { setTheme } = useTheme();
 
-  return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button
-          variant="ghost"
-          size="icon"
-          className="border-l  ring-0 rounded-none h-14 w-14 hidden md:flex shrink-0"
-        >
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem
-          data-umami-event="theme-toggle-light"
-          onClick={() => setTheme("light")}
-        >
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem
-          data-umami-event="theme-toggle-dark"
-          onClick={() => setTheme("dark")}
-        >
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem
-          data-umami-event="theme-toggle-system"
-          onClick={() => setTheme("system")}
-        >
-          System
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
-  );
+	return (
+		<DropdownMenu>
+			<DropdownMenuTrigger asChild>
+				<Button
+					variant="ghost"
+					size="icon"
+					className="border-l  ring-0 rounded-none h-14 w-14 hidden md:flex shrink-0"
+				>
+					<Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+					<Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+					<span className="sr-only">Toggle theme</span>
+				</Button>
+			</DropdownMenuTrigger>
+			<DropdownMenuContent align="end">
+				<DropdownMenuItem
+					data-umami-event="theme-toggle-light"
+					onClick={() => setTheme("light")}
+				>
+					Light
+				</DropdownMenuItem>
+				<DropdownMenuItem
+					data-umami-event="theme-toggle-dark"
+					onClick={() => setTheme("dark")}
+				>
+					Dark
+				</DropdownMenuItem>
+				<DropdownMenuItem
+					data-umami-event="theme-toggle-system"
+					onClick={() => setTheme("system")}
+				>
+					System
+				</DropdownMenuItem>
+			</DropdownMenuContent>
+		</DropdownMenu>
+	);
 }
 export function MobileThemeToggle() {
-  const { theme, setTheme } = useTheme();
-  return (
-    <div className="">
-      {theme === "dark" ? (
-        <Button
-          onClick={() => {
-            setTheme("light");
-          }}
-          variant={"ghost"}
-        >
-          <Sun className="w-4 h-4" />
-        </Button>
-      ) : (
-        <Button
-          onClick={() => {
-            setTheme("dark");
-          }}
-          variant={"ghost"}
-        >
-          <MoonIcon className="w-4 h-4" />
-        </Button>
-      )}
-    </div>
-  );
+	const { theme, setTheme } = useTheme();
+	return (
+		<div className="">
+			{theme === "dark" ? (
+				<Button
+					onClick={() => {
+						setTheme("light");
+					}}
+					variant={"ghost"}
+				>
+					<Sun className="w-4 h-4" />
+				</Button>
+			) : (
+				<Button
+					onClick={() => {
+						setTheme("dark");
+					}}
+					variant={"ghost"}
+				>
+					<MoonIcon className="w-4 h-4" />
+				</Button>
+			)}
+		</div>
+	);
 }
