8f580fdbfe9a1c855da8c8ff2cb260c998708578
Bereket Engida <bekacru@gmail.com>
2024-10-05 07:50:49 +0300

    fix: bearer plugin not setting cookie on some endpoints

--- packages/better-auth/src/plugins/bearer/bearer.test.ts
+++ packages/better-auth/src/plugins/bearer/bearer.test.ts
@@ -3,27 +3,33 @@ import { bearer } from ".";
 import { getTestInstance } from "../../test-utils/test-instance";
 
 describe("bearer", async () => {
-	const { client, testUser } = await getTestInstance({
+	const { client, signInWithTestUser } = await getTestInstance({
 		plugins: [bearer()],
 	});
+
+	let token: string;
 	it("should get session", async () => {
-		const res = await client.$fetch<{ session: { id: string } }>(
-			"/sign-in/credential",
-			{
-				method: "POST",
-				body: {
-					email: "test@test.com",
-					password: "test123456",
-				},
-			},
-		);
+		const { res } = await signInWithTestUser();
+		token = res.data?.session.id || "";
+
 		const session = await client.session({
 			fetchOptions: {
 				headers: {
-					authorization: `Bearer ${res.data?.session.id}`,
+					authorization: `Bearer ${token}`,
 				},
 			},
 		});
 		expect(session.data?.session.id).toBe(res.data?.session.id);
+	});
+
+	it("should list session", async () => {
+		const sessions = await client.user.listSessions({
+			fetchOptions: {
+				headers: {
+					authorization: `Bearer ${token}`,
+				},
+			},
+		});
+		expect(sessions.data).toHaveLength(2);
 	});
 });
