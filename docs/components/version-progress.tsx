777562c652815d45a4e1db697996f0b85233c223
Kinfe123 <kinfishtech@gmail.com>
2024-10-09 23:16:54 +0300

    feat: added roadmap link

--- docs/components/version-progress.tsx
+++ docs/components/version-progress.tsx
@@ -1,6 +1,7 @@
 import { release } from "os";
 import { Progress } from "./ui/progress";
 import { motion } from "framer-motion";
+import Link from "next/link";
 
 async function getNpmVersion() {
   try {
@@ -38,12 +39,17 @@ export const VersionProgress = async () => {
   const minorPatch = parseInt(version.slice(1, 3).join(""));
   const currentPercent = (minorPatch / 100) * 100;
   return (
-    <div className="inline-flex border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground rounded-none h-10 p-5 ml-auto z-50 overflow-hidden items-center text-sm font-medium focus-visible:outline-none  disabled:pointer-events-none disabled:opacity-50 bg-transprent dark:text-white text-black px-4 py-2 max-w-[14rem] whitespace-pre md:flex group relative w-full justify-center gap-2 transition-all duration-300 ease-out  hover:ring-black">
-      <div className="flex w-full gap-2 items-center">
-        <p style={{ fontFamily: "monospace" }}>v{majorVersion}</p>
-        <Progress value={currentPercent} />
-        <p style={{ fontFamily: "monospace" }}>{currentPercent}%</p>
+    <Link
+      href="https://github.com/orgs/better-auth/projects/2"
+      className="w-full"
+    >
+      <div className="inline-flex bg-background ring-black border border-input shadow-sm hover:border hover:border-input hover:text-accent-foreground rounded-none h-10 p-5 ml-auto z-50 overflow-hidden items-center text-sm font-medium focus-visible:outline-none  disabled:pointer-events-none disabled:opacity-50 bg-transprent dark:text-white text-black px-4 py-2 max-w-[14rem] whitespace-pre md:flex group relative w-full justify-center gap-2 transition-all duration-300 ease-out hover:ring-black">
+        <div className="flex w-full gap-2 items-center">
+          <p style={{ fontFamily: "monospace" }}>v{majorVersion}.0</p>
+          <Progress value={currentPercent} />
+          <p style={{ fontFamily: "monospace" }}>{currentPercent}%</p>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
