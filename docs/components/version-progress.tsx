4828f1465090dd3d700ad2873e3c6f1ce52a1846
Kinfe123 <kinfishtech@gmail.com>
2024-10-10 12:42:55 +0300

    chore: move to docs

--- docs/components/version-progress.tsx
+++ docs/components/version-progress.tsx
@@ -1,55 +1,41 @@
-import { release } from "os";
+"use client";
+
 import { Progress } from "./ui/progress";
 import { motion } from "framer-motion";
 import Link from "next/link";
+import { cn } from "@/lib/utils";
 
-async function getNpmVersion() {
-	try {
-		const response = await fetch("https://registry.npmjs.org/better-auth", {
-			next: {
-				revalidate: 60,
-			},
-		});
-		if (!response?.ok) {
-			return null;
-		}
-		const json = await response.json();
-		console.log({ json });
-		const versions = json.versions as Record<string, string>[];
-		const versionToKey = Object.keys(versions);
-		const latestVersion = versionToKey[versionToKey.length - 1];
-		const releases = latestVersion.split(".").slice(0, 3);
-		const betaCandidate = releases[releases.length - 1];
-		releases[releases.length - 1] = betaCandidate.includes("beta")
-			? betaCandidate.split("-")[0]
-			: betaCandidate;
-
-		return releases;
-	} catch {
-		return null;
-	}
-}
-export const VersionProgress = async () => {
-	const version = await getNpmVersion();
-	if (!version?.length) {
-		return <></>;
-	}
-
-	const majorVersion = version[0];
-	const minorPatch = parseInt(version.slice(1, 3).join(""));
-	const currentPercent = (minorPatch / 100) * 100;
-	return (
-		<Link
-			href="https://github.com/orgs/better-auth/projects/2"
-			className="w-full"
-		>
-			<div className="inline-flex bg-background ring-black border border-input shadow-sm hover:border hover:border-input hover:text-accent-foreground rounded-none h-10 p-5 ml-auto z-50 overflow-hidden text-sm font-medium focus-visible:outline-none  disabled:pointer-events-none disabled:opacity-50 bg-transprent dark:text-white text-black px-4 py-2 max-w-[14rem] whitespace-pre md:flex group relative w-full justify-center items-center gap-2 transition-all duration-300 ease-out hover:ring-black">
-				<div className="flex w-full gap-2 items-center">
-					<p style={{ fontFamily: "monospace" }}>v{majorVersion}.0</p>
-					<Progress value={currentPercent} />
-					<p style={{ fontFamily: "monospace" }}>{currentPercent}%</p>
-				</div>
-			</div>
-		</Link>
-	);
+export const VersionProgress = ({ releases }: { releases: string[] }) => {
+  const majorVersion = releases[0];
+  const minorPatch = parseInt(releases.slice(1, 3).join(""));
+  const currentPercent = (minorPatch / 100) * 100;
+  return (
+    <Link
+      href="https://github.com/orgs/better-auth/projects/2"
+      className="w-full"
+    >
+      <div className="inline-flex bg-background ring-black border border-input shadow-sm hover:border hover:border-input hover:text-accent-foreground rounded-none h-10 p-5 ml-auto z-50 overflow-hidden text-sm font-medium focus-visible:outline-none  disabled:pointer-events-none disabled:opacity-50 bg-transprent dark:text-white text-black px-4 py-2 max-w-full whitespace-pre md:flex group relative w-full justify-center items-center gap-2 transition-all duration-300 ease-out hover:ring-black">
+        <div className="flex w-full gap-2 items-center">
+          <p style={{ fontFamily: "monospace" }}>v{majorVersion}.0</p>
+          <div className="flex gap-2 items-center">
+            {Array.from({ length: 10 }).map((el, indx) => {
+              return <Bars current={currentPercent} indx={indx} />;
+            })}
+          </div>
+          {/* <Progress value={currentPercent} /> */}
+          <p style={{ fontFamily: "monospace" }}>{currentPercent}%</p>
+        </div>
+      </div>
+    </Link>
+  );
+};
+export const Bars = ({ current, indx }: { current: number; indx: number }) => {
+  const barCount = Math.floor(current / 10);
+  const filled = barCount >= indx + 1;
+  const gradientFill = filled
+    ? "bg-gradient-to-tr from-green-800 via-red-300 to-red-400"
+    : "bg-white";
+  return (
+    <div className={cn(gradientFill, "rounded-md w-[3px] h-[15px]")}></div>
+  );
 };
