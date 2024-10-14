43778fe873efc1cf0f712bfac86bf4553d59817f
Bereket Engida <bekacru@gmail.com>
2024-10-14 14:50:37 +0300

    test: add sign-out test

---
+++ packages/better-auth/src/api/routes/sign-out.test.ts
@@ -0,0 +1,19 @@
+import { describe, expect } from "vitest";
+import { getTestInstance } from "../../test-utils/test-instance";
+
+describe("sign-out", async (it) => {
+	const { signInWithTestUser, client } = await getTestInstance();
+
+	it("should sign out", async () => {
+		const { headers } = await signInWithTestUser();
+
+		const res = await client.signOut({
+			fetchOptions: {
+				headers,
+			},
+		});
+		expect(res.data).toMatchObject({
+			success: true,
+		});
+	});
+});
--- packages/better-auth/src/api/routes/sign-out.ts
+++ packages/better-auth/src/api/routes/sign-out.ts
@@ -8,26 +8,8 @@ export const signOut = createAuthEndpoint(
 	"/sign-out",
 	{
 		method: "POST",
-		body: z.optional(
-			z.object({
-				callbackURL: z.string().optional(),
-			}),
-		),
-		use: [redirectURLMiddleware],
 	},
 	async (ctx) => {
-		const callbackOrigin = ctx.body?.callbackURL
-			? new URL(ctx.body.callbackURL).origin
-			: null;
-		const trustedOrigins = [
-			ctx.context.baseURL,
-			...(ctx.context.options.trustedOrigins || []),
-		];
-		if (callbackOrigin && !trustedOrigins.includes(callbackOrigin)) {
-			throw new APIError("UNAUTHORIZED", {
-				message: "Invalid callback URL",
-			});
-		}
 		const sessionCookieToken = await ctx.getSignedCookie(
 			ctx.context.authCookies.sessionToken.name,
 			ctx.context.secret,
@@ -39,11 +21,8 @@ export const signOut = createAuthEndpoint(
 		}
 		await ctx.context.internalAdapter.deleteSession(sessionCookieToken);
 		deleteSessionCookie(ctx);
-		return ctx.json(null, {
-			body: {
-				redirect: !!ctx.body?.callbackURL,
-				url: ctx.body?.callbackURL,
-			},
+		return ctx.json({
+			success: true,
 		});
 	},
 );
