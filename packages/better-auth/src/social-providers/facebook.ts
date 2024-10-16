d551ae2db669db5919bfe60c05c66d51356e7a2c
Bereket Engida <bekacru@gmail.com>
2024-10-16 07:38:22 +0300

    fix: remove codeverifier

--- packages/better-auth/src/social-providers/facebook.ts
+++ packages/better-auth/src/social-providers/facebook.ts
@@ -25,30 +25,28 @@ export const facebook = (options: FacebookOptions) => {
 	return {
 		id: "facebook",
 		name: "Facebook",
-		async createAuthorizationURL({ state, scopes, codeVerifier }) {
+		async createAuthorizationURL({ state, scopes }) {
 			const _scopes = options.scope || scopes || ["email", "public_profile"];
 			return await createAuthorizationURL({
 				id: "facebook",
 				options,
-				authorizationEndpoint: "https://www.facebook.com/v16.0/dialog/oauth",
+				authorizationEndpoint: "https://www.facebook.com/v21.0/dialog/oauth",
 				scopes: _scopes,
 				state,
-				codeVerifier,
 			});
 		},
-		validateAuthorizationCode: async (code, codeVerifier, redirectURI) => {
+		validateAuthorizationCode: async (code, _, redirectURI) => {
 			return validateAuthorizationCode({
 				code,
-				codeVerifier,
 				redirectURI:
 					redirectURI || getRedirectURI("facebook", options.redirectURI),
 				options,
-				tokenEndpoint: "https://graph.facebook.com/v16.0/oauth/access_token",
+				tokenEndpoint: "https://graph.facebook.com/oauth/access_token",
 			});
 		},
 		async getUserInfo(token) {
 			const { data: profile, error } = await betterFetch<FacebookProfile>(
-				"https://graph.facebook.com/me",
+				"https://graph.facebook.com/me?fields=id,name,email,picture",
 				{
 					auth: {
 						type: "Bearer",
