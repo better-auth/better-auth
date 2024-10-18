1c24f9f2168b26e5bbdfb5c5ad496716d5b61e74
Bereket Engida <bekacru@gmail.com>
2024-10-19 01:21:12 +0300

    fix: douple check on csrf tokem

--- packages/better-auth/src/api/middlewares/csrf.ts
+++ packages/better-auth/src/api/middlewares/csrf.ts
@@ -39,7 +39,7 @@ export const csrfMiddleware = createAuthMiddleware(
 			ctx.context.secret,
 		);
 		const [token, hash] = csrfCookie?.split("!") || [null, null];
-		if (!csrfToken || !csrfCookie || !token || !hash || token !== csrfToken) {
+		if (!csrfToken || !token || !hash || token !== csrfToken) {
 			ctx.setCookie(ctx.context.authCookies.csrfToken.name, "", {
 				maxAge: 0,
 			});
