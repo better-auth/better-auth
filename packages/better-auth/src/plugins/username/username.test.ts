51502c8828b8fa0bcdb1405ee8805151626a592b
Bereket Engida <bekacru@gmail.com>
2024-10-21 09:23:23 +0300

    fix: test

--- packages/better-auth/src/plugins/username/username.test.ts
+++ packages/better-auth/src/plugins/username/username.test.ts
@@ -38,7 +38,7 @@ describe("username", async (it) => {
 		expect(res.data?.session).toBeDefined();
 	});
 	it("should update username", async () => {
-		const res = await client.updateUsername({
+		const res = await client.user.update({
 			username: "new-username-2",
 			fetchOptions: {
 				headers,
