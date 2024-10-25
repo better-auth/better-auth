68eaf7e47a1728fc14a718cb24ca21b5b90acbaf
Bereket Engida <86073083+Bekacru@users.noreply.github.com>
2024-10-25 21:54:21 +0300

    docs: move to fumadocs 14 (#338)

--- docs/components/sidebar-content.tsx
+++ docs/components/sidebar-content.tsx
@@ -559,8 +559,8 @@ export const contents: Content[] = [
 				<path
 					fill="currentColor"
 					stroke="currentColor"
-					stroke-linejoin="round"
-					stroke-width="4"
+					strokeLinejoin="round"
+					strokeWidth="4"
 					d="M18 6H8a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V8a2 2 0 0 0-2-2Zm0 22H8a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V30a2 2 0 0 0-2-2ZM40 6H30a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V8a2 2 0 0 0-2-2Zm0 22H30a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V30a2 2 0 0 0-2-2Z"
 				></path>
 			</svg>
@@ -709,8 +709,8 @@ export const contents: Content[] = [
 							fill="none"
 							stroke="currentColor"
 							stroke-linecap="round"
-							stroke-linejoin="round"
-							stroke-width="2"
+							strokeLinejoin="round"
+							strokeWidth="2"
 						>
 							<path d="M2 12a10 10 0 1 0 20 0a10 10 0 1 0-20 0"></path>
 							<path d="M12.556 6c.65 0 1.235.373 1.508.947l2.839 7.848a1.646 1.646 0 0 1-1.01 2.108a1.673 1.673 0 0 1-2.068-.851L13.365 15h-2.73l-.398.905A1.67 1.67 0 0 1 8.26 16.95l-.153-.047a1.647 1.647 0 0 1-1.056-1.956l2.824-7.852a1.66 1.66 0 0 1 1.409-1.087z"></path>
