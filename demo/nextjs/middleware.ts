ce1cfcba4c00d90ed9fcacd78d6f7c5c37c6b4b4
Bereket Engida <bekacru@gmail.com>
2024-10-26 13:34:20 +0300

    chore:lint

--- demo/nextjs/middleware.ts
+++ demo/nextjs/middleware.ts
@@ -2,19 +2,19 @@ import { client } from "@/lib/auth-client";
 import { NextRequest, NextResponse } from "next/server";
 
 export default async function authMiddleware(request: NextRequest) {
-    const { data: session } = await client.getSession({
-        fetchOptions: {
-            headers: {
-                //get the cookie from the request
-                cookie: request.headers.get("cookie") || "",
-            },
-        },
-    });
+	const { data: session } = await client.getSession({
+		fetchOptions: {
+			headers: {
+				//get the cookie from the request
+				cookie: request.headers.get("cookie") || "",
+			},
+		},
+	});
 
-    if (!session) {
-        return NextResponse.redirect(new URL("/", request.url));
-    }
-    return NextResponse.next();
+	if (!session) {
+		return NextResponse.redirect(new URL("/", request.url));
+	}
+	return NextResponse.next();
 }
 
 export const config = {
