44df136c0f3ed50f2349032eced347e49a2d8713
Bereket Engida <bekacru@gmail.com>
2024-10-26 13:11:07 +0300

    fix: remove next auth middelware and provide a guide instead

--- demo/nextjs/middleware.ts
+++ demo/nextjs/middleware.ts
@@ -1,17 +1,22 @@
-import { authMiddleware } from "better-auth/next-js";
-import { NextResponse } from "next/server";
-import { auth } from "@/lib/auth";
+import { client } from "@/lib/auth-client";
+import { NextRequest, NextResponse } from "next/server";
 
-export default authMiddleware({
-	customRedirect: async (session, request) => {
-		const baseURL = request.nextUrl.origin;
-		if (request.nextUrl.pathname === "/dashboard" && !session) {
-			return NextResponse.redirect(new URL("/sign-in", baseURL));
-		}
-		return NextResponse.next();
-	},
-});
+export default async function authMiddleware(request: NextRequest) {
+    const { data: session } = await client.getSession({
+        fetchOptions: {
+            headers: {
+                //get the cookie from the request
+                cookie: request.headers.get("cookie") || "",
+            },
+        },
+    });
+
+    if (!session) {
+        return NextResponse.redirect(new URL("/", request.url));
+    }
+    return NextResponse.next();
+}
 
 export const config = {
-	matcher: ["/dashboard", "/sign-in"],
+	matcher: ["/dashboard"],
 };
