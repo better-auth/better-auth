73b913417326adf3ed19b526f4cef42672bfe325
Bereket Engida <bekacru@gmail.com>
2024-10-26 17:29:01 +0300

    fix: attach error on failed to create user

--- packages/better-auth/src/api/routes/sign-up.ts
+++ packages/better-auth/src/api/routes/sign-up.ts
@@ -14,6 +14,7 @@ import type { toZod } from "../../types/to-zod";
 import { parseAdditionalUserInput } from "../../db/schema";
 import { getDate } from "../../utils/date";
 import { redirectURLMiddleware } from "../middlewares/redirect";
+import { logger } from "../../utils";
 
 export const signUpEmail = <O extends BetterAuthOptions>() =>
 	createAuthEndpoint(
@@ -98,6 +99,7 @@ export const signUpEmail = <O extends BetterAuthOptions>() =>
 					});
 				}
 			} catch (e) {
+				logger.error("Failed to create user", e);
 				throw new APIError("UNPROCESSABLE_ENTITY", {
 					message: "Failed to create user",
 					details: e,
