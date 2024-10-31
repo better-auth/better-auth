2a2fb415882f48a615c8d0bf2e63d4c68d37bc25
Bereket Engida <bekacru@gmail.com>
2024-10-31 09:47:02 +0300

    chore:lint

--- packages/better-auth/src/client/query.ts
+++ packages/better-auth/src/client/query.ts
@@ -40,7 +40,7 @@ export const useAuthQuery = <T>(
 						isPending: value.get().isPending,
 					})
 				: options;
-				
+
 		return $fetch<T>(path, {
 			...opts,
 			onSuccess: async (context) => {
