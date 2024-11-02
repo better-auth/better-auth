56ea0f391eabe0543b4dd4e3b2e3c5b1b6e1d24b
Bereket Engida <bekacru@gmail.com>
2024-11-03 01:51:09 +0300

    chore: cleanup

--- packages/better-auth/src/adapters/index.ts
+++ packages/better-auth/src/adapters/index.ts
@@ -1,6 +1,5 @@
 export * from "./prisma-adapter";
 export * from "./drizzle-adapter";
 export * from "./mongodb-adapter";
-export * from "./surrealdb-adapter";
 export * from "./kysely-adapter";
 export * from "./test";
