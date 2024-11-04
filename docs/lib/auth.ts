49810b6489d184a77c0c227100344a1606580401
Bereket Engida <bekacru@gmail.com>
2024-11-04 09:51:38 +0300

    docs: remove unused import

--- docs/lib/auth.ts
+++
@@ -1,8 +0,0 @@
-import { betterAuth } from "better-auth";
-import Database from "better-sqlite3";
-
-export const auth = betterAuth({
-	database: new Database("database.db"),
-});
-
-export type Session = typeof auth.$Infer.Session;
