a1f202ee632460eae0344b1170626629a811a8a2
Bereket Engida <bekacru@gmail.com>
2024-11-05 12:36:51 +0300

    chore: correct env.example

--- demo/nextjs/lib/auth.ts
+++ demo/nextjs/lib/auth.ts
@@ -64,7 +64,7 @@ export const auth = betterAuth({
 			clientSecret: process.env.GITHUB_CLIENT_SECRET || "",
 		},
 		google: {
-			clientId: process.env.GOOGLE_CLIENT_ID || "",
+			clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || "",
 			clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
 		},
 		discord: {
