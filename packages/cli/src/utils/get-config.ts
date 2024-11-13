70c8eecbf64a714db2b15219f11e8c5983afc0e5
Thorben <38816229+tklein1801@users.noreply.github.com>
2024-11-13 22:37:48 +0100

    fix: Use correct log-level

--- packages/cli/src/utils/get-config.ts
+++ packages/cli/src/utils/get-config.ts
@@ -139,7 +139,7 @@ export async function getConfig({
 						configFile =
 							config.auth?.options || config.default?.options || null;
 						if (!configFile) {
-							logger.info("[#better-auth]: Couldn't read your auth config.");
+							logger.error("[#better-auth]: Couldn't read your auth config.");
 							console.log("");
 							logger.info(
 								"[#better-auth]: Make sure to default export your auth instance or to export as a variable named auth.",
