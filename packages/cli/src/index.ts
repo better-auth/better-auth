dbf7c301e468077e24a200fc41bfcb5c9d785eba
Bereket Engida <bekacru@gmail.com>
2024-11-25 23:27:49 +0300

    feat(cli): add secret generator command for the cli

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -4,12 +4,14 @@ import { Command } from "commander";
 import { migrate } from "./commands/migrate";
 import { generate } from "./commands/generate";
 import "dotenv/config";
+import { generateSecret } from "./commands/secert";
 
 async function main() {
 	const program = new Command("better-auth");
 	program
 		.addCommand(migrate)
 		.addCommand(generate)
+		.addCommand(generateSecret)
 		.version("0.0.1")
 		.description("Better Auth CLI");
 	program.parse();
