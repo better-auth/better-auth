78b5082747bba8c5d8da1a1bdf4989602ab322fe
Bereket Engida <bekacru@gmail.com>
2024-12-01 16:32:12 +0300

    feat: move twofa otp to use random string instead of totp code

--- packages/better-auth/src/plugins/two-factor/otp/index.ts
+++ packages/better-auth/src/plugins/two-factor/otp/index.ts
@@ -1,5 +1,4 @@
 import { APIError } from "better-call";
-import { TOTPController } from "oslo/otp";
 import { z } from "zod";
 import { createAuthEndpoint } from "../../../api/call";
 import { verifyTwoFactorMiddleware } from "../verify-middleware";
@@ -9,6 +8,7 @@ import type {
 	UserWithTwoFactor,
 } from "../types";
 import { TimeSpan } from "oslo";
+import { alphabet, generateRandomString } from "../../../crypto";
 
 export interface OTPOptions {
 	/**
@@ -18,14 +18,12 @@ export interface OTPOptions {
 	 * @default "3 mins"
 	 */
 	period?: number;
-
 	/**
 	 * Number of digits for the OTP code
 	 *
 	 * @default 6
 	 */
-	digits?: 6 | 8;
-
+	digits?: number;
 	/**
 	 * Send the otp to the user
 	 *
@@ -60,10 +58,6 @@ export const otp2fa = (options: OTPOptions, twoFactorTable: string) => {
 		digits: options?.digits || 6,
 		period: new TimeSpan(options?.period || 3, "m"),
 	};
-	const totp = new TOTPController({
-		digits: opts.digits,
-		period: opts.period,
-	});
 	/**
 	 * Generate OTP and send it to the user.
 	 */
@@ -120,7 +114,12 @@ export const otp2fa = (options: OTPOptions, twoFactorTable: string) => {
 					message: "OTP isn't enabled",
 				});
 			}
-			const code = await totp.generate(Buffer.from(twoFactor.secret));
+			const code = generateRandomString(opts.digits, alphabet("0-9"));
+			await ctx.context.internalAdapter.createVerificationValue({
+				value: code,
+				identifier: `2fa-otp-${user.id}`,
+				expiresAt: new Date(Date.now() + opts.period.milliseconds()),
+			});
 			await options.sendOTP({ user, otp: code }, ctx.request);
 			return ctx.json({ status: true });
 		},
@@ -181,8 +180,16 @@ export const otp2fa = (options: OTPOptions, twoFactorTable: string) => {
 					message: "OTP isn't enabled",
 				});
 			}
-			const toCheckOtp = await totp.generate(Buffer.from(twoFactor.secret));
-			if (toCheckOtp === ctx.body.code) {
+			const toCheckOtp =
+				await ctx.context.internalAdapter.findVerificationValue(
+					`2fa-otp-${user.id}`,
+				);
+			if (!toCheckOtp || toCheckOtp.expiresAt < new Date()) {
+				throw new APIError("BAD_REQUEST", {
+					message: "OTP has expired",
+				});
+			}
+			if (toCheckOtp.value === ctx.body.code) {
 				return ctx.context.valid();
 			} else {
 				return ctx.context.invalid();
