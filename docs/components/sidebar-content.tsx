532ff9f2f6ff890aeac9cbc709603342c5d2282f
Multinite <founders.multinite@gmail.com>
2024-12-07 07:20:21 +1000

    add: community-plugins sidebar link

--- docs/components/sidebar-content.tsx
+++ docs/components/sidebar-content.tsx
@@ -944,6 +944,18 @@ export const contents: Content[] = [
 				),
 				href: "/docs/plugins/jwt",
 			},
+			{
+				title: "Community Plugins",
+				href: "/docs/plugins/community-plugins",
+				icon: () => (
+					<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-users">
+						<path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2"/>
+						<circle cx="9" cy="7" r="4"/>
+						<path d="M22 21v-2a4 4 0 0 0-3-3.87"/>
+						<path d="M16 3.13a4 4 0 0 1 0 7.75"/>
+					</svg>
+				),
+			},
 		],
 	},
 	{
@@ -1027,7 +1039,7 @@ export const contents: Content[] = [
 			{
 				title: "Security",
 				href: "/docs/reference/security",
-				icon: () => <ShieldCheck className="text-current w-4 h-4" />,
+				icon: () => <ShieldCheck className="w-4 h-4 text-current" />,
 			},
 		],
 	},
