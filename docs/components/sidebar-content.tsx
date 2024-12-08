15ad38597a2c26304e1710756f02656dce1cf28c
Bereket Engida <bekacru@gmail.com>
2024-12-08 10:58:52 +0300

    minor fixes

--- docs/components/sidebar-content.tsx
+++ docs/components/sidebar-content.tsx
@@ -945,14 +945,31 @@ export const contents: Content[] = [
 				href: "/docs/plugins/jwt",
 			},
 			{
+				title: "Other",
+				group: true,
+				href: "/docs/plugins/1st-party-plugins",
+				icon: () => <LucideAArrowDown className="w-4 h-4" />,
+			},
+			{
 				title: "Community Plugins",
 				href: "/docs/plugins/community-plugins",
 				icon: () => (
-					<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-users">
-						<path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2"/>
-						<circle cx="9" cy="7" r="4"/>
-						<path d="M22 21v-2a4 4 0 0 0-3-3.87"/>
-						<path d="M16 3.13a4 4 0 0 1 0 7.75"/>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="1em"
+						height="1em"
+						viewBox="0 0 24 24"
+						fill="none"
+						stroke="currentColor"
+						stroke-width="2"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						className="lucide lucide-users"
+					>
+						<path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
+						<circle cx="9" cy="7" r="4" />
+						<path d="M22 21v-2a4 4 0 0 0-3-3.87" />
+						<path d="M16 3.13a4 4 0 0 1 0 7.75" />
 					</svg>
 				),
 			},
