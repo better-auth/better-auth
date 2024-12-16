e802841ab671752f6d620eab722e15e3e904b54f
Bereket Engida <bekacru@gmail.com>
2024-12-16 15:36:30 +0300

    fix: dont override freshAge when set to 0

--- packages/better-auth/src/init.ts
+++ packages/better-auth/src/init.ts
@@ -100,7 +100,7 @@ export const init = async (options: BetterAuthOptions) => {
 					? options.session.updateAge
 					: 24 * 60 * 60, // 24 hours
 			expiresIn: options.session?.expiresIn || 60 * 60 * 24 * 7, // 7 days
-			freshAge: options.session?.freshAge === undefined ? 0 : options.session.freshAge,
+			freshAge: options.session?.freshAge === undefined ? 5 * 60 : options.session.freshAge,
 		},
 		secret,
 		rateLimit: {
