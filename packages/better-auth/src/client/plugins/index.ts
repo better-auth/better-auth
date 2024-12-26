25eb5b9ddd74d10e0e35ddc721dbf4790ec619d3
panteliselef <panteliselef@outlook.com>
2024-12-26 19:46:22 +0200

    chore: remove duplicate passkey export

--- packages/better-auth/src/client/plugins/index.ts
+++ packages/better-auth/src/client/plugins/index.ts
@@ -2,7 +2,6 @@ export * from "../../plugins/organization/client";
 export * from "../../plugins/username/client";
 export * from "../../plugins/passkey/client";
 export * from "../../plugins/two-factor/client";
-export * from "../../plugins/passkey/client";
 export * from "../../plugins/magic-link/client";
 export * from "../../plugins/phone-number/client";
 export * from "../../plugins/anonymous/client";
