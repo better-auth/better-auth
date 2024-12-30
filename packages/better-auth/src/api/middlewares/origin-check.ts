3cb73f45d56b02ae8bc73a9b49ce4e876a18d6ee
Bereket Engida <bekacru@gmail.com>
2024-12-30 13:40:04 +0300

    fix: apply origin check selectively

--- packages/better-auth/src/api/middlewares/origin-check.ts
+++ packages/better-auth/src/api/middlewares/origin-check.ts
@@ -2,12 +2,16 @@ import { APIError } from "better-call";
 import { createAuthMiddleware } from "../call";
 import { wildcardMatch } from "../../utils/wildcard";
 import { getHost } from "../../utils/url";
+import type { GenericEndpointContext } from "src/types";
 
 /**
  * A middleware to validate callbackURL and origin against
  * trustedOrigins.
  */
 export const originCheckMiddleware = createAuthMiddleware(async (ctx) => {
+	if (ctx.request?.method !== "POST") {
+		return;
+	}
 	const { body, query, context } = ctx;
 	const originHeader =
 		ctx.headers?.get("origin") || ctx.headers?.get("referer") || "";
@@ -55,3 +59,42 @@ export const originCheckMiddleware = createAuthMiddleware(async (ctx) => {
 	errorCallbackURL && validateURL(errorCallbackURL, "errorCallbackURL");
 	newUserCallbackURL && validateURL(redirectURL, "newUserCallbackURL");
 });
+
+export const originCheck = (
+	getValue: (ctx: GenericEndpointContext) => string,
+) =>
+	createAuthMiddleware(async (ctx) => {
+		const { context } = ctx;
+		const callbackURL = getValue(ctx);
+		const trustedOrigins = context.trustedOrigins;
+
+		const matchesPattern = (url: string, pattern: string): boolean => {
+			if (url.startsWith("/")) {
+				return false;
+			}
+			if (pattern.includes("*")) {
+				return wildcardMatch(pattern)(getHost(url));
+			}
+			return url.startsWith(pattern);
+		};
+
+		const validateURL = (url: string | undefined, label: string) => {
+			if (!url) {
+				return;
+			}
+			const isTrustedOrigin = trustedOrigins.some(
+				(origin) =>
+					matchesPattern(url, origin) ||
+					(url?.startsWith("/") && label !== "origin" && !url.includes(":")),
+			);
+			if (!isTrustedOrigin) {
+				ctx.context.logger.error(`Invalid ${label}: ${url}`);
+				ctx.context.logger.info(
+					`If it's a valid URL, please add ${url} to trustedOrigins in your auth config\n`,
+					`Current list of trustedOrigins: ${trustedOrigins}`,
+				);
+				throw new APIError("FORBIDDEN", { message: `Invalid ${label}` });
+			}
+		};
+		callbackURL && validateURL(callbackURL, "callbackURL");
+	});
