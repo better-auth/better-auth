6a2c4d972be70d848ccb0b7068efe5a582da908c
Multinite <founders.multinite@gmail.com>
2025-01-04 05:59:54 +1000

    fix: content.tsx

--- examples/browser-extension-example/src/content.tsx
+++ examples/browser-extension-example/src/content.tsx
@@ -1,5 +1,11 @@
 import cssText from "data-text:~style.css"
 import type { PlasmoCSConfig } from "plasmo"
+import { useState } from "react"
+import { PageControls } from "./popup"
+import { Home } from "./components/Home"
+import { Toaster } from "sonner"
+import { SignIn } from "./components/SignIn"
+import { SignUp } from "./components/SignUp"
 
 export const config: PlasmoCSConfig = {
   matches: ["https://www.plasmo.com/*"]
@@ -12,9 +18,15 @@ export const getStyle = () => {
 }
 
 const PlasmoOverlay = () => {
+  const [page, setPage] = useState<"home" | "sign-in" | "sign-up">("home")
+
   return (
-    <div className="plasmo-z-50 plasmo-flex plasmo-fixed plasmo-top-32 plasmo-right-8">
-      222
+    <div className="min-w-[400px] w-fit h-fit min-h-[500px] overflow-hidden dark bg-background text-foreground">
+      <Toaster />
+      {page === "home" && <Home setPage={setPage} />}
+      {page === "sign-in" && <SignIn setPage={setPage} />}
+      {page === "sign-up" && <SignUp setPage={setPage} />}
+      <PageControls setPage={setPage} page={page} />
     </div>
   )
 }
