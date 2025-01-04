95344a6784a7cd9bc7d591a8b4c7e5c5a2c43021
Multinite <145994855+Multinite@users.noreply.github.com>
2025-01-05 01:14:59 +1000

    Browser extensions example & guide (#1117)
    * init: plasmo example project
    
    * feat: add example for browser extension with BetterAuth
    
    - Introduced a basic browser extension example using Plasmo framework and BetterAuth for authentication.
    - Updated .env.example with BetterAuth configuration.
    - Removed unnecessary GitHub workflow file for submitting to Web Store.
    - Updated .gitignore to exclude local env files.
    - Improved README documentation for the example.
    - Included BetterAuth dependency in the package.json and updated the pnpm-lock.yaml.
    
    * add: browser-extension example complete
    
    * fix: content.tsx
    
    * remove: browser-extension package as that isn't needed
    
    * add: browser extension buide
    
    * docs: added references in the guide to the examples code
    
    * remove(dep): unused dependency
    
    * refactor: package.json - remove author & update description
    
    * wip
    
    * chore: update lock file
    
    * docs: update icon
    
    ---------
    
    Co-authored-by: Bereket Engida <bekacru@gmail.com>

---
+++ examples/browser-extension-example/src/components/Home.tsx
@@ -0,0 +1,26 @@
+import { authClient } from "@/auth/auth-client"
+
+import { Button } from "./ui/button"
+
+export const Home = ({
+  setPage
+}: {
+  setPage: (page: "home" | "sign-in" | "sign-up") => void
+}) => {
+  const { data, error, isPending } = authClient.useSession()
+
+  return (
+    <>
+      <h1 className="py-10 text-2xl text-center">Better Auth Extension Demo</h1>
+      <h2 className="py-10 text-xl text-center text-secondary-foreground">
+        {isPending
+          ? "Loading your session data..."
+          : error
+            ? error.message
+            : data
+              ? "You're Logged in 👍"
+              : "You're Not logged in 😢"}
+      </h2>
+    </>
+  )
+}
