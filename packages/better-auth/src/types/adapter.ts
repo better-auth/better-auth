753f930e888f8a75cfa4b6448a6017554c59062c
Multinite <145994855+Multinite@users.noreply.github.com>
2025-01-05 23:44:46 +1000

    refactor: Adapter createSchema return type (#1145)

--- packages/better-auth/src/types/adapter.ts
+++ packages/better-auth/src/types/adapter.ts
@@ -65,18 +65,33 @@ export type Adapter = {
 	 *
 	 * @param options
 	 * @param file - file path if provided by the user
-	 * @returns
 	 */
 	createSchema?: (
 		options: BetterAuthOptions,
 		file?: string,
-	) => Promise<{
-		code: string;
-		fileName: string;
-		append?: boolean;
-		overwrite?: boolean;
-	}>;
+	) => Promise<AdapterSchemaCreation>;
 	options?: Record<string, any>;
+};
+
+export type AdapterSchemaCreation = {
+	/**
+	 * Code to be inserted into the file
+	 */
+	code: string;
+	/**
+	 * Path to the file, including the file name and extension.
+	 * Relative paths are supported, with the current working directory devs as the base.
+	 */
+	path: string;
+	/**
+	 * Append the file if it already exists.
+	 * Note: This will not apply if `overwrite` is set to true.
+	 */
+	append?: boolean;
+	/**
+	 * Overwrite the file if it already exists
+	 */
+	overwrite?: boolean;
 };
 
 export interface AdapterInstance {
