55408d62d03b90f8f3c296912b43db225c47ae6e
reslear <reslear@gmail.com>
2025-01-10 23:53:58 +0100

    style: format

--- packages/better-auth/src/social-providers/facebook.ts
+++ packages/better-auth/src/social-providers/facebook.ts
@@ -1,7 +1,7 @@
 import { betterFetch } from "@better-fetch/fetch";
 import type { OAuthProvider, ProviderOptions } from "../oauth2";
 import { createAuthorizationURL, validateAuthorizationCode } from "../oauth2";
-import { createRemoteJWKSet, jwtVerify, decodeJwt } from 'jose';
+import { createRemoteJWKSet, jwtVerify, decodeJwt } from "jose";
 
 export interface FacebookProfile {
 	id: string;
@@ -44,91 +44,87 @@ export const facebook = (options: FacebookOptions) => {
 		},
 		async verifyIdToken(token, nonce) {
 			if (options.disableIdTokenSignIn) {
-		            return false;
-		        }
-			
-		        if (options.verifyIdToken) {
-		            return options.verifyIdToken(token, nonce);
-		        }
-					 
-			/* limited login */
-			if (token.split('.')?.length) {
-			  try {
-			    const { payload } = await jwtVerify(
-			      token,
-			      createRemoteJWKSet(
-				new URL(
-				  'https://www.facebook.com/.well-known/oauth/openid/jwks'
-				)
-			      ),
-			      {
-				algorithms: ['RS256'],
-				audience: options.clientId,
-				issuer: 'https://www.facebook.com',
-			      }
-			    )
-	      
-			    if (nonce && payload.nonce !== nonce) {
-              			return false;
-		            }
-
-			  } catch (error) {
-			    return false
-			  }
+				return false;
 			}
-		
+
+			if (options.verifyIdToken) {
+				return options.verifyIdToken(token, nonce);
+			}
+
+			/* limited login */
+			if (token.split(".")?.length) {
+				try {
+					const { payload } = await jwtVerify(
+						token,
+						createRemoteJWKSet(
+							new URL("https://www.facebook.com/.well-known/oauth/openid/jwks"),
+						),
+						{
+							algorithms: ["RS256"],
+							audience: options.clientId,
+							issuer: "https://www.facebook.com",
+						},
+					);
+
+					if (nonce && payload.nonce !== nonce) {
+						return false;
+					}
+				} catch (error) {
+					return false;
+				}
+			}
+
 			/* access_token */
-			return true
+			return true;
 		},
 
-		 async getUserInfo(token) {
-		        if (options.getUserInfo) {
-		          return options.getUserInfo(token);
-		        }
-		        // limited 
-		        if(!token.accessToken && token.idToken) {
-		          const profile = decodeJwt(token.idToken)
-		          // https://developers.facebook.com/docs/facebook-login/limited-login/permissions
-		          const userMap = await options.mapProfileToUser?.(profile);
-		          return {
-		            user: {
-		              id: profile.sub,
-		              name: profile.name,
-		              email: profile.email,
-		              image: profile.picture,
-		              emailVerified: false,
-		              ...userMap
-		            },
-		            data: profile,
-		          }
-		        } 
-			
- 			// access_token 
-		        const { data: profile, error } = await betterFetch<FacebookProfile>(
-		          "https://graph.facebook.com/me?fields=id,name,email,picture",
-		          {
-		            auth: {
-		              type: "Bearer",
-		              token: token.accessToken,
-		            },
-		          },
-		        );
-		        if (error) {
-		          return null;
-		        }
-		        const userMap = await options.mapProfileToUser?.(profile);
-		        return {
-		          user: {
-		            id: profile.id,
-		            name: profile.name,
-		            email: profile.email,
-		            image: profile.picture.data.url,
-		            emailVerified: profile.email_verified,
-		            ...userMap,
-		          },
-		          data: profile,
-		        };
-	      },
-		
+		async getUserInfo(token) {
+			if (options.getUserInfo) {
+				return options.getUserInfo(token);
+			}
+			// limited
+			if (!token.accessToken && token.idToken) {
+				const profile = decodeJwt(token.idToken);
+				// https://developers.facebook.com/docs/facebook-login/limited-login/permissions
+				const userMap = await options.mapProfileToUser?.(profile);
+				return {
+					user: {
+						id: profile.sub,
+						name: profile.name,
+						email: profile.email,
+						image: profile.picture,
+						emailVerified: false,
+						...userMap,
+					},
+					data: profile,
+				};
+			}
+
+			// access_token
+			const { data: profile, error } = await betterFetch<FacebookProfile>(
+				"https://graph.facebook.com/me?fields=id,name,email,picture",
+				{
+					auth: {
+						type: "Bearer",
+						token: token.accessToken,
+					},
+				},
+			);
+			if (error) {
+				return null;
+			}
+			const userMap = await options.mapProfileToUser?.(profile);
+			return {
+				user: {
+					id: profile.id,
+					name: profile.name,
+					email: profile.email,
+					image: profile.picture.data.url,
+					emailVerified: profile.email_verified,
+					...userMap,
+				},
+				data: profile,
+			};
+		},
 	} satisfies OAuthProvider<FacebookProfile>;
 };
