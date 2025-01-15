7a93cc90d68b4e03424c530886ab8dad137e5f76
Bereket Engida <bekacru@gmail.com>
2025-01-15 18:06:20 +0300

    chore: lint

--- packages/better-auth/src/index.ts
+++ packages/better-auth/src/index.ts
@@ -3,7 +3,7 @@ export * from "./types";
 export * from "./error";
 export * from "./cookies";
 export * from "./utils";
-export * from "./adapters"
+export * from "./adapters";
 //@ts-expect-error
 export type * from "better-call";
 export type * from "zod";
