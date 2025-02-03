54edc8e75203b86d23f10a58ee58f174309bf771
Bereket Engida <bekacru@gmail.com>
2025-02-03 22:48:49 +0300

    wip

--- packages/better-auth/src/plugins/stripe/index.ts
+++
@@ -1,8 +0,0 @@
-import type { BetterAuthPlugin } from "../../types";
-import type { StripeOptions } from "./type";
-
-export const stripe = (options: StripeOptions) => {
-	return {
-		id: "stripe",
-	} satisfies BetterAuthPlugin;
-};
