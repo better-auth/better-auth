62422f236c5248dee3d512c6f09507395a10c91f
Kinfe123 <kinfishtech@gmail.com>
2025-02-08 05:17:29 +0300

    clean ups

--- packages/cli/test/generate.test.ts
+++ packages/cli/test/generate.test.ts
@@ -9,108 +9,139 @@ import Database from "better-sqlite3";
 import type { BetterAuthOptions } from "better-auth";
 
 describe("generate", async () => {
-	it("should generate prisma schema", async () => {
-		const schema = await generatePrismaSchema({
-			file: "test.prisma",
-			adapter: prismaAdapter(
-				{},
-				{
-					provider: "postgresql",
-				},
-			)({} as BetterAuthOptions),
-			options: {
-				database: prismaAdapter(
-					{},
-					{
-						provider: "postgresql",
-					},
-				),
-				plugins: [twoFactor(), username()],
-			},
-		});
-		expect(schema.code).toMatchFileSnapshot("./__snapshots__/schema.prisma");
-	});
+  it("should generate prisma schema", async () => {
+    const schema = await generatePrismaSchema({
+      file: "test.prisma",
+      adapter: prismaAdapter(
+        {},
+        {
+          provider: "postgresql",
+        },
+      )({} as BetterAuthOptions),
+      options: {
+        database: prismaAdapter(
+          {},
+          {
+            provider: "postgresql",
+          },
+        ),
+        plugins: [twoFactor(), username()],
+      },
+    });
+    expect(schema.code).toMatchFileSnapshot("./__snapshots__/schema.prisma");
+  });
 
-	it("should generate prisma schema for mongodb", async () => {
-		const schema = await generatePrismaSchema({
-			file: "test.prisma",
-			adapter: prismaAdapter(
-				{},
-				{
-					provider: "mongodb",
-				},
-			)({} as BetterAuthOptions),
-			options: {
-				database: prismaAdapter(
-					{},
-					{
-						provider: "mongodb",
-					},
-				),
-				plugins: [twoFactor(), username()],
-			},
-		});
-		expect(schema.code).toMatchFileSnapshot(
-			"./__snapshots__/schema-mongodb.prisma",
-		);
-	});
+  it("should generate prisma schema for mongodb", async () => {
+    const schema = await generatePrismaSchema({
+      file: "test.prisma",
+      adapter: prismaAdapter(
+        {},
+        {
+          provider: "mongodb",
+        },
+      )({} as BetterAuthOptions),
+      options: {
+        database: prismaAdapter(
+          {},
+          {
+            provider: "mongodb",
+          },
+        ),
+        plugins: [twoFactor(), username()],
+      },
+    });
+    expect(schema.code).toMatchFileSnapshot(
+      "./__snapshots__/schema-mongodb.prisma",
+    );
+  });
 
-	it("should generate prisma schema for mysql", async () => {
-		const schema = await generatePrismaSchema({
-			file: "test.prisma",
-			adapter: prismaAdapter(
-				{},
-				{
-					provider: "mysql",
-				},
-			)({} as BetterAuthOptions),
-			options: {
-				database: prismaAdapter(
-					{},
-					{
-						provider: "mongodb",
-					},
-				),
-				plugins: [twoFactor(), username()],
-			},
-		});
-		expect(schema.code).toMatchFileSnapshot(
-			"./__snapshots__/schema-mysql.prisma",
-		);
-	});
+  it("should generate prisma schema for mysql", async () => {
+    const schema = await generatePrismaSchema({
+      file: "test.prisma",
+      adapter: prismaAdapter(
+        {},
+        {
+          provider: "mysql",
+        },
+      )({} as BetterAuthOptions),
+      options: {
+        database: prismaAdapter(
+          {},
+          {
+            provider: "mongodb",
+          },
+        ),
+        plugins: [twoFactor(), username()],
+      },
+    });
+    expect(schema.code).toMatchFileSnapshot(
+      "./__snapshots__/schema-mysql.prisma",
+    );
+  });
 
-	it("should generate drizzle schema", async () => {
-		const schema = await generateDrizzleSchema({
-			file: "test.drizzle",
-			adapter: drizzleAdapter(
-				{},
-				{
-					provider: "pg",
-					schema: {},
-				},
-			)({} as BetterAuthOptions),
-			options: {
-				database: drizzleAdapter(
-					{},
-					{
-						provider: "pg",
-						schema: {},
-					},
-				),
-				plugins: [twoFactor(), username()],
-			},
-		});
-		expect(schema.code).toMatchFileSnapshot("./__snapshots__/auth-schema.txt");
-	});
-
-	it("should generate kysely schema", async () => {
-		const schema = await generateMigrations({
-			file: "test.sql",
-			options: {
-				database: new Database(":memory:"),
-			},
-			adapter: {} as any,
-		});
-		expect(schema.code).toMatchFileSnapshot("./__snapshots__/migrations.sql");
-	});
+  it("should generate drizzle schema", async () => {
+    const schema = await generateDrizzleSchema({
+      file: "test.drizzle",
+      adapter: drizzleAdapter(
+        {},
+        {
+          provider: "pg",
+          schema: {},
+        },
+      )({} as BetterAuthOptions),
+      options: {
+        database: drizzleAdapter(
+          {},
+          {
+            provider: "pg",
+            schema: {},
+          },
+        ),
+        plugins: [twoFactor(), username()],
+      },
+    });
+    expect(schema.code).toMatchFileSnapshot("./__snapshots__/auth-schema.txt");
+  });
+  it("should generate drizzle schema with array column", async () => {
+    const schema = await generateDrizzleSchema({
+      file: "test.drizzle.array",
+      adapter: drizzleAdapter(
+        {},
+        {
+          provider: "pg",
+          schema: {},
+        },
+      )({} as BetterAuthOptions),
+      options: {
+        database: drizzleAdapter(
+          {},
+          {
+            provider: "pg",
+            schema: {},
+          },
+        ),
+        user: {
+          additionalFields: {
+            arrayCol: {
+              type: "string[]",
+            },
+          },
+        },
+        plugins: [twoFactor(), username()],
+      },
+    });
+    expect(schema.code).toMatchFileSnapshot(
+      "./__snapshots__/auth-schema-array.txt",
+    );
+  });
+  it("should generate kysely schema", async () => {
+    const schema = await generateMigrations({
+      file: "test.sql",
+      options: {
+        database: new Database(":memory:"),
+      },
+      adapter: {} as any,
+    });
+    expect(schema.code).toMatchFileSnapshot("./__snapshots__/migrations.sql");
+  });
 });
