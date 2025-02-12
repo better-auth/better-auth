4e1a82b4004721156409d511f4c3a59771d9a492
ping-maxwell <maxwell.multinite@gmail.com>
2025-02-12 13:28:45 +1000

    fix: made index.ts's getPackageInfo not crash the program entirely if ENOENT

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -13,7 +13,12 @@ process.on("SIGTERM", () => process.exit(0));
 
 async function main() {
 	const program = new Command("better-auth");
-	const packageInfo = await getPackageInfo();
+	let packageInfo: Record<string, any> = {};
+	try {
+		packageInfo = await getPackageInfo();
+	} catch (error) {
+		// it doesn't matter if we can't read the package.json file, we'll just use an empty object
+	}
 	program
 		.addCommand(migrate)
 		.addCommand(generate)
