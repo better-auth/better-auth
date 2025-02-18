fe7a593f0194d30c03960b576daca3b136a920b1
Alessandro Jean <14254807+alessandrojean@users.noreply.github.com>
2025-02-17 22:22:13 -0300

    feat: add Trakt social provider

--- packages/better-auth/src/social-providers/index.ts
+++ packages/better-auth/src/social-providers/index.ts
@@ -12,6 +12,7 @@ import { dropbox } from "./dropbox";
 import { linkedin } from "./linkedin";
 import { gitlab } from "./gitlab";
 import { reddit } from "./reddit";
+import { trakt } from "./trakt";
 import { z } from "zod";
 export const socialProviders = {
 	apple,
@@ -27,6 +28,7 @@ export const socialProviders = {
 	linkedin,
 	gitlab,
 	reddit,
+	trakt,
 };
 
 export const socialProviderList = Object.keys(socialProviders) as [
@@ -63,3 +65,4 @@ export * from "./dropbox";
 export * from "./linkedin";
 export * from "./gitlab";
 export * from "./reddit";
+export * from "./trakt";
---
+++ packages/better-auth/src/social-providers/trakt.ts
@@ -0,0 +1,90 @@
+import { betterFetch } from "@better-fetch/fetch";
+import type { OAuthProvider, ProviderOptions } from "../oauth2";
+import { createAuthorizationURL, validateAuthorizationCode } from "../oauth2";
+
+export interface TraktProfile {
+	/** The user's username */
+	username: string
+	/** The user's full name */
+	name: string
+	/** The user's ID */
+  ids: {
+		/** Usually the same as `username` */
+    slug: string
+  }
+	/** The user's biography */
+  about: string
+	/** The user's avatar */
+  images: {
+    avatar: {
+      full: string
+    }
+  }
+}
+
+export interface TraktOption extends ProviderOptions<TraktProfile> {
+	prompt?: "login"
+}
+
+export const trakt = (options: TraktOption) => {
+	return {
+		id: "trakt",
+		name: "Trakt",
+		createAuthorizationURL(data) {
+			const _scopes = data.scopes || ["openid", "profile"]
+			options.scope && _scopes.push(...options.scope);
+			return createAuthorizationURL({
+				id: "trakt",
+				options,
+				authorizationEndpoint: "https://trakt.tv/oauth/authorize",
+				scopes: _scopes,
+				state: data.state,
+				codeVerifier: data.codeVerifier,
+				redirectURI: data.redirectURI,
+			});
+		},
+		validateAuthorizationCode: async ({ code, redirectURI }) => {
+			return validateAuthorizationCode({
+				code,
+				redirectURI: options.redirectURI || redirectURI,
+				options,
+				tokenEndpoint: "https://api.trakt.tv/oauth/token",
+				authentication: "post",
+			});
+		},
+		async getUserInfo(token) {
+			if (options.getUserInfo) {
+				return options.getUserInfo(token);
+			}
+			const { data: profile, error } = await betterFetch<TraktProfile>(
+				"https://api.trakt.tv/users/me?extended=full",
+				{
+					method: "GET",
+					headers: {
+						Authorization: `Bearer ${token.accessToken}`,
+						"trakt-api-version": "2",
+						"trakt-api-key": options.clientId,
+					},
+				},
+			);
+
+			if (error) {
+				return null;
+			}
+
+			const userMap = await options.mapProfileToUser?.(profile);
+
+			return {
+				user: {
+					id: profile.ids.slug,
+					name: profile.name,
+					email: profile.username || profile.ids.slug || null,
+					image: profile.images.avatar.full,
+					emailVerified: true,
+					...userMap,
+				},
+				data: profile,
+			};
+		},
+	} satisfies OAuthProvider<TraktProfile>;
+};
