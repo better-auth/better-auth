4bfafaaecbf070bc7039ab6cfdd3976bf2f56775
Thalles Passos <contato@thalles.me>
2025-02-23 15:05:21 -0300

    feat: add guard and decorators

--- packages/better-auth/src/integrations/nestjs.ts
+++ packages/better-auth/src/integrations/nestjs.ts
@@ -1,13 +1,58 @@
 import {
+	createParamDecorator,
 	Inject,
 	Logger,
 	Module,
 	RequestMethod,
+	SetMetadata,
+	UnauthorizedException,
+	type CanActivate,
+	type ExecutionContext,
 	type MiddlewareConsumer,
 	type NestModule,
 } from "@nestjs/common";
+import { Reflector } from "@nestjs/core";
 import type { Auth } from "../auth";
-import { toNodeHandler } from "./node";
+import { fromNodeHeaders, toNodeHandler } from "./node";
+
+export const Public = () => SetMetadata("PUBLIC", true);
+export const Optional = () => SetMetadata("OPTIONAL", true);
+
+export const Session = createParamDecorator(
+	(_data: unknown, context: ExecutionContext) => {
+		const request = context.switchToHttp().getRequest();
+		return request.session;
+	},
+);
+
+class BetterAuthGuard implements CanActivate {
+	constructor(
+		private readonly reflector: Reflector,
+		@Inject("BETTER_AUTH_OPTIONS")
+		private readonly auth: Auth,
+	) {}
+
+	async canActivate(context: ExecutionContext): Promise<boolean> {
+		const request = context.switchToHttp().getRequest();
+		const session = await this.auth.api.getSession({
+			headers: fromNodeHeaders(request.headers),
+		});
+
+		request.session = session;
+
+		const isPublic = this.reflector.get("PUBLIC", context.getHandler());
+
+		if (isPublic) return true;
+
+		const isOptional = this.reflector.get("OPTIONAL", context.getHandler());
+
+		if (isOptional && !session) return true;
+
+		if (!session) throw new UnauthorizedException();
+
+		return true;
+	}
+}
 
 @Module({})
 export class BetterAuthModule implements NestModule {
