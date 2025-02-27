51475751b0addf832ce4a3bd26161de956ff1b23
Bereket Engida <bekacru@gmail.com>
2025-02-27 09:26:45 +0300

    docs

--- docs/components/sidebar-content.tsx
+++ docs/components/sidebar-content.tsx
@@ -1117,10 +1117,31 @@ export const contents: Content[] = [
 				href: "/docs/plugins/jwt",
 			},
 			{
-				title: "Other",
+				title: "3d party",
 				group: true,
 				href: "/docs/plugins/1st-party-plugins",
 				icon: () => <LucideAArrowDown className="w-4 h-4" />,
+			},
+			{
+				title: "Stripe",
+				href: "/docs/plugins/stripe",
+				icon: () => (
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="1.2em"
+						height="1.2em"
+						viewBox="0 0 24 24"
+					>
+						<path
+							fill="none"
+							stroke="currentColor"
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							strokeWidth="2"
+							d="M11.453 8.056c0-.623.518-.979 1.442-.979c1.69 0 3.41.343 4.605.923l.5-4c-.948-.449-2.82-1-5.5-1c-1.895 0-3.373.087-4.5 1c-1.172.956-2 2.33-2 4c0 3.03 1.958 4.906 5 6c1.961.69 3 .743 3 1.5c0 .735-.851 1.5-2 1.5c-1.423 0-3.963-.609-5.5-1.5l-.5 4c1.321.734 3.474 1.5 6 1.5c2 0 3.957-.468 5.084-1.36C18.347 18.661 19 17.372 19 15.5c0-3.096-1.915-4.547-5-5.637c-1.646-.605-2.544-1.07-2.544-1.807z"
+						></path>
+					</svg>
+				),
 			},
 			{
 				title: "Community Plugins",
