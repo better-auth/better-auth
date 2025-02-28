657f6aa76108d2a34cf7eccf306b0571b3388b08
Bereket Engida <bekacru@gmail.com>
2025-02-28 11:56:31 +0300

    feat: Enhance version release page design and content

--- docs/app/v/[[...slug]]/_compoents/version-release.tsx
+++ docs/app/v/[[...slug]]/_compoents/version-release.tsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import TableOfContents from "./toc";
 import { cn, formatDate } from "@/lib/utils";
 
-export default function VersionRelase({
+export default function VersionRelease({
 	children,
 	sections,
 	title,
@@ -30,23 +30,9 @@ export default function VersionRelase({
 	return (
 		<div className="flex flex-col relative min-h-screen space-y-10 bg-black/90 text-white">
 			{/* Header Section */}
-			<div className="flex">
-				<div className="flex-1 overflow-auto">
-					<div className="max-w-4xl sticky top-0 space-y-5 mt-20 mx-auto py-8 px-4">
-						<div className="size-full absolute top-0 left-0 bg-[length:65px_65px] opacity-5 bg-[url(/plus.svg)] bg-repeat w-full h-full" />
-
-						<h1 className="text-5xl mb-4 text-center">
-							{title} - {formatDate(date)}
-						</h1>
-						<p className="text-lg text-gray-400 text-center max-w-xl mx-auto">
-							{description}
-						</p>
-					</div>
-				</div>
-			</div>
 
 			{/* Grid Background */}
-			<div className="relative py-24 pb-0">
+			<div className=" py-10 pb-0 ">
 				<div className="absolute inset-0 z-0">
 					<div className="grid grid-cols-12 h-full">
 						{Array(12)
@@ -54,11 +40,11 @@ export default function VersionRelase({
 							.map((_, i) => (
 								<div
 									key={i}
-									className="border-l border-dashed border-stone-100 dark:border-white/10 h-full"
+									className="border-l border-dashed border-stone-100 dark:border-white/5 h-full"
 								/>
 							))}
 					</div>
-					<div className="grid grid-rows-12 w-full absolute top-0">
+					<div className="grid grid-rows-12 w-full absolute  top-0">
 						{Array(12)
 							.fill(null)
 							.map((_, i) => (
@@ -71,6 +57,20 @@ export default function VersionRelase({
 				</div>
 			</div>
 
+			<div className="flex">
+				<div className="flex-1 overflow-auto">
+					<div className="max-w-4xl sticky top-0 space-y-2 mx-auto px-4">
+						<div className="size-full absolute top-0 left-0 bg-[length:65px_65px] opacity-5 bg-[url(/plus.svg)] bg-repeat w-full h-full" />
+						<h1 className="text-3xl text-center">
+							{title} - {formatDate(date)}
+						</h1>
+						<p className="text-lg text-gray-400 text-center max-w-xl mx-auto">
+							{description}
+						</p>
+					</div>
+				</div>
+			</div>
+
 			{/* Main Content */}
 			<div className="bg-black max-w-6xl mx-auto text-white/90 flex-grow">
 				<div className="container mx-auto px-4 py-8 flex gap-8">
@@ -78,7 +78,7 @@ export default function VersionRelase({
 
 					<main className="flex-1 space-y-8 relative pb-44 border px-10 border-dashed border-zinc-400  dark:border-zinc-700/50 relative">
 						<Icon className="-top-3 -left-3" />
-						<Icon className="-top-3 -right-3" />
+						<Icon className="-top-0 -right-3" />
 						<Icon className="-bottom-3 -left-3" />
 						<Icon className="-bottom-3 -right-3" />
 						{children}
--- docs/app/v/[[...slug]]/page.tsx
+++ docs/app/v/[[...slug]]/page.tsx
@@ -15,119 +15,119 @@ import defaultMdxComponents from "fumadocs-ui/mdx";
 import { File, Folder, Files } from "fumadocs-ui/components/files";
 import { Accordion, Accordions } from "fumadocs-ui/components/accordion";
 import { contents } from "@/components/sidebar-content";
-import VersionRelase from "./_compoents/version-release";
+import VersionRelease from "./_compoents/version-release";
 import { Pre } from "fumadocs-ui/components/codeblock";
 
 export default async function Page({
-  params,
+	params,
 }: {
-  params: Promise<{ slug?: string[] }>;
+	params: Promise<{ slug?: string[] }>;
 }) {
-  const { slug } = await params;
-  const page = v.getPage(slug);
-  if (!page) {
-    notFound();
-  }
-  const MDX = page.data?.body;
-  const toc = page.data?.toc;
-  const { title, description, date } = page.data;
-  let tocContent = toc.map((t) => {
-    return {
-      id: t.url,
-      title: t.title.props?.children?.toString(),
-    };
-  });
-  console.log({ tocContent });
-  return (
-    <VersionRelase
-      title={title}
-      description={description}
-      date={date}
-      sections={tocContent}
-    >
-      <MDX
-        components={{
-          ...defaultMdxComponents,
-          Link: ({
-            className,
-            ...props
-          }: React.ComponentProps<typeof Link>) => (
-            <Link
-              className={cn(
-                "font-medium underline underline-offset-4",
-                className,
-              )}
-              {...props}
-            />
-          ),
-          Step,
-          Steps,
-          File,
-          Folder,
-          Files,
-          Tab,
-          Tabs,
-          Pre: Pre,
-          GenerateSecret,
-          AnimatePresence,
-          TypeTable,
-          Features,
-          ForkButton,
-          DatabaseTable,
-          Accordion,
-          Accordions,
-        }}
-      />
-    </VersionRelase>
-  );
+	const { slug } = await params;
+	const page = v.getPage(slug);
+	if (!page) {
+		notFound();
+	}
+	const MDX = page.data?.body;
+	const toc = page.data?.toc;
+	const { title, description, date } = page.data;
+	let tocContent = toc.map((t) => {
+		return {
+			id: t.url,
+			title: t.title.props?.children?.toString(),
+		};
+	});
+	console.log({ tocContent });
+	return (
+		<VersionRelease
+			title={title}
+			description={description}
+			date={date}
+			sections={tocContent}
+		>
+			<MDX
+				components={{
+					...defaultMdxComponents,
+					Link: ({
+						className,
+						...props
+					}: React.ComponentProps<typeof Link>) => (
+						<Link
+							className={cn(
+								"font-medium underline underline-offset-4",
+								className,
+							)}
+							{...props}
+						/>
+					),
+					Step,
+					Steps,
+					File,
+					Folder,
+					Files,
+					Tab,
+					Tabs,
+					Pre: Pre,
+					GenerateSecret,
+					AnimatePresence,
+					TypeTable,
+					Features,
+					ForkButton,
+					DatabaseTable,
+					Accordion,
+					Accordions,
+				}}
+			/>
+		</VersionRelease>
+	);
 }
 
 export async function generateMetadata({
-  params,
+	params,
 }: {
-  params: Promise<{ slug?: string[] }>;
+	params: Promise<{ slug?: string[] }>;
 }) {
-  const { slug } = await params;
-  const page = v.getPage(slug);
-  if (page == null) notFound();
-  const baseUrl = process.env.NEXT_PUBLIC_URL || process.env.VERCEL_URL;
-  const url = new URL(`${baseUrl}/api/og`);
-  const { title, description } = page.data;
-  console.log({ title, description });
-  const pageSlug = page.file.path;
-  url.searchParams.set("type", "Version Release");
-  url.searchParams.set("mode", "dark");
-  url.searchParams.set("heading", `${title}`);
+	const { slug } = await params;
+	const page = v.getPage(slug);
+	if (page == null) notFound();
+	const baseUrl = process.env.NEXT_PUBLIC_URL || process.env.VERCEL_URL;
+	const url = new URL(`${baseUrl}/api/og`);
+	const { title, description } = page.data;
+	console.log({ title, description });
+	const pageSlug = page.file.path;
+	url.searchParams.set("type", "Version Release");
+	url.searchParams.set("mode", "dark");
+	url.searchParams.set("heading", `${title}`);
 
-  return {
-    title,
-    description,
-    openGraph: {
-      title,
-      description,
-      type: "website",
-      url: absoluteUrl(`v/${pageSlug}`),
-      images: [
-        {
-          url: url.toString(),
-          width: 1200,
-          height: 630,
-          alt: title,
-        },
-      ],
-    },
-    twitter: {
-      card: "summary_large_image",
-      title,
-      description,
-      images: [url.toString()],
-    },
-  };
+	return {
+		title,
+		description,
+		openGraph: {
+			title,
+			description,
+			type: "website",
+			url: absoluteUrl(`v/${pageSlug}`),
+			images: [
+				{
+					url: url.toString(),
+					width: 1200,
+					height: 630,
+					alt: title,
+				},
+			],
+		},
+		twitter: {
+			card: "summary_large_image",
+			title,
+			description,
+			images: [url.toString()],
+		},
+	};
 }
 
 export function generateStaticParams() {
-  const res = v.getPages().map((page) => ({
-    slug: page.slugs,
-  }));
-  return v.generateParams();
+	const res = v.getPages().map((page) => ({
+		slug: page.slugs,
+	}));
+	return v.generateParams();
 }
