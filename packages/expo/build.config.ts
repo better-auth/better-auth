44352d741414714a0f7296776a95c96edce5a05a
Bereket Engida <bekacru@gmail.com>
2025-02-28 09:45:29 +0300

    chore: migrate from tsup to unbuild

---
+++ packages/expo/build.config.ts
@@ -0,0 +1,22 @@
+import { defineBuildConfig } from "unbuild";
+
+export default defineBuildConfig({
+	declaration: true,
+	rollup: {
+		emitCJS: true,
+		esbuild: {
+			treeShaking: true,
+		},
+	},
+	outDir: "dist",
+	externals: [
+		"better-auth",
+		"better-call",
+		"@better-fetch/fetch",
+		"react-native",
+		"expo-web-browser",
+		"expo-linking",
+		"expo-constants",
+	],
+	entries: ["./src/index.ts", "./src/client.ts"],
+});
