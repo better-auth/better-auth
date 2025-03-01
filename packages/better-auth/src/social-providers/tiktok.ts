07217636a9a084fb17c70d94181f7890423d5ad5
Bereket Engida <bekacru@gmail.com>
2025-03-01 21:09:45 +0300

    feat(social-providers): Add option to disable default scopes

--- packages/better-auth/src/social-providers/tiktok.ts
+++ packages/better-auth/src/social-providers/tiktok.ts
@@ -124,8 +124,9 @@ export const tiktok = (options: TiktokOptions) => {
 		id: "tiktok",
 		name: "TikTok",
 		createAuthorizationURL({ state, scopes, redirectURI }) {
-			const _scopes = scopes || ["user.info.profile"];
+			const _scopes = options.disableDefaultScope ? [] : ["user.info.profile"];
 			options.scope && _scopes.push(...options.scope);
+			scopes && _scopes.push(...scopes);
 			return new URL(
 				`https://www.tiktok.com/v2/auth/authorize?scope=${_scopes.join(
 					",",
