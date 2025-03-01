5399397ce283b724a0cb7fca9a53c7e4049605e2
Kinfe123 <kinfishtech@gmail.com>
2025-03-02 01:27:42 +0300

    fix: metadata issue

--- docs/app/changelogs/layout.tsx
+++ docs/app/changelogs/layout.tsx
@@ -1,5 +1,30 @@
 import type { ReactNode } from "react";
 
+const title = "Changel";
+const description = "Latest changes , fixes and updates.";
+const ogImage = "https://better-auth.com/release-og/changelog-og.png";
+
+export const metadata = {
+	metadataBase: new URL("https://better-auth.com/changelogs"),
+	title,
+	description,
+	openGraph: {
+		title,
+		description,
+		images: [
+			{
+				url: ogImage,
+			},
+		],
+		url: "https://better-auth.com/changelogs",
+	},
+	twitter: {
+		card: "summary_large_image",
+		title,
+		description,
+		images: [ogImage],
+	},
+};
 export default function Layout({ children }: { children: ReactNode }) {
 	return <div>{children}</div>;
 }
