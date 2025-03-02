6c71eebb2afe894d26fe49829dee953b6a551fed
ping-maxwell <maxwell.multinite@gmail.com>
2025-03-02 17:12:48 +1000

    remove: new badge from "other social providers"

--- docs/components/sidebar-content.tsx
+++ docs/components/sidebar-content.tsx
@@ -737,7 +737,6 @@ C0.7,239.6,62.1,0.5,62.2,0.4c0,0,54,13.8,119.9,30.8S302.1,62,302.2,62c0.2,0,0.2,
 			{
 				title: "Other Social Providers",
 				href: "/docs/authentication/other-social-providers",
-				isNew: true,
 				icon: () => (
 					<svg
 						xmlns="http://www.w3.org/2000/svg"
