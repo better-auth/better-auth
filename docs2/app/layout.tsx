06431f49151249ded7dba1887eaa43e78036018f
Kinfe123 <kinfishtech@gmail.com>
2025-03-07 21:57:02 +0300

    fix: docs layout and bg consistency

--- docs2/app/layout.tsx
+++ docs2/app/layout.tsx
@@ -10,42 +10,42 @@ import Loglib from "@loglib/tracker/react";
 import { Analytics } from "@vercel/analytics/react";
 
 export const metadata = createMetadata({
-	title: {
-		template: "%s | Better Auth",
-		default: "Better Auth",
-	},
-	description: "The most comprehensive authentication library for TypeScript.",
-	metadataBase: baseUrl,
+  title: {
+    template: "%s | Better Auth",
+    default: "Better Auth",
+  },
+  description: "The most comprehensive authentication library for TypeScript.",
+  metadataBase: baseUrl,
 });
 
 export default function Layout({ children }: { children: ReactNode }) {
-	return (
-		<html lang="en" suppressHydrationWarning>
-			<head>
-				<link rel="icon" href="/favicon/favicon.ico" sizes="any" />
-			</head>
-			<body
-				className={`${GeistSans.variable} ${GeistMono.variable} font-sans relative`}
-			>
-				<RootProvider
-					theme={{
-						enableSystem: true,
-						defaultTheme: "dark",
-					}}
-				>
-					<NavbarProvider>
-						<Navbar />
-						{children}
-					</NavbarProvider>
-				</RootProvider>
-				<Loglib
-					config={{
-						id: "better-auth",
-						consent: "granted",
-					}}
-				/>
-				<Analytics />
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="/favicon/favicon.ico" sizes="any" />
+      </head>
+      <body
+        className={`${GeistSans.variable} ${GeistMono.variable} font-sans relative dark:bg-black/[0.95]`}
+      >
+        <RootProvider
+          theme={{
+            enableSystem: true,
+            defaultTheme: "dark",
+          }}
+        >
+          <NavbarProvider>
+            <Navbar />
+            {children}
+          </NavbarProvider>
+        </RootProvider>
+        <Loglib
+          config={{
+            id: "better-auth",
+            consent: "granted",
+          }}
+        />
+        <Analytics />
+      </body>
+    </html>
+  );
 }
