2d06cb711660e5a40d1abb87064ed5806d533718
Jumpei Ogawa <git@phanective.org>
2025-03-07 11:53:48 +0900

    fix; add missing `import`s

--- examples/svelte-kit-example/src/lib/components/ui/range-calendar/range-calendar-prev-button.svelte
+++ examples/svelte-kit-example/src/lib/components/ui/range-calendar/range-calendar-prev-button.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 import { RangeCalendar as RangeCalendarPrimitive } from "bits-ui";
+import { cn } from "$lib/utils.js";
 
 type $$Props = RangeCalendarPrimitive.PrevButtonProps;
 type $$Events = RangeCalendarPrimitive.PrevButtonEvents;
