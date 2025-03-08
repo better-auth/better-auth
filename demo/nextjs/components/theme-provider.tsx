ecd2b7b42eb88666d3ad0cd67c915836e2de8c19
Kinfe123 <kinfishtech@gmail.com>
2025-03-08 21:52:58 +0300

    fix: better props support

--- demo/nextjs/components/theme-provider.tsx
+++ demo/nextjs/components/theme-provider.tsx
@@ -1,7 +1,8 @@
 "use client";
+
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { type ThemeProviderProps } from "next-themes/dist/types";
+import { ThemeProviderProps } from "next-themes/dist/types";
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-	return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
--- docs/app/layout.tsx
+++ docs/app/layout.tsx
@@ -6,58 +6,69 @@ import { NavbarProvider } from "@/components/nav-mobile";
 import { GeistMono } from "geist/font/mono";
 import { GeistSans } from "geist/font/sans";
 import { baseUrl, createMetadata } from "@/lib/metadata";
+import Loglib from "@loglib/tracker/react";
 import { Analytics } from "@vercel/analytics/react";
 import { ThemeProvider } from "@/components/theme-provider";
 
 export const metadata = createMetadata({
-	title: {
-		template: "%s | Better Auth",
-		default: "Better Auth",
-	},
-	description: "The most comprehensive authentication library for TypeScript.",
-	metadataBase: baseUrl,
+  title: {
+    template: "%s | Better Auth",
+    default: "Better Auth",
+  },
+  description: "The most comprehensive authentication library for TypeScript.",
+  metadataBase: baseUrl,
 });
 
-export default function Layout({ children }: { children: ReactNode }) {
-	return (
-		<html lang="en" suppressHydrationWarning>
-			<head>
-				<link rel="icon" href="/favicon/favicon.ico" sizes="any" />
-				<script
-					dangerouslySetInnerHTML={{
-						__html: `
+export default function Layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="/favicon/favicon.ico" sizes="any" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
                     try {
                       if (localStorage.theme === 'dark' || ((!('theme' in localStorage) || localStorage.theme === 'system') && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
                         document.querySelector('meta[name="theme-color"]').setAttribute('content')
                       }
                     } catch (_) {}
                   `,
-					}}
-				/>
-			</head>
-			<body
-				className={`${GeistSans.variable} ${GeistMono.variable} bg-background font-sans relative `}
-			>
-				<ThemeProvider
-					attribute="class"
-					defaultTheme="dark"
-					enableSystem
-					disableTransitionOnChange
-				>
-					<RootProvider
-						theme={{
-							enableSystem: true,
-							defaultTheme: "dark",
-						}}
-					>
-						<NavbarProvider>
-							<Navbar />
-							{children}
-						</NavbarProvider>
-					</RootProvider>
-					<Analytics />
-				</ThemeProvider>
-			</body>
-		</html>
-	);
+          }}
+        />
+      </head>
+      <body
+        className={`${GeistSans.variable} ${GeistMono.variable} bg-background font-sans relative `}
+      >
+        <RootProvider
+          theme={{
+            enableSystem: true,
+            defaultTheme: "dark",
+          }}
+        >
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <NavbarProvider>
+              <Navbar />
+              {children}
+            </NavbarProvider>
+          </ThemeProvider>
+        </RootProvider>
+        <Loglib
+          config={{
+            id: "better-auth",
+            consent: "granted",
+          }}
+        />
+        <Analytics />
+      </body>
+    </html>
+  );
 }
