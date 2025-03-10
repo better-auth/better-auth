d9541f07570e0a6484b97a08f4813bef8f058a6f
Sander Vispoel <sandervispoel@hotmail.com>
2025-03-10 13:55:42 +0100

    chore: formatting

--- packages/better-auth/src/social-providers/battlenet.ts
+++ packages/better-auth/src/social-providers/battlenet.ts
@@ -3,79 +3,85 @@ import type { OAuthProvider, ProviderOptions } from "../oauth2";
 import { createAuthorizationURL, validateAuthorizationCode } from "../oauth2";
 
 export interface BattleNetProfile {
-  id: string;
-  battletag: string;
-  sub: string;
+	id: string;
+	battletag: string;
+	sub: string;
 }
 
 export interface BattleNetOptions extends ProviderOptions<BattleNetProfile> {
-  region?: "us" | "eu" | "kr" | "tw" | "cn";
+	region?: "us" | "eu" | "kr" | "tw" | "cn";
 }
 
 export const battlenet = (options: BattleNetOptions) => {
-  const region = options.region || "us";
-  const authorizationEndpoint = region === 'cn' ? 'https://oauth.battlenet.com.cn/authorize' : 'https://oauth.battle.net/authorize';
-  const tokenEndpoint = region === 'cn' ? 'https://oauth.battlenet.com.cn/token' : 'https://oauth.battle.net/token';
+	const region = options.region || "us";
+	const authorizationEndpoint =
+		region === "cn"
+			? "https://oauth.battlenet.com.cn/authorize"
+			: "https://oauth.battle.net/authorize";
+	const tokenEndpoint =
+		region === "cn"
+			? "https://oauth.battlenet.com.cn/token"
+			: "https://oauth.battle.net/token";
 
-  return {
-    id: "battlenet",
-    name: "Battle.net",
-    createAuthorizationURL({ state, scopes, codeVerifier, redirectURI }) {
-      const _scopes = options.disableDefaultScope ? [] : ["openid"];
-      options.scope && _scopes.push(...options.scope);
-      scopes && _scopes.push(...scopes);
-      
-      return createAuthorizationURL({
-        id: "battlenet",
-        options,
-        authorizationEndpoint,
-        scopes: _scopes,
-        state,
-        codeVerifier,
-        redirectURI,
-      });
-    },
-    validateAuthorizationCode: async ({ code, redirectURI, codeVerifier }) => {
-      return validateAuthorizationCode({
-        code,
-        redirectURI,
-        codeVerifier,
-        options,
-        tokenEndpoint,
-        authentication: "basic",
-      });
-    },
-    async getUserInfo(token) {
-      if (options.getUserInfo) {
-        return options.getUserInfo(token);
-      }
-      
-      const { data: profile, error } = await betterFetch<BattleNetProfile>(
-        'https://oauth.battle.net/userinfo',
-        {
-          headers: {
-            "User-Agent": "better-auth",
-            authorization: `Bearer ${token.accessToken}`,
-          },
-        },
-      );
+	return {
+		id: "battlenet",
+		name: "Battle.net",
+		createAuthorizationURL({ state, scopes, codeVerifier, redirectURI }) {
+			const _scopes = options.disableDefaultScope ? [] : ["openid"];
+			options.scope && _scopes.push(...options.scope);
+			scopes && _scopes.push(...scopes);
 
-      if (error) {
-        return null;
-      }
+			return createAuthorizationURL({
+				id: "battlenet",
+				options,
+				authorizationEndpoint,
+				scopes: _scopes,
+				state,
+				codeVerifier,
+				redirectURI,
+			});
+		},
+		validateAuthorizationCode: async ({ code, redirectURI, codeVerifier }) => {
+			return validateAuthorizationCode({
+				code,
+				redirectURI,
+				codeVerifier,
+				options,
+				tokenEndpoint,
+				authentication: "basic",
+			});
+		},
+		async getUserInfo(token) {
+			if (options.getUserInfo) {
+				return options.getUserInfo(token);
+			}
 
-      const userMap = await options.mapProfileToUser?.(profile);
+			const { data: profile, error } = await betterFetch<BattleNetProfile>(
+				"https://oauth.battle.net/userinfo",
+				{
+					headers: {
+						"User-Agent": "better-auth",
+						authorization: `Bearer ${token.accessToken}`,
+					},
+				},
+			);
 
-      return {
-        user: {
-          id: profile.id,
-          name: profile.battletag,
-          email: '-', // Battle.net doesn't provide email
-          emailVerified: false,
-          ...userMap,
-        },
-        data: profile,
-      };
-    },
-  } satisfies OAuthProvider<BattleNetProfile>;
+			if (error) {
+				return null;
+			}
+
+			const userMap = await options.mapProfileToUser?.(profile);
+
+			return {
+				user: {
+					id: profile.id,
+					name: profile.battletag,
+					email: "-", // Battle.net doesn't provide email
+					emailVerified: false,
+					...userMap,
+				},
+				data: profile,
+			};
+		},
+	} satisfies OAuthProvider<BattleNetProfile>;
 };
