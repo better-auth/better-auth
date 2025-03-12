cb992112cd65acbba7a24cdd0d3082861614255a
Bereket Engida <bekacru@gmail.com>
2025-03-12 15:37:27 +0300

    chore: lint

--- dev/bun/auth.ts
+++ dev/bun/auth.ts
@@ -1,6 +1,6 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-import { createAuthMiddleware, twoFactor } from "better-auth/plugins";
+import { twoFactor } from "better-auth/plugins";
 
 export const auth = betterAuth({
 	baseURL: "http://localhost:4000",
