c4bc11df4d234cf7cbf030be4fd54c9b2c775f84
ping-maxwell <maxwell.multinite@gmail.com>
2025-03-14 03:43:11 +1000

    chore: format

--- packages/better-auth/src/adapters/test.ts
+++ packages/better-auth/src/adapters/test.ts
@@ -46,12 +46,10 @@ export async function runAdapterTest(opts: AdapterTestOptions) {
 		updatedAt: new Date(),
 	};
 
-	test.skipIf(opts.testSkips?.createModel)(
-		"create model",
-		async () => {
-			const res = await adapter.create({
-				model: "user",
-				data: user,
+	test.skipIf(opts.testSkips?.createModel)("create model", async () => {
+		const res = await adapter.create({
+			model: "user",
+			data: user,
 		});
 		expect({
 			name: res.name,
@@ -63,10 +61,8 @@ export async function runAdapterTest(opts: AdapterTestOptions) {
 		user.id = res.id;
 	});
 
-	test.skipIf(opts.testSkips?.findModel)(
-		"find model",
-		async () => {
-			const res = await adapter.findOne<User>({
+	test.skipIf(opts.testSkips?.findModel)("find model", async () => {
+		const res = await adapter.findOne<User>({
 			model: "user",
 			where: [
 				{
@@ -88,43 +84,43 @@ export async function runAdapterTest(opts: AdapterTestOptions) {
 		"find model without id",
 		async () => {
 			const res = await adapter.findOne<User>({
-			model: "user",
-			where: [
-				{
-					field: "email",
-					value: user.email,
-				},
-			],
-		});
-		expect({
-			name: res?.name,
-			email: res?.email,
-		}).toEqual({
-			name: user.name,
-			email: user.email,
-		});
-	});
+				model: "user",
+				where: [
+					{
+						field: "email",
+						value: user.email,
+					},
+				],
+			});
+			expect({
+				name: res?.name,
+				email: res?.email,
+			}).toEqual({
+				name: user.name,
+				email: user.email,
+			});
+		},
+	);
 
 	test.skipIf(opts.testSkips?.findModelWithSelect)(
 		"find model with select",
 		async () => {
 			const res = await adapter.findOne({
-			model: "user",
-			where: [
-				{
-					field: "id",
-					value: user.id,
-				},
-			],
-			select: ["email"],
-		});
-		expect(res).toEqual({ email: user.email });
-	});
+				model: "user",
+				where: [
+					{
+						field: "id",
+						value: user.id,
+					},
+				],
+				select: ["email"],
+			});
+			expect(res).toEqual({ email: user.email });
+		},
+	);
 
-	test.skipIf(opts.testSkips?.updateModel)(
-		"update model",
-		async () => {
-			const newEmail = "updated@email.com";
+	test.skipIf(opts.testSkips?.updateModel)("update model", async () => {
+		const newEmail = "updated@email.com";
 
 		const res = await adapter.update<User>({
 			model: "user",
@@ -144,15 +140,12 @@ export async function runAdapterTest(opts: AdapterTestOptions) {
 		});
 	});
 
-	test.skipIf(opts.testSkips?.findMany)(
-		"should find many",
-		async () => {
-			const res = await adapter.findMany({
-				model: "user",
-			});
-			expect(res.length).toBe(1);
-		},
-	);
+	test.skipIf(opts.testSkips?.findMany)("should find many", async () => {
+		const res = await adapter.findMany({
+			model: "user",
+		});
+		expect(res.length).toBe(1);
+	});
 
 	test.skipIf(opts.testSkips?.shouldFindManyWithWhere)(
 		"should find many with where",
@@ -160,142 +153,142 @@ export async function runAdapterTest(opts: AdapterTestOptions) {
 			const user = await adapter.create<User>({
 				model: "user",
 				data: {
-				id: "2",
-				name: "user2",
-				email: "test@email.com",
-				emailVerified: true,
-				createdAt: new Date(),
-				updatedAt: new Date(),
-			},
-		});
-		const res = await adapter.findMany({
-			model: "user",
-			where: [
-				{
-					field: "id",
-					value: user.id,
+					id: "2",
+					name: "user2",
+					email: "test@email.com",
+					emailVerified: true,
+					createdAt: new Date(),
+					updatedAt: new Date(),
 				},
-			],
-		});
-		expect(res.length).toBe(1);
-	},
+			});
+			const res = await adapter.findMany({
+				model: "user",
+				where: [
+					{
+						field: "id",
+						value: user.id,
+					},
+				],
+			});
+			expect(res.length).toBe(1);
+		},
 	);
 
 	test.skipIf(opts.testSkips?.shouldFindManyWithOperators)(
 		"should find many with operators",
 		async () => {
 			const newUser = await adapter.create<User>({
-			model: "user",
-			data: {
-				id: "3",
-				name: "user",
-				email: "test-email2@email.com",
-				emailVerified: true,
-				createdAt: new Date(),
-				updatedAt: new Date(),
-			},
-		});
-		const res = await adapter.findMany({
-			model: "user",
-			where: [
-				{
-					field: "id",
-					operator: "in",
-					value: [user.id, newUser.id],
+				model: "user",
+				data: {
+					id: "3",
+					name: "user",
+					email: "test-email2@email.com",
+					emailVerified: true,
+					createdAt: new Date(),
+					updatedAt: new Date(),
 				},
-			],
-		});
-		expect(res.length).toBe(2);
-	},
+			});
+			const res = await adapter.findMany({
+				model: "user",
+				where: [
+					{
+						field: "id",
+						operator: "in",
+						value: [user.id, newUser.id],
+					},
+				],
+			});
+			expect(res.length).toBe(2);
+		},
 	);
 
 	test.skipIf(opts.testSkips?.shouldWorkWithReferenceFields)(
 		"should work with reference fields",
 		async () => {
 			let token = null;
-		const user = await adapter.create<{ id: string } & Record<string, any>>({
-			model: "user",
-			data: {
-				id: "4",
-				name: "user",
-				email: "my-email@email.com",
-				emailVerified: true,
-				createdAt: new Date(),
-				updatedAt: new Date(),
-			},
-		});
-		const session = await adapter.create({
-			model: "session",
-			data: {
-				id: "1",
-				token: generateId(),
-				createdAt: new Date(),
-				updatedAt: new Date(),
+			const user = await adapter.create<{ id: string } & Record<string, any>>({
+				model: "user",
+				data: {
+					id: "4",
+					name: "user",
+					email: "my-email@email.com",
+					emailVerified: true,
+					createdAt: new Date(),
+					updatedAt: new Date(),
+				},
+			});
+			const session = await adapter.create({
+				model: "session",
+				data: {
+					id: "1",
+					token: generateId(),
+					createdAt: new Date(),
+					updatedAt: new Date(),
+					userId: user.id,
+					expiresAt: new Date(),
+				},
+			});
+			token = session.token;
+			const res = await adapter.findOne({
+				model: "session",
+				where: [
+					{
+						field: "userId",
+						value: user.id,
+					},
+				],
+			});
+			const resToken = await adapter.findOne({
+				model: "session",
+				where: [
+					{
+						field: "token",
+						value: token,
+					},
+				],
+			});
+			expect(res).toMatchObject({
 				userId: user.id,
-				expiresAt: new Date(),
-			},
-		});
-		token = session.token;
-		const res = await adapter.findOne({
-			model: "session",
-			where: [
-				{
-					field: "userId",
-					value: user.id,
-				},
-			],
-		});
-		const resToken = await adapter.findOne({
-			model: "session",
-			where: [
-				{
-					field: "token",
-					value: token,
-				},
-			],
-		});
-		expect(res).toMatchObject({
-			userId: user.id,
-		});
-		expect(resToken).toMatchObject({
-			userId: user.id,
-		});
-	},
+			});
+			expect(resToken).toMatchObject({
+				userId: user.id,
+			});
+		},
 	);
 
 	test.skipIf(opts.testSkips?.shouldFindManyWithSortBy)(
 		"should find many with sortBy",
 		async () => {
 			await adapter.create({
-			model: "user",
-			data: {
-				id: "5",
-				name: "a",
-				email: "a@email.com",
-				emailVerified: true,
-				createdAt: new Date(),
-				updatedAt: new Date(),
-			},
-		});
-		const res = await adapter.findMany<User>({
-			model: "user",
-			sortBy: {
-				field: "name",
-				direction: "asc",
-			},
-		});
-		expect(res[0].name).toBe("a");
+				model: "user",
+				data: {
+					id: "5",
+					name: "a",
+					email: "a@email.com",
+					emailVerified: true,
+					createdAt: new Date(),
+					updatedAt: new Date(),
+				},
+			});
+			const res = await adapter.findMany<User>({
+				model: "user",
+				sortBy: {
+					field: "name",
+					direction: "asc",
+				},
+			});
+			expect(res[0].name).toBe("a");
 
-		const res2 = await adapter.findMany<User>({
-			model: "user",
-			sortBy: {
-				field: "name",
-				direction: "desc",
-			},
-		});
+			const res2 = await adapter.findMany<User>({
+				model: "user",
+				sortBy: {
+					field: "name",
+					direction: "desc",
+				},
+			});
 
-		expect(res2[res2.length - 1].name).toBe("a");
-	},
+			expect(res2[res2.length - 1].name).toBe("a");
+		},
 	);
 
 	test.skipIf(opts.testSkips?.shouldFindManyWithLimit)(
@@ -326,39 +319,37 @@ export async function runAdapterTest(opts: AdapterTestOptions) {
 			await adapter.updateMany({
 				model: "user",
 				where: [
-				{
-					field: "name",
-					value: user.name,
+					{
+						field: "name",
+						value: user.name,
+					},
+					{
+						field: "email",
+						value: user.email,
+					},
+				],
+				update: {
+					email: "updated@email.com",
 				},
-				{
-					field: "email",
-					value: user.email,
-				},
-			],
-			update: {
+			});
+			const updatedUser = await adapter.findOne<User>({
+				model: "user",
+				where: [
+					{
+						field: "email",
+						value: "updated@email.com",
+					},
+				],
+			});
+			expect(updatedUser).toMatchObject({
+				name: user.name,
 				email: "updated@email.com",
-			},
-		});
-		const updatedUser = await adapter.findOne<User>({
-			model: "user",
-			where: [
-				{
-					field: "email",
-					value: "updated@email.com",
-				},
-			],
-		});
-		expect(updatedUser).toMatchObject({
-			name: user.name,
-			email: "updated@email.com",
-		});
-	},
+			});
+		},
 	);
 
-	test.skipIf(opts.testSkips?.deleteModel)(
-		"delete model",
-		async () => {
-			await adapter.delete({
+	test.skipIf(opts.testSkips?.deleteModel)("delete model", async () => {
+		await adapter.delete({
 			model: "user",
 			where: [
 				{
@@ -385,76 +376,78 @@ export async function runAdapterTest(opts: AdapterTestOptions) {
 			for (const id of ["to-be-delete1", "to-be-delete2", "to-be-delete3"]) {
 				await adapter.create({
 					model: "user",
-				data: {
-					id,
-					name: "to-be-deleted",
-					email: `email@test-${id}.com`,
-					emailVerified: true,
-					createdAt: new Date(),
-					updatedAt: new Date(),
-				},
+					data: {
+						id,
+						name: "to-be-deleted",
+						email: `email@test-${id}.com`,
+						emailVerified: true,
+						createdAt: new Date(),
+						updatedAt: new Date(),
+					},
+				});
+			}
+			const findResFirst = await adapter.findMany({
+				model: "user",
+				where: [
+					{
+						field: "name",
+						value: "to-be-deleted",
+					},
+				],
 			});
-		}
-		const findResFirst = await adapter.findMany({
-			model: "user",
-			where: [
-				{
-					field: "name",
-					value: "to-be-deleted",
-				},
-			],
-		});
-		expect(findResFirst.length).toBe(3);
-		await adapter.deleteMany({
-			model: "user",
-			where: [
-				{
-					field: "name",
-					value: "to-be-deleted",
-				},
-			],
-		});
-		const findRes = await adapter.findMany({
-			model: "user",
-			where: [
-				{
-					field: "name",
-					value: "to-be-deleted",
-				},
-			],
-		});
-		expect(findRes.length).toBe(0);
-	});
+			expect(findResFirst.length).toBe(3);
+			await adapter.deleteMany({
+				model: "user",
+				where: [
+					{
+						field: "name",
+						value: "to-be-deleted",
+					},
+				],
+			});
+			const findRes = await adapter.findMany({
+				model: "user",
+				where: [
+					{
+						field: "name",
+						value: "to-be-deleted",
+					},
+				],
+			});
+			expect(findRes.length).toBe(0);
+		},
+	);
 
 	test.skipIf(opts.testSkips?.shouldNotThrowOnDeleteRecordNotFound)(
 		"shouldn't throw on delete record not found",
 		async () => {
 			await adapter.delete({
-			model: "user",
-			where: [
-				{
-					field: "id",
-					value: "5",
-				},
-			],
-		});
-	},
+				model: "user",
+				where: [
+					{
+						field: "id",
+						value: "5",
+					},
+				],
+			});
+		},
 	);
 
 	test.skipIf(opts.testSkips?.shouldFindManyWithContainsOperator)(
 		"shouldn't throw on record not found",
 		async () => {
 			const res = await adapter.findOne({
-			model: "user",
-			where: [
-				{
-					field: "id",
-					value: "5",
-				},
-			],
-		});
-		expect(res).toBeNull();
-	});
+				model: "user",
+				where: [
+					{
+						field: "id",
+						value: "5",
+					},
+				],
+			});
+			expect(res).toBeNull();
+		},
+	);
 
 	test.skipIf(opts.testSkips?.shouldFindManyWithContainsOperator)(
 		"should find many with contains operator",
@@ -462,31 +455,33 @@ export async function runAdapterTest(opts: AdapterTestOptions) {
 			const res = await adapter.findMany({
 				model: "user",
 				where: [
-				{
-					field: "name",
-					operator: "contains",
-					value: "user2",
-				},
-			],
-		});
-		expect(res.length).toBe(1);
-	});
+					{
+						field: "name",
+						operator: "contains",
+						value: "user2",
+					},
+				],
+			});
+			expect(res.length).toBe(1);
+		},
+	);
 
 	test.skipIf(opts.testSkips?.shouldSearchUsersWithStartsWith)(
 		"should search users with startsWith",
 		async () => {
 			const res = await adapter.findMany({
-			model: "user",
-			where: [
-				{
-					field: "name",
-					operator: "starts_with",
-					value: "us",
-				},
-			],
-		});
-		expect(res.length).toBe(3);
-	});
+				model: "user",
+				where: [
+					{
+						field: "name",
+						operator: "starts_with",
+						value: "us",
+					},
+				],
+			});
+			expect(res.length).toBe(3);
+		},
+	);
 
 	test.skipIf(opts.testSkips?.shouldSearchUsersWithEndsWith)(
 		"should search users with endsWith",
@@ -494,15 +489,16 @@ export async function runAdapterTest(opts: AdapterTestOptions) {
 			const res = await adapter.findMany({
 				model: "user",
 				where: [
-				{
-					field: "name",
-					operator: "ends_with",
-					value: "er2",
-				},
-			],
-		});
-		expect(res.length).toBe(1);
-	});
+					{
+						field: "name",
+						operator: "ends_with",
+						value: "er2",
+					},
+				],
+			});
+			expect(res.length).toBe(1);
+		},
+	);
 
 	test.skipIf(opts.testSkips?.generateId)(
 		"should prefer generateId if provided",
