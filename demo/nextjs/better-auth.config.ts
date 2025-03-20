1dca06674255e3f10233297f5bb665f1dc7ff918
KinfeMichael Tariku <65047246+Kinfe123@users.noreply.github.com>
2025-03-20 18:37:19 +0300

    unwanted

--- demo/nextjs/better-auth.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "@better-auth/cli";
-export default defineConfig({
-	config: {
-		path: "libs/core/api/src/lib/auth.ts",
-	},
-	tsConfig: {
-		path: "../../tsconfig.json",
-	},
-});
