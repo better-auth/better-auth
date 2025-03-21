3f508c562336d8262fb5793e3965a9a0e6255179
Bereket Engida <bekacru@gmail.com>
2025-03-21 16:11:41 +0300

    feat: disabled paths

--- packages/better-auth/src/api/index.ts
+++ packages/better-auth/src/api/index.ts
@@ -32,7 +32,7 @@ import { ok } from "./routes/ok";
 import { signUpEmail } from "./routes/sign-up";
 import { error } from "./routes/error";
 import { logger } from "../utils/logger";
-import type { BetterAuthPlugin } from "../plugins";
+import { type BetterAuthPlugin } from "../plugins";
 import { onRequestRateLimit } from "./rate-limiter";
 import { toAuthEndpoints } from "./to-auth-endpoints";
 
@@ -145,6 +145,13 @@ export const router = <C extends AuthContext, Option extends BetterAuthOptions>(
 			...middlewares,
 		],
 		async onRequest(req) {
+			//handle disabled paths
+			const disabledPaths = ctx.options.disabledPaths || [];
+			const path = new URL(req.url).pathname.replace(basePath, "");
+			if (disabledPaths.includes(path)) {
+				return new Response("Not Found", { status: 404 });
+			}
+
 			for (const plugin of ctx.options.plugins || []) {
 				if (plugin.onRequest) {
 					const response = await plugin.onRequest(req, ctx);
