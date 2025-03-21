76d56ccfbec9d355fe70fe79b925c138af66c37f
Kinfe123 <kinfishtech@gmail.com>
2025-03-21 11:16:00 +0300

    spreading already existing configs

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -15,33 +15,33 @@ process.on("SIGINT", () => process.exit(0));
 process.on("SIGTERM", () => process.exit(0));
 
 async function main() {
-  const DEFAULT_CONFIG_PATH = path.join(process.cwd(), "better-auth.config.ts");
-  let config = {};
+	const DEFAULT_CONFIG_PATH = path.join(process.cwd(), "better-auth.config.ts");
+	let config = {};
 
-  try {
-    config = await loadConfig(DEFAULT_CONFIG_PATH);
-  } catch (error) {
-    console.warn(`Warning: ${error.message}`);
-  }
-  const program = new Command("better-auth");
+	try {
+		config = await loadConfig(DEFAULT_CONFIG_PATH);
+	} catch (error) {
+		console.warn(`Warning: ${error.message}`);
+	}
+	const program = new Command("better-auth");
 
-  let packageInfo: Record<string, any> = {};
-  try {
-    packageInfo = await getPackageInfo();
-  } catch (error) {
-    // it doesn't matter if we can't read the package.json file, we'll just use an empty object
-  }
-  program
-    .addCommand(migrate)
-    .addCommand(generate)
-    .addCommand(generateSecret)
-    .addCommand(init)
-    .version(packageInfo.version || "1.1.2")
-    .description("Better Auth CLI");
-  program.commands.forEach((cmd) => {
-    cmd.opts().config = config;
-  });
-  program.parse();
+	let packageInfo: Record<string, any> = {};
+	try {
+		packageInfo = await getPackageInfo();
+	} catch (error) {
+		// it doesn't matter if we can't read the package.json file, we'll just use an empty object
+	}
+	program
+		.addCommand(migrate)
+		.addCommand(generate)
+		.addCommand(generateSecret)
+		.addCommand(init)
+		.version(packageInfo.version || "1.1.2")
+		.description("Better Auth CLI");
+	program.commands.forEach((cmd) => {
+		cmd.opts().config = config;
+	});
+	program.parse();
 }
 
 main();
