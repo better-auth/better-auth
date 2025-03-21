490fa0eda646a110991b56cb0c05a01460321ef2
Bereket Engida <bekacru@gmail.com>
2025-03-21 12:54:23 +0300

    add animation and change logo icon

--- docs/components/logo-context-menu.tsx
+++ docs/components/logo-context-menu.tsx
@@ -2,7 +2,7 @@
 
 import type React from "react";
 import { useState, useRef, useEffect } from "react";
-import { Code, Image, Type, Shapes } from "lucide-react";
+import { Code, Image, Type } from "lucide-react";
 import { toast } from "sonner";
 import { useTheme } from "next-themes";
 import type { StaticImageData } from "next/image";
@@ -26,10 +26,6 @@ export default function LogoContextMenu({
 	logoAssets,
 }: ContextMenuProps) {
 	const [showMenu, setShowMenu] = useState<boolean>(false);
-	const [position, setPosition] = useState<{ x: number; y: number }>({
-		x: 0,
-		y: 0,
-	});
 	const menuRef = useRef<HTMLDivElement>(null);
 	const logoRef = useRef<HTMLDivElement>(null);
 	const { theme } = useTheme();
@@ -38,10 +34,6 @@ export default function LogoContextMenu({
 		e.preventDefault();
 		const rect = logoRef.current?.getBoundingClientRect();
 		if (rect) {
-			setPosition({
-				x: e.clientX,
-				y: e.clientY,
-			});
 			setShowMenu(true);
 		}
 	};
@@ -118,16 +110,10 @@ export default function LogoContextMenu({
 			{showMenu && (
 				<div
 					ref={menuRef}
-					className="fixed ml-20 z-50 bg-white dark:bg-black border border-gray-200 dark:border-border p-1 rounded-sm shadow-xl w-56 overflow-hidden"
-					style={{
-						top: `${position.y}px`,
-						left: `${position.x}px`,
-						transform: "translate(-50%, 10px)",
-					}}
+					className="fixed mx-10 z-50 bg-white dark:bg-black border border-gray-200 dark:border-border p-1 rounded-sm shadow-xl w-56 overflow-hidden animate-fd-dialog-in duration-500"
 				>
 					<div className="">
 						<div className="flex p-0 gap-1 flex-col text-xs">
-							<hr className="border-border/[60%] mt-3" />
 							<button
 								onClick={() =>
 									copySvgToClipboard(
@@ -135,7 +121,7 @@ export default function LogoContextMenu({
 										"Logo SVG",
 									)
 								}
-								className="flex items-center gap-3 w-full p-2 text-black dark:text-white hover:bg-gray-100 dark:hover:bg-zinc-900 rounded-md transition-colors"
+								className="flex items-center gap-3 w-full p-2 text-black dark:text-white hover:bg-gray-100 dark:hover:bg-zinc-900 rounded-md transition-colors cursor-pointer"
 							>
 								<div className="flex items-center">
 									<span className="text-gray-400 dark:text-zinc-400/30">[</span>
@@ -153,7 +139,7 @@ export default function LogoContextMenu({
 										"Logo Wordmark",
 									)
 								}
-								className="flex items-center gap-3 w-full p-2 text-black dark:text-white hover:bg-gray-100 dark:hover:bg-zinc-900 rounded-md transition-colors"
+								className="flex items-center gap-3 w-full p-2 text-black dark:text-white hover:bg-gray-100 dark:hover:bg-zinc-900 rounded-md transition-colors cursor-pointer"
 							>
 								<div className="flex items-center">
 									<span className="text-gray-400 dark:text-zinc-400/30">[</span>
@@ -172,7 +158,7 @@ export default function LogoContextMenu({
 										`better-auth-logo-${theme}.png`,
 									)
 								}
-								className="flex items-center gap-3 w-full p-2 text-black dark:text-white hover:bg-gray-100 dark:hover:bg-zinc-900 rounded-md transition-colors"
+								className="flex items-center gap-3 w-full p-2 text-black dark:text-white hover:bg-gray-100 dark:hover:bg-zinc-900 rounded-md transition-colors cursor-pointer"
 							>
 								<div className="flex items-center">
 									<span className="text-gray-400 dark:text-zinc-400/30">[</span>
@@ -185,12 +171,27 @@ export default function LogoContextMenu({
 							<hr className="borde-border" />
 							<button
 								onClick={downloadAllAssets}
-								className="flex items-center gap-3 w-full p-2 text-black dark:text-white hover:bg-gray-100 dark:hover:bg-zinc-900 rounded-md transition-colors"
+								className="flex items-center gap-3 w-full p-2 text-black dark:text-white hover:bg-gray-100 dark:hover:bg-zinc-900 rounded-md transition-colors cursor-pointer"
 							>
 								<div className="flex items-center">
 									<span className="text-gray-400 dark:text-zinc-400/30">[</span>
 
-									<Shapes className="h-[13.8px] w-[13.8px] mx-[3px]" />
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										width="1em"
+										height="1em"
+										viewBox="0 0 24 24"
+										className="h-[13.8px] w-[13.8px] mx-[3px]"
+									>
+										<path
+											fill="none"
+											stroke="currentColor"
+											stroke-linecap="round"
+											stroke-linejoin="round"
+											stroke-width="2"
+											d="M4 8v8.8c0 1.12 0 1.68.218 2.108a2 2 0 0 0 .874.874c.427.218.987.218 2.105.218h9.606c1.118 0 1.677 0 2.104-.218c.377-.192.683-.498.875-.874c.218-.428.218-.987.218-2.105V8M4 8h16M4 8l1.365-2.39c.335-.585.503-.878.738-1.092c.209-.189.456-.332.723-.42C7.13 4 7.466 4 8.143 4h7.714c.676 0 1.015 0 1.318.099c.267.087.513.23.721.42c.236.213.404.506.74 1.093L20 8m-8 3v6m0 0l3-2m-3 2l-3-2"
+										></path>
+									</svg>
 									<span className="text-gray-400 dark:text-zinc-400/30">]</span>
 								</div>
 								<span>Brand Assets</span>
