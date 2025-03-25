72d422378f5eb829f406554423c5feff8cb1deb3
Bereket Engida <bekacru@gmail.com>
2025-03-25 18:00:20 +0300

    chore: fix base url docs

--- packages/better-auth/src/init.ts
+++ packages/better-auth/src/init.ts
@@ -154,11 +154,6 @@ export const init = async (options: BetterAuthOptions) => {
 	};
 	let { context } = runPluginInit(ctx);
 	context;
-	if (!context.baseURL || !context.options.baseURL) {
-		throw new BetterAuthError(
-			"Base URL is not provided. Please set `BETTER_AUTH_URL` in your environment variables or pass `baseURL` in your auth config.",
-		);
-	}
 	return context;
 };
 
