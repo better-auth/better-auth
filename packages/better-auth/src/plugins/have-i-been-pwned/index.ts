d32ccdca2bbf7d3305e2ec36b52563930e9b0870
John Tanzer <82201261+moshetanzer@users.noreply.github.com>
2025-03-25 14:55:51 +0200

    fix: small fixes import and safe options

--- packages/better-auth/src/plugins/have-i-been-pwned/index.ts
+++ packages/better-auth/src/plugins/have-i-been-pwned/index.ts
@@ -1,14 +1,14 @@
 import type { BetterAuthPlugin } from "better-auth";
-import { APIError } from "better-auth/api";
+import { APIError } from "../../api";
 import { createHash } from "@better-auth/utils/hash"
 import { betterFetch } from '@better-fetch/fetch';
 
 async function checkPasswordCompromise(password: string, customMessage?: string) {
     if (!password) return
+
     const sha1Hash = (await createHash('SHA-1', 'hex').digest(password)).toUpperCase()
     const prefix = sha1Hash.substring(0, 5)
     const suffix = sha1Hash.substring(5)
-  
     try {
         const {data, error} = await betterFetch<string>(`https://api.pwnedpasswords.com/range/${prefix}`, {
             headers: {
@@ -16,7 +16,7 @@ async function checkPasswordCompromise(password: string, customMessage?: string)
                 'User-Agent': 'BetterAuth Password Checker' 
             }
         })
-   
+
         if (error) {
             throw new APIError('INTERNAL_SERVER_ERROR', { 
                 message: `Failed to check password. Status: ${error.status}` 
@@ -40,6 +40,7 @@ async function checkPasswordCompromise(password: string, customMessage?: string)
         })
     }
 }
+
 export interface HaveIBeenPwnedOptions {
     customPasswordCompromisedMessage?: string
 }
@@ -54,7 +55,7 @@ export const haveIBeenPwned = (options?: HaveIBeenPwnedOptions) =>
                 async before(user, ctx) {
                     if(ctx && ctx.body){
                         const password = ctx.body.newPassword || ctx.body.password
-                        await checkPasswordCompromise(password, options.customPasswordCompromisedMessage)
+                        await checkPasswordCompromise(password, options?.customPasswordCompromisedMessage)
                     }
                 }
               },
@@ -62,7 +63,7 @@ export const haveIBeenPwned = (options?: HaveIBeenPwnedOptions) =>
                 async before(user, ctx) {
                     if(ctx && ctx.body){
                     const password = ctx.body.newPassword || ctx.body.password
-                    await checkPasswordCompromise(password, options.customPasswordCompromisedMessage)
+                    await checkPasswordCompromise(password, options?.customPasswordCompromisedMessage)
                 }
                 }
               }
