06193a3b63eec553b53505d15542f3e61b209ddf
KinfeMichael Tariku <65047246+Kinfe123@users.noreply.github.com>
2025-03-26 23:51:33 +0300

    feat: refresh token endpoint (#1779)

--- packages/better-auth/src/social-providers/apple.ts
+++ packages/better-auth/src/social-providers/apple.ts
@@ -2,7 +2,11 @@ import { betterFetch } from "@better-fetch/fetch";
 import { APIError } from "better-call";
 import { decodeJwt, decodeProtectedHeader, importJWK, jwtVerify } from "jose";
 import type { OAuthProvider, ProviderOptions } from "../oauth2";
-import { createAuthorizationURL, validateAuthorizationCode } from "../oauth2";
+import {
+	refreshAccessToken,
+	createAuthorizationURL,
+	validateAuthorizationCode,
+} from "../oauth2";
 export interface AppleProfile {
 	/**
 	 * The subject registered claim identifies the principal that’s the subject
@@ -124,6 +128,19 @@ export const apple = (options: AppleOptions) => {
 			}
 			return !!jwtClaims;
 		},
+		refreshAccessToken: options.refreshAccessToken
+			? options.refreshAccessToken
+			: async (refreshToken) => {
+					return refreshAccessToken({
+						refreshToken,
+						options: {
+							clientId: options.clientId,
+							clientKey: options.clientKey,
+							clientSecret: options.clientSecret,
+						},
+						tokenEndpoint: "https://appleid.apple.com/auth/token",
+					});
+				},
 		async getUserInfo(token) {
 			if (options.getUserInfo) {
 				return options.getUserInfo(token);
