b7aceb501afc1218e9ec80c7d20da25d0a933b7d
ping-maxwell <maxwell.multinite@gmail.com>
2025-03-30 16:33:05 +1000

    fix: Prisma adapter tests running one after another

--- packages/better-auth/src/adapters/prisma-adapter/test/normal-tests/adapter.prisma.test.ts
+++ packages/better-auth/src/adapters/prisma-adapter/test/normal-tests/adapter.prisma.test.ts
@@ -2,14 +2,20 @@ import { beforeAll, describe } from "vitest";
 import { pushPrismaSchema } from "../push-schema";
 import { createTestOptions } from "../test-options";
 import { runAdapterTest } from "../../../test";
+import { setState } from "../state";
 
 describe("Adapter tests", async () => {
 	beforeAll(async () => {
+		setState("RUNNING");
 		await pushPrismaSchema("normal");
 		console.log("Successfully pushed normal Prisma Schema using pnpm...");
 		const { getAdapter } = await import("./get-adapter");
 		const { clearDb } = getAdapter();
 		await clearDb();
+		return () => {
+			console.log(`Normal Prisma adapter test finished. Now allowing number ID prisma tests to run.`);
+			setState("IDLE");
+		}
 	});
 
 	await runAdapterTest({
--- packages/better-auth/src/adapters/prisma-adapter/test/number-id-tests/adapter.prisma.numberid.test.ts
+++ packages/better-auth/src/adapters/prisma-adapter/test/number-id-tests/adapter.prisma.numberid.test.ts
@@ -2,14 +2,30 @@ import { beforeAll, describe } from "vitest";
 import { runNumberIdAdapterTest } from "../../../test";
 import { pushPrismaSchema } from "../push-schema";
 import { createTestOptions } from "../test-options";
+import * as fs from "node:fs";
+import { getState, stateFilePath } from "../state";
+
 
 describe("Number Id Adapter Test", async () => {
 	beforeAll(async () => {
+		await new Promise((resolve) => {
+			if (getState() === "IDLE") {
+				resolve(true);
+				return;
+			}
+			fs.watch(stateFilePath, () => {
+				if (getState() === "IDLE") {
+					resolve(true);
+					return;
+				}
+			});
+		});
+		console.log(`Now running Number ID Prisma adapter test...`);
 		await pushPrismaSchema("number-id");
 		const { getAdapter } = await import("./get-adapter");
 		const { clearDb } = getAdapter();
 		await clearDb();
-	});
+	}, Number.POSITIVE_INFINITY);
 
 	await runNumberIdAdapterTest({
 		getAdapter: async (customOptions = {}) => {
