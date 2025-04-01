a47e3036b6355f63c67ecc0f0187b4885618e432
Thalles Passos <contato@thalles.me>
2025-04-01 07:59:14 -0300

    fix: AuthContext is exported from better-auth instead of /nestjs

--- examples/nestjs-example/src/hooks/before-signup.hook.ts
+++ examples/nestjs-example/src/hooks/before-signup.hook.ts
@@ -1,9 +1,10 @@
+import { AuthContext } from "better-auth";
 import { BeforeHook, Hook } from "better-auth/nestjs";
 
 @Hook()
 export class BeforeSignupHook {
 	@BeforeHook("/sign-up/email")
-	async beforeSignup() {
+	async beforeSignup(ctx: AuthContext) {
 		console.log("before signup");
 	}
 }
