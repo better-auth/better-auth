8bc57df407e74fb2e11de7f59f0972f24d888b33
Thomas Mol <email@thomasmol.com>
2025-04-03 20:25:32 +0200

    lint

--- packages/better-auth/src/plugins/api-key/index.ts
+++ packages/better-auth/src/plugins/api-key/index.ts
@@ -219,94 +219,93 @@ export const apiKey = (options?: ApiKeyOptions) => {
 			],
 		},
 		endpoints: {
-			createApiKey: createAuthEndpoint(
-				"/api-key/create",
-				{
-					method: "POST",
-					body: z.object({
-						name: z.string({ description: "Name of the Api Key" }).optional(),
-						expiresIn: z
-							.number({
-								description: "Expiration time of the Api Key in seconds",
-							})
-							.min(1)
-							.optional()
-							.nullable()
-							.default(null),
+			createApiKey: createAuthEndpoint("/api-key/create", {
+				method: "POST",
+				body: z.object({
+					name: z.string({ description: "Name of the Api Key" }).optional(),
+					expiresIn: z
+						.number({
+							description: "Expiration time of the Api Key in seconds",
+						})
+						.min(1)
+						.optional()
+						.nullable()
+						.default(null),
 
-						userId: z.coerce
-							.string({
-								description:
-									"User Id of the user that the Api Key belongs to. Useful for server-side only.",
-							})
-							.optional(),
-						prefix: z
-							.string({ description: "Prefix of the Api Key" })
-							.regex(/^[a-zA-Z0-9_-]+$/, {
-								message:
-									"Invalid prefix format, must be alphanumeric and contain only underscores and hyphens.",
-							})
-							.optional(),
-						remaining: z
-							.number({
-								description: "Remaining number of requests. Server side only",
-							})
-							.min(0)
-							.optional()
-							.nullable()
-							.default(null),
-						metadata: z.any({ description: "Metadata of the Api Key" }).optional(),
-						refillAmount: z
-							.number({
-								description:
-									"Amount to refill the remaining count of the Api Key. Server Only Property",
-							})
-							.min(1)
-							.optional(),
-						refillInterval: z
-							.number({
-								description:
-									"Interval to refill the Api Key in milliseconds. Server Only Property.",
-							})
-							.optional(),
-						rateLimitTimeWindow: z
-							.number({
-								description:
-									"The duration in milliseconds where each request is counted. Once the `maxRequests` is reached, the request will be rejected until the `timeWindow` has passed, at which point the `timeWindow` will be reset. Server Only Property.",
-							})
-							.optional(),
-						rateLimitMax: z
-							.number({
-								description:
-									"Maximum amount of requests allowed within a window. Once the `maxRequests` is reached, the request will be rejected until the `timeWindow` has passed, at which point the `timeWindow` will be reset. Server Only Property.",
-							})
-							.optional(),
-						rateLimitEnabled: z
-							.boolean({
-								description:
-									"Whether the key has rate limiting enabled. Server Only Property.",
-							})
-							.optional(),
-						permissions: z.record(z.string(), z.array(z.string())).optional(),
-					}),
-					metadata: {
-						openapi: {
-							operationId: "apiKeyCreate",
-							description: "Create an API key",
-							responses: {
-								200: {
-									description: "Success",
-									content: {
-										"application/json": {
-											schema: {
-												type: "object",
-												properties: {
-													id: {
-														type: "string",
-													},
-													key: {
-														type: "string",
-													},
+					userId: z.coerce
+						.string({
+							description:
+								"User Id of the user that the Api Key belongs to. Useful for server-side only.",
+						})
+						.optional(),
+					prefix: z
+						.string({ description: "Prefix of the Api Key" })
+						.regex(/^[a-zA-Z0-9_-]+$/, {
+							message:
+								"Invalid prefix format, must be alphanumeric and contain only underscores and hyphens.",
+						})
+						.optional(),
+					remaining: z
+						.number({
+							description: "Remaining number of requests. Server side only",
+						})
+						.min(0)
+						.optional()
+						.nullable()
+						.default(null),
+					metadata: z
+						.any({ description: "Metadata of the Api Key" })
+						.optional(),
+					refillAmount: z
+						.number({
+							description:
+								"Amount to refill the remaining count of the Api Key. Server Only Property",
+						})
+						.min(1)
+						.optional(),
+					refillInterval: z
+						.number({
+							description:
+								"Interval to refill the Api Key in milliseconds. Server Only Property.",
+						})
+						.optional(),
+					rateLimitTimeWindow: z
+						.number({
+							description:
+								"The duration in milliseconds where each request is counted. Once the `maxRequests` is reached, the request will be rejected until the `timeWindow` has passed, at which point the `timeWindow` will be reset. Server Only Property.",
+						})
+						.optional(),
+					rateLimitMax: z
+						.number({
+							description:
+								"Maximum amount of requests allowed within a window. Once the `maxRequests` is reached, the request will be rejected until the `timeWindow` has passed, at which point the `timeWindow` will be reset. Server Only Property.",
+						})
+						.optional(),
+					rateLimitEnabled: z
+						.boolean({
+							description:
+								"Whether the key has rate limiting enabled. Server Only Property.",
+						})
+						.optional(),
+					permissions: z.record(z.string(), z.array(z.string())).optional(),
+				}),
+				metadata: {
+					openapi: {
+						operationId: "apiKeyCreate",
+						description: "Create an API key",
+						responses: {
+							200: {
+								description: "Success",
+								content: {
+									"application/json": {
+										schema: {
+											type: "object",
+											properties: {
+												id: {
+													type: "string",
+												},
+												key: {
+													type: "string",
 												},
 											},
 										},
@@ -316,187 +315,43 @@ export const apiKey = (options?: ApiKeyOptions) => {
 						},
 					},
 				},
-			),
-			verifyApiKey: createAuthEndpoint(
-				"/api-key/verify",
-				{
-					method: "POST",
-					body: z.object({
-						key: z.string({
-							description: "The key to verify",
-						}),
-						permissions: z.record(z.string(), z.array(z.string())).optional(),
+			}),
+			verifyApiKey: createAuthEndpoint("/api-key/verify", {
+				method: "POST",
+				body: z.object({
+					key: z.string({
+						description: "The key to verify",
 					}),
-					metadata: {
-						SERVER_ONLY: true,
-						openapi: {
-							operationId: "apiKeyVerify",
-							description: "Verify an API key",
-							responses: {
-								200: {
-									description: "Success",
-									content: {
-										"application/json": {
-											schema: {
-												type: "object",
-												properties: {
-													valid: {
-														type: "boolean",
-													},
-													error: {
-														type: "object",
-														properties: {
-															message: {
-																type: "string",
-															},
-															code: {
-																type: "string",
-															},
+					permissions: z.record(z.string(), z.array(z.string())).optional(),
+				}),
+				metadata: {
+					SERVER_ONLY: true,
+					openapi: {
+						operationId: "apiKeyVerify",
+						description: "Verify an API key",
+						responses: {
+							200: {
+								description: "Success",
+								content: {
+									"application/json": {
+										schema: {
+											type: "object",
+											properties: {
+												valid: {
+													type: "boolean",
+												},
+												error: {
+													type: "object",
+													properties: {
+														message: {
+															type: "string",
 														},
-													},
-													key: {
-														type: "object",
-														properties: {
-															id: {
-																type: "string",
-															},
+														code: {
+															type: "string",
 														},
 													},
 												},
-											},
-										},
-									},
-								},
-							},
-						},
-					},
-				},
-			),
-			getApiKey: createAuthEndpoint(
-				"/api-key/get",
-				{
-					method: "GET",
-					query: z.object({
-						id: z.string({
-							description: "The id of the Api Key",
-						}),
-					}),
-					use: [sessionMiddleware],
-					metadata: {
-						openapi: {
-							operationId: "apiKeyGet",
-							description: "Get an API key",
-							responses: {
-								200: {
-									description: "Success",
-									content: {
-										"application/json": {
-											schema: {
-												type: "object",
-												properties: {
-													id: {
-														type: "string",
-													},
-												},
-											},
-										},
-									},
-								},
-							},
-						},
-					},
-				},
-			),
-			updateApiKey: createAuthEndpoint(
-				"/api-key/update",
-				{
-					method: "POST",
-					body: z.object({
-						id: z.string({
-							description: "The id of the Api Key",
-						}),
-						name: z.string({ description: "Name of the Api Key" }).optional(),
-						enabled: z.boolean({ description: "Enable/Disable the Api Key" }).optional(),
-						metadata: z.any({ description: "Metadata of the Api Key" }).optional(),
-					}),
-					use: [sessionMiddleware],
-					metadata: {
-						openapi: {
-							operationId: "apiKeyUpdate",
-							description: "Update an API key",
-							responses: {
-								200: {
-									description: "Success",
-									content: {
-										"application/json": {
-											schema: {
-												type: "object",
-												properties: {
-													id: {
-														type: "string",
-													},
-												},
-											},
-										},
-									},
-								},
-							},
-						},
-					},
-				},
-			),
-			deleteApiKey: createAuthEndpoint(
-				"/api-key/delete",
-				{
-					method: "POST",
-					body: z.object({
-						id: z.string({
-							description: "The id of the Api Key",
-						}),
-					}),
-					use: [sessionMiddleware],
-					metadata: {
-						openapi: {
-							operationId: "apiKeyDelete",
-							description: "Delete an API key",
-							responses: {
-								200: {
-									description: "Success",
-									content: {
-										"application/json": {
-											schema: {
-												type: "object",
-												properties: {
-													success: {
-														type: "boolean",
-													},
-												},
-											},
-										},
-									},
-								},
-							},
-						},
-					},
-				},
-			),
-			listApiKeys: createAuthEndpoint(
-				"/api-key/list",
-				{
-					method: "GET",
-					use: [sessionMiddleware],
-					metadata: {
-						openapi: {
-							operationId: "apiKeyList",
-							description: "List API keys",
-							responses: {
-								200: {
-									description: "Success",
-									content: {
-										"application/json": {
-											schema: {
-												type: "array",
-												items: {
+												key: {
 													type: "object",
 													properties: {
 														id: {
@@ -512,7 +367,140 @@ export const apiKey = (options?: ApiKeyOptions) => {
 						},
 					},
 				},
-			),
+			}),
+			getApiKey: createAuthEndpoint("/api-key/get", {
+				method: "GET",
+				query: z.object({
+					id: z.string({
+						description: "The id of the Api Key",
+					}),
+				}),
+				use: [sessionMiddleware],
+				metadata: {
+					openapi: {
+						operationId: "apiKeyGet",
+						description: "Get an API key",
+						responses: {
+							200: {
+								description: "Success",
+								content: {
+									"application/json": {
+										schema: {
+											type: "object",
+											properties: {
+												id: {
+													type: "string",
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			}),
+			updateApiKey: createAuthEndpoint("/api-key/update", {
+				method: "POST",
+				body: z.object({
+					id: z.string({
+						description: "The id of the Api Key",
+					}),
+					name: z.string({ description: "Name of the Api Key" }).optional(),
+					enabled: z
+						.boolean({ description: "Enable/Disable the Api Key" })
+						.optional(),
+					metadata: z
+						.any({ description: "Metadata of the Api Key" })
+						.optional(),
+				}),
+				use: [sessionMiddleware],
+				metadata: {
+					openapi: {
+						operationId: "apiKeyUpdate",
+						description: "Update an API key",
+						responses: {
+							200: {
+								description: "Success",
+								content: {
+									"application/json": {
+										schema: {
+											type: "object",
+											properties: {
+												id: {
+													type: "string",
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			}),
+			deleteApiKey: createAuthEndpoint("/api-key/delete", {
+				method: "POST",
+				body: z.object({
+					id: z.string({
+						description: "The id of the Api Key",
+					}),
+				}),
+				use: [sessionMiddleware],
+				metadata: {
+					openapi: {
+						operationId: "apiKeyDelete",
+						description: "Delete an API key",
+						responses: {
+							200: {
+								description: "Success",
+								content: {
+									"application/json": {
+										schema: {
+											type: "object",
+											properties: {
+												success: {
+													type: "boolean",
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			}),
+			listApiKeys: createAuthEndpoint("/api-key/list", {
+				method: "GET",
+				use: [sessionMiddleware],
+				metadata: {
+					openapi: {
+						operationId: "apiKeyList",
+						description: "List API keys",
+						responses: {
+							200: {
+								description: "Success",
+								content: {
+									"application/json": {
+										schema: {
+											type: "array",
+											items: {
+												type: "object",
+												properties: {
+													id: {
+														type: "string",
+													},
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			}),
 			deleteAllExpiredApiKeys: createAuthEndpoint(
 				"/api-key/delete-all-expired-api-keys",
 				{
