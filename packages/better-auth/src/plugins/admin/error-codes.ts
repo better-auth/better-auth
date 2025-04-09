1b502f4c35fb18ac2b37f91545101b01cba296e2
Zastinian <zastinian@gmail.com>
2025-04-08 19:49:23 -0600

    feat: Added admin error codes

--- packages/better-auth/src/plugins/admin/error-codes.ts
+++ packages/better-auth/src/plugins/admin/error-codes.ts
@@ -1,6 +1,9 @@
 export const ADMIN_ERROR_CODES = {
 	FAILED_TO_CREATE_USER: "Failed to create user",
 	USER_ALREADY_EXISTS: "User already exists",
+	YOU_ARE_NOT_ALLOWED_TO_LIST_ROLES: "You are not allowed to list roles",
+	YOU_ARE_NOT_ALLOWED_TO_VIEW_ROLE_PERMISSIONS:
+		"You are not allowed to view role permissions",
 	YOU_CANNOT_BAN_YOURSELF: "You cannot ban yourself",
 	YOU_ARE_NOT_ALLOWED_TO_CHANGE_USERS_ROLE:
 		"You are not allowed to change users role",
@@ -17,4 +20,6 @@ export const ADMIN_ERROR_CODES = {
 	YOU_ARE_NOT_ALLOWED_TO_SET_USERS_PASSWORD:
 		"You are not allowed to set users password",
 	BANNED_USER: "You have been banned from this application",
+	ROLE_NOT_PROVIDED: "Role not provided",
+	ROLE_NOT_FOUND: "Role not found",
 } as const;
