9bc60559979bd1d248459d476b7ba197d9335239
Kyle Roach <kroach.work@gmail.com>
2025-04-17 21:48:23 -0400

    feat(organization): support createdAt on invitations
    Adds createdAt attribute to invitation schema.

--- packages/better-auth/src/plugins/organization/routes/crud-invites.ts
+++ packages/better-auth/src/plugins/organization/routes/crud-invites.ts
@@ -113,6 +113,9 @@ export const createInvitation = <O extends OrganizationOptions | undefined>(
 											expiresAt: {
 												type: "string",
 											},
+											createdAt: {
+												type: "string",
+											}
 										},
 										required: [
 											"id",
@@ -122,6 +125,7 @@ export const createInvitation = <O extends OrganizationOptions | undefined>(
 											"inviterId",
 											"status",
 											"expiresAt",
+											"createdAt"
 										],
 									},
 								},
@@ -550,6 +554,9 @@ export const getInvitation = createAuthEndpoint(
 										expiresAt: {
 											type: "string",
 										},
+										createdAt: {
+											type: "string",
+										},
 										organizationName: {
 											type: "string",
 										},
@@ -568,6 +575,7 @@ export const getInvitation = createAuthEndpoint(
 										"inviterId",
 										"status",
 										"expiresAt",
+										"createAt",
 										"organizationName",
 										"organizationSlug",
 										"inviterEmail",
