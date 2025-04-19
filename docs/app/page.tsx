793ced0e7a3b453218145e664a8b963783bb0c87
Bereket Engida <bekacru@gmail.com>
2025-04-19 14:08:50 +0300

    chore: add infra announcement banner

--- docs/app/page.tsx
+++ docs/app/page.tsx
@@ -1,7 +1,7 @@
 import Section from "@/components/landing/section";
 import Hero from "@/components/landing/hero";
 import Features from "@/components/features";
-
+import Link from "next/link";
 async function getGitHubStars() {
 	try {
 		const response = await fetch(
@@ -27,6 +27,21 @@ export default async function HomePage() {
 	const stars = await getGitHubStars();
 	return (
 		<main className="h-min mx-auto overflow-x-hidden">
+			<div className="w-full bg-gradient-to-br border-b dark:border-zinc-800 border-zinc-200 dark:from-zinc-900 dark:to-zinc-950 from-zinc-150 to-zinc-100 dark:text-white text-center py-2">
+				<p className="text-sm px-4">
+					🎉 Introducing{" "}
+					<Link
+						target="_blank"
+						href="https://better-auth.build"
+						className="text-blue-500 hover:text-blue-600 underline"
+					>
+						Infrastructure
+					</Link>{" "}
+					| Join the <span className="font-bold">waitlist</span> to our infra
+					layer that provides all the missing pieces you need to own your auth
+					with confidence!
+				</p>
+			</div>
 			<Section
 				className="mb-1 overflow-y-clip"
 				crosses
