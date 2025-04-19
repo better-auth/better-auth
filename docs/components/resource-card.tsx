3d8742915acf1447d7741768b1158afbec5159da
Kinfe123 <kinfishtech@gmail.com>
2025-04-19 06:15:33 +0300

    alignment

--- docs/components/resource-card.tsx
+++ docs/components/resource-card.tsx
@@ -17,35 +17,41 @@ export function ResourceCard({
 	className,
 }: ResourceCardProps) {
 	return (
-		<p
+		<div
 			rel="noopener noreferrer"
 			className={cn(
-				"group relative flex rounded-none flex-col group space-y-1 border transition-colors hover:bg-muted/80",
+				"group relative flex justify-between rounded-none flex-col group space-y-1 border transition-colors hover:bg-muted/80",
 				className,
 			)}
 		>
-			<ArrowUpRight className="absolute top-3 right-3 h-4 w-4 group-hover:opacity-100 opacity-80 text-muted-foreground transition-colors group-hover:text-foreground no-underline underline-offset-0" />
-			<div className="p-4 py-0 flex items-start justify-between">
-				<a href={href} target="_blank" rel="noopener noreferrer">
-					<h3 className="font-semibold tracking-tight no-underline">{title}</h3>
-				</a>
-			</div>
-			<p
-				dangerouslySetInnerHTML={{ __html: `${description}` }}
-				className="p-4 text-sm md:decoration-none text-muted-foreground"
-			></p>
-			{tags && tags.length > 0 && (
-				<div className="py-3 border-zinc-700/80 border-t-[1.2px] flex flex-wrap items-end gap-2">
-					{tags.map((tag) => (
-						<span
-							key={tag}
-							className="inline-flex items-end underline underline-offset-2 rounded-md bg-secondary/10 px-2 py-1 text-xs font-medium text-secondary-foreground"
-						>
-							{tag}
-						</span>
-					))}
+			<div>
+				<ArrowUpRight className="absolute top-3 right-3 h-4 w-4 group-hover:opacity-100 opacity-80 text-muted-foreground transition-colors group-hover:text-foreground no-underline underline-offset-0" />
+				<div className="p-4 py-0 flex items-start justify-between">
+					<a href={href} target="_blank" rel="noopener noreferrer">
+						<h3 className="font-semibold tracking-tight no-underline">
+							{title}
+						</h3>
+					</a>
 				</div>
-			)}
-		</p>
+				<p
+					dangerouslySetInnerHTML={{ __html: `${description}` }}
+					className="p-4 text-sm md:decoration-none text-muted-foreground"
+				></p>
+			</div>
+			<div>
+				{tags && tags.length > 0 && (
+					<div className="py-3 border-zinc-700/80 border-t-[1.2px] flex flex-wrap items-end gap-2">
+						{tags.map((tag) => (
+							<span
+								key={tag}
+								className="inline-flex items-end underline underline-offset-2 rounded-md bg-secondary/10 px-2 py-1 text-xs font-medium text-secondary-foreground"
+							>
+								{tag}
+							</span>
+						))}
+					</div>
+				)}
+			</div>
+		</div>
 	);
 }
