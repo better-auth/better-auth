a49920a97ebb1bcfb91d7aa5b4ce931a9582de5b
Anthony DePasquale <anthony@depasquale.org>
2025-05-02 17:52:07 +0200

    Fix SvelteKit example

--- examples/svelte-kit-example/src/lib/components/ui/button/button.svelte
+++ examples/svelte-kit-example/src/lib/components/ui/button/button.svelte
@@ -1,14 +1,15 @@
 <script lang="ts">
-import { type Events, type Props } from "./index.js";
+	import { Button as ButtonPrimitive } from 'bits-ui';
+	import { cn } from '$lib/utils';
+	import { type Props, buttonVariants } from './index.js';
 
-type $$Props = Props;
-type $$Events = Events;
+	type $$Props = Props;
 
-let className: $$Props["class"] = undefined;
-export let variant: $$Props["variant"] = "default";
-export let size: $$Props["size"] = "default";
-export let builders: $$Props["builders"] = [];
-export { className as class };
+	let className: $$Props['class'] = undefined;
+	export let variant: $$Props['variant'] = 'default';
+	export let size: $$Props['size'] = 'default';
+	export let builders: $$Props['builders'] = [];
+	export { className as class };
 </script>
 
 <ButtonPrimitive.Root
--- examples/svelte-kit-example/src/lib/components/ui/input/index.ts
+++ examples/svelte-kit-example/src/lib/components/ui/input/index.ts
@@ -1,4 +1,4 @@
-import Root from "./input.svelte";
+import Root from './input.svelte';
 
 export type FormInputEvent<T extends Event = Event> = T & {
 	currentTarget: EventTarget & HTMLInputElement;
