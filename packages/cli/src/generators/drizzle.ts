514da684b6aedc3d7599bfec8495e22ac4965177
Bereket Engida <bekacru@gmail.com>
2025-05-05 13:07:52 -0700

    feat(drizzle): support camel case schema generation

--- packages/cli/src/generators/drizzle.ts
+++ packages/cli/src/generators/drizzle.ts
@@ -6,7 +6,10 @@ import {
 import { existsSync } from "fs";
 import type { SchemaGenerator } from "./types";
 
-export function convertToSnakeCase(str: string) {
+export function convertToSnakeCase(str: string, camelCase?: boolean) {
+	if (camelCase) {
+		return str;
+	}
 	return str.replace(/[A-Z]/g, (letter) => `_${letter.toLowerCase()}`);
 }
 
@@ -41,8 +44,7 @@ export const generateDrizzleSchema: SchemaGenerator = async ({
 					`Database provider type is undefined during Drizzle schema generation. Please define a \`provider\` in the Drizzle adapter config. Read more at https://better-auth.com/docs/adapters/drizzle`,
 				);
 			}
-			name = convertToSnakeCase(name);
-
+			name = convertToSnakeCase(name, adapter.options?.camelCase);
 			if (field.references?.field === "id") {
 				if (options.advanced?.database?.useNumberId) {
 					if (databaseType === "pg") {
@@ -130,6 +132,7 @@ export const generateDrizzleSchema: SchemaGenerator = async ({
 
 		const schema = `export const ${modelName} = ${databaseType}Table("${convertToSnakeCase(
 			modelName,
+			adapter.options?.camelCase,
 		)}", {
 					id: ${id},
 					${Object.keys(fields)
