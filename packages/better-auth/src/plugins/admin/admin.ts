bd93b0bfa57f8f463719bf8bbd334530bde0923a
Talha Mujahid <htmujahid@gmail.com>
2025-05-06 20:40:45 -0700

    Update admin.ts

--- packages/better-auth/src/plugins/admin/admin.ts
+++ packages/better-auth/src/plugins/admin/admin.ts
@@ -1308,8 +1308,14 @@ export const admin = <O extends AdminOptions>(options?: O) => {
 													success: {
 														type: "boolean",
 													},
+													user: {	
+														$ref: "#/components/schemas/User",
+													},
+													session: {	
+														$ref: "#/components/schemas/Session",
+													},
 												},
-												required: ["success"],
+												required: ["success", "user", "session"],
 											},
 										},
 									},
@@ -1361,6 +1367,8 @@ export const admin = <O extends AdminOptions>(options?: O) => {
 					return ctx.json({
 						error: null,
 						success: result,
+						user,
+						session: session.session,
 					});
 				},
 			),
