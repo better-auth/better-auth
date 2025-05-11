0f0e3d6c8e52601706561ebe1927f4a30dd63944
kylekz <kairu@team-reflex.com>
2025-05-11 18:20:33 +1200

    fix test

--- packages/better-auth/src/plugins/username/username.test.ts
+++ packages/better-auth/src/plugins/username/username.test.ts
@@ -4,7 +4,7 @@ import { username } from ".";
 import { usernameClient } from "./client";
 
 describe("username", async (it) => {
-	const { client, sessionSetter } = await getTestInstance(
+	const { client, sessionSetter, signInWithTestUser } = await getTestInstance(
 		{
 			plugins: [
 				username({
@@ -81,28 +81,30 @@ describe("username", async (it) => {
 	});
 
 	it("should fail on duplicate username in update-user if user is different", async () => {
+		const newHeaders = new Headers();
 		await client.signUp.email({
 			email: "new-email-2@gamil.com",
-			username: "new_username1",
+			username: "duplicate-username",
 			password: "new_password",
 			name: "new-name",
 			fetchOptions: {
-				headers: new Headers(),
+				headers: newHeaders,
 			},
 		});
 
+		const { headers: testUserHeaders } = await signInWithTestUser();
 		const res = await client.updateUser({
-			username: "new_username1",
+			username: "duplicate-username",
 			fetchOptions: {
-				headers,
+				headers: testUserHeaders,
 			},
 		});
 		expect(res.error?.status).toBe(422);
 	});
 
 	it("should succeed on duplicate username in update-user if user is the same", async () => {
-		const res = await client.updateUser({
-			username: "new_username",
+		await client.updateUser({
+			username: "New_username_2.1",
 			fetchOptions: {
 				headers,
 			},
@@ -114,7 +116,7 @@ describe("username", async (it) => {
 				throw: true,
 			},
 		});
-		expect(session?.user.username).toBe("new_username");
+		expect(session?.user.username).toBe("new_username_2.1");
 	});
 
 	it("should fail on invalid username", async () => {
