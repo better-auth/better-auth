better-auth/better-auth

commit af402dba3a0a00570a80b6401d6dde134ada1262

fix: add a default value for generated fields

packages/cli/src/generators/drizzle.ts
@@ -135,9 +135,17 @@ export const generateDrizzleSchema: SchemaGenerator = async ({
 					${Object.keys(fields)
 						.map((field) => {
 							const attr = fields[field]!;
-							return `${field}: ${getType(field, attr)}${
-								attr.required ? ".notNull()" : ""
-							}${attr.unique ? ".unique()" : ""}${
+							let type = getType(field, attr);
+							if (attr.defaultValue) {
+								if (typeof attr.defaultValue === "function") {
+									type += `.$defaultFn(${attr.defaultValue})`;
+								} else {
+									type += `.default(${attr.defaultValue})`;
+								}
+							}
+							return `${field}: ${type}${attr.required ? ".notNull()" : ""}${
+								attr.unique ? ".unique()" : ""
+							}${
 								attr.references
 									? `.references(()=> ${getModelName(
 											attr.references.model,

commit 9b9143f4153c0c38eadda4479c65aaba2a8e250b

fix: missing quotes for drizzle default values
When using the drizzle generator it doesn't include quotes for string default types, causing it to generate code that doesn't compile.
This regression was introduced with af402dba3a0a00570a80b6401d6dde134ada1262

packages/cli/src/generators/drizzle.ts
@@ -139,6 +139,8 @@ export const generateDrizzleSchema: SchemaGenerator = async ({
 							if (attr.defaultValue) {
 								if (typeof attr.defaultValue === "function") {
 									type += `.$defaultFn(${attr.defaultValue})`;
+								} else if (typeof attr.defaultValue === "string") {
+									type += `.default("${attr.defaultValue}")`;
 								} else {
 									type += `.default(${attr.defaultValue})`;
 								}