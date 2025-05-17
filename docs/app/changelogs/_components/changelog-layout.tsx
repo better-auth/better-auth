1118f3c656e610660e9cafc76d98c6b1c840ec5e
Rahul Mishra <blankparticle@gmail.com>
2025-05-17 17:39:10 +0530

    docs: fix discord invite links

--- docs/app/changelogs/_components/changelog-layout.tsx
+++ docs/app/changelogs/_components/changelog-layout.tsx
@@ -69,7 +69,7 @@ export function Intro() {
 					GitHub
 				</IconLink>
 				<IconLink
-					href="https://discord.com/better-auth"
+					href="https://discord.gg/better-auth"
 					icon={DiscordLogoIcon}
 					className="flex-none text-gray-600 dark:text-gray-300"
 				>
