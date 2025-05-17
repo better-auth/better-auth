631a4693efa81fb681668818d92ddd12f3c48377
Kinfe123 <kinfishtech@gmail.com>
2025-05-16 23:16:46 -0700

    import

--- dev/bun/auth.ts
+++ dev/bun/auth.ts
@@ -1,10 +1,15 @@
 import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
 import { twoFactor } from "better-auth/plugins";
-import Database from "bun:sqlite";
 
 export const auth = betterAuth({
 	baseURL: "http://localhost:4000",
-	database: new Database("./auth.db"),
+	database: prismaAdapter(
+		{},
+		{
+			provider: "sqlite",
+		},
+	),
 	emailAndPassword: {
 		enabled: true,
 	},
