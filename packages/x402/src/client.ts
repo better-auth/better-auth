6d5fab1cd927c9e873a9f20c4100a6e602efbcae
ping-maxwell <maxwell.multinite@gmail.com>
2025-05-19 15:25:19 +1000

    update: x402 client

--- packages/x402/src/client.ts
+++ packages/x402/src/client.ts
@@ -1,10 +1,68 @@
-import { betterFetch } from "@better-fetch/fetch";
+import { betterFetch, type RequestContext } from "@better-fetch/fetch";
 import type { BetterAuthClientPlugin } from "better-auth";
+import type { Account } from "viem";
+import { ChainIdToNetwork, PaymentRequirementsSchema } from "x402/types";
+import { evm } from "x402/types";
+import { createPaymentHeader, selectPaymentRequirements } from "x402/client";
+import type { PaymentRequirementsSelector } from "x402/client";
 
-//TODO: Make client
-export const x402Client = () => {
+export const x402Client = ({
+	walletClient,
+	onPayment
+}: {
+	walletClient: typeof evm.SignerWallet | Account;
+	onPayment?: (request: RequestContext<any>) => Promise<void>;
+}) => {
 	return {
 		id: "x402",
+		fetchPlugins: [
+			{
+				id: "x402",
+				name: "X402",
+				hooks: {
+					async onResponse(context) {
+						if (context.response.status !== 402) {
+							return context.response;
+						}
+
+						const { x402Version, accepts } =
+							(await context.response.json()) as {
+								x402Version: number;
+								accepts: unknown[];
+							};
+
+						const parsedPaymentRequirements = accepts.map((x) =>
+							PaymentRequirementsSchema.parse(x),
+						);
+
+						const chainId = evm.isSignerWallet(walletClient)
+							? walletClient.chain?.id
+							: evm.isAccount(walletClient)
+								? walletClient.client?.chain?.id
+								: undefined;
+						const selectedPaymentRequirements = selectPaymentRequirements(
+							parsedPaymentRequirements,
+							chainId ? ChainIdToNetwork[chainId] : undefined,
+							"exact",
+						);
+
+						const paymentHeader = await createPaymentHeader(
+							walletClient,
+							x402Version,
+							selectedPaymentRequirements,
+						  );
+
+						context.request.headers.set("X-PAYMENT", paymentHeader);
+						context.request.headers.set("Access-Control-Expose-Headers", "X-PAYMENT-RESPONSE");
+
+						// Some way to refetch the request
+
+						onPayment?.(context.request);
+						return fetch() // TODO: Refetch the request with the new payment header, then return the response.
+					},
+				},
+			},
+		],
 	} satisfies BetterAuthClientPlugin;
 };
 
