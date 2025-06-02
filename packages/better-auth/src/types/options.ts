5aa38a1bce3e360a0eafa8397c6672079f1d9ba2
KinfeMichael Tariku <65047246+Kinfe123@users.noreply.github.com>
2025-06-01 21:41:55 -0700

    chore: removed custom path options (#2865)
    * removed from options
    
    * removed from options
    
    * test
    
    * lint
    
    * test
    
    * lint

--- packages/better-auth/src/types/options.ts
+++ packages/better-auth/src/types/options.ts
@@ -999,10 +999,4 @@ export type BetterAuthOptions = {
 	 * Paths you want to disable.
 	 */
 	disabledPaths?: string[];
-	/**
-	 * Custom paths
-	 *
-	 * This is used to modify the underlying path of a route.
-	 */
-	customPaths?: Record<string, string>;
 };
