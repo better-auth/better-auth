db1c2b088299f6e39ca1e6fa5ebe48b1c4c2d44a
Kinfe123 <kinfishtech@gmail.com>
2025-06-05 16:43:55 -0700

    lint

--- packages/better-auth/src/client/fetch-plugins.ts
+++ packages/better-auth/src/client/fetch-plugins.ts
@@ -8,8 +8,8 @@ export const redirectPlugin = {
 			if (context.data?.url && context.data?.redirect) {
 				if (typeof window !== "undefined" && window.location) {
 					try {
-						if (context.data.target === '_blank') {
-							window.open(context.data.url, '_blank');
+						if (context.data.target === "_blank") {
+							window.open(context.data.url, "_blank");
 						} else {
 							window.location.href = context.data.url;
 						}
