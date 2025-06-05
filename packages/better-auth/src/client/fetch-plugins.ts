9fe3026dbe914046058f234cd634755762387299
Kinfe123 <kinfishtech@gmail.com>
2025-06-05 16:43:15 -0700

    feat: add target option here in fetch plugin

--- packages/better-auth/src/client/fetch-plugins.ts
+++ packages/better-auth/src/client/fetch-plugins.ts
@@ -7,11 +7,13 @@ export const redirectPlugin = {
 		onSuccess(context) {
 			if (context.data?.url && context.data?.redirect) {
 				if (typeof window !== "undefined" && window.location) {
-					if (window.location) {
-						try {
+					try {
+						if (context.data.target === '_blank') {
+							window.open(context.data.url, '_blank');
+						} else {
 							window.location.href = context.data.url;
-						} catch {}
-					}
+						}
+					} catch {}
 				}
 			}
 		},
