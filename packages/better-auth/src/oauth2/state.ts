69468d0341c0b46a9c77a577fb26593ba19e7b08
Bereket Engida <86073083+Bekacru@users.noreply.github.com>
2025-06-09 20:20:28 -0700

    feat: support passing error callback url for account linking (#2966)
    * feat: support passing error callback url for account linking
    
    * revert change

--- packages/better-auth/src/oauth2/state.ts
+++ packages/better-auth/src/oauth2/state.ts
@@ -24,7 +24,6 @@ export async function generateState(
 		errorURL: c.body?.errorCallbackURL,
 		newUserURL: c.body?.newUserCallbackURL,
 		link,
-
 		/**
 		 * This is the actual expiry time of the state
 		 */
