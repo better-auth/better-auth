22b6eca27f6923ae18d63a763f309658e0d0b715
Maxwell <145994855+ping-maxwell@users.noreply.github.com>
2025-06-15 08:14:16 +1000

    feat(organization): maximumMembersPerTeam support (#2374)
    * feat(organization): maximumMembersPerTeam support
    
    Allow configuration of maximum number of members allowed in a team.
    
    * chore: cleanup

--- packages/better-auth/src/plugins/organization/organization.ts
+++ packages/better-auth/src/plugins/organization/organization.ts
@@ -151,6 +151,22 @@ export interface OrganizationOptions {
 					request?: Request,
 			  ) => number | Promise<number>)
 			| number;
+
+		/**
+		 * The maximum number of members per team.
+		 *
+		 * if `undefined`, there is no limit.
+		 *
+		 * @default undefined
+		 */
+		maximumMembersPerTeam?:
+			| number
+			| ((data: {
+					teamId: string;
+					session: { user: User; session: Session };
+					organizationId: string;
+			  }) => Promise<number> | number)
+			| undefined;
 		/**
 		 * By default, if an organization does only have one team, they'll not be able to remove it.
 		 *
