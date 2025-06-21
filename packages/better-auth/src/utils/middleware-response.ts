8d19f563642d28e1798ac052fed8362af016e375
Alexandre Rodrigues Batista <40678306+xTudoS@users.noreply.github.com>
2025-06-21 08:12:52 -0300

    fix: add missing code var in params

--- packages/better-auth/src/utils/middleware-response.ts
+++ packages/better-auth/src/utils/middleware-response.ts
@@ -4,7 +4,7 @@ type Params = {
 	status: number;
 };
 
-export const middlewareResponse = ({ message, status }: Params) => ({
+export const middlewareResponse = ({ code, message, status }: Params) => ({
 	response: new Response(
 		JSON.stringify({
 			code,
