e3d516cc705fc59068d22349a73000089f6ce3be
Matt Russell <41727084+Whats-A-MattR@users.noreply.github.com>
2025-06-23 16:59:50 +1000

    Update docs/components/builder/social-provider.tsx
    Co-authored-by: cubic-dev-ai[bot] <191113872+cubic-dev-ai[bot]@users.noreply.github.com>

--- docs/components/builder/social-provider.tsx
+++ docs/components/builder/social-provider.tsx
@@ -3,7 +3,7 @@ import { SVGProps } from "react";
 export const socialProviders = {
   apple: {
     Icon: (props: SVGProps<any>) => (
-      <svg
+      <svg {...props}
         {...props}
         xmlns="http://www.w3.org/2000/svg"
         width="1em"
