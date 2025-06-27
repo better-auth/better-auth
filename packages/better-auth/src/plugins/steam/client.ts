39a7325e2a7ab028287f0a21b7007d169d9616c8
ping-maxwell <maxwell.multinite@gmail.com>
2025-06-28 08:17:33 +1000

    fix: client needs to use POST on sign-in/steam

--- packages/better-auth/src/plugins/steam/client.ts
+++ packages/better-auth/src/plugins/steam/client.ts
@@ -1,9 +1,12 @@
 import type { steamAuth } from "./index.js";
-import type { BetterAuthClientPlugin } from "../../client/types.js"; 
+import type { BetterAuthClientPlugin } from "../../client/types.js";
 
 export const steamAuthClient = () => {
 	return {
 		id: "steam-auth-client",
 		$InferServerPlugin: {} as ReturnType<typeof steamAuth>,
+		pathMethods: {
+			"/sign-in/steam": "POST",
+		},
 	} satisfies BetterAuthClientPlugin;
 };
