2d1f74c3fade5bc32443dfab1c67fcb809be6ba4
ping-maxwell <maxwell.multinite@gmail.com>
2025-06-29 14:43:17 +1000

    feat: Vercel BotID plugin

---
+++ packages/botid/src/index.ts
@@ -0,0 +1,84 @@
+import type { BetterAuthPlugin } from "better-auth/types";
+import { APIError } from "better-auth/api";
+import { checkBotId } from "botid/server";
+import { wildcardMatch } from "./utils";
+
+const ERROR_CODES = {
+	BOT_DETECTED: "Bot detected",
+};
+
+export type BotIdVerification = Awaited<ReturnType<typeof checkBotId>>;
+
+export type BotIdOptions = {
+	/**
+	 * Whether to enable bot detection.
+	 *
+	 * @default true
+	 */
+	enabled?: boolean;
+	/**
+	 * The endpoints which will be checked for bots.
+	 * This can be a list of endpoints or "all" to check all endpoints. Supports wildcards.
+	 *
+	 * @example
+	 * ```ts
+	 * ["/sign-up/*", "/sign-in/*"]
+	 * ```
+	 *
+	 * @default "all"
+	 */
+	endpoints?: string[] | "all";
+	/**
+	 * If you want custom logic to validate the request, you can use this function.
+	 * Return `false` to invalidate the request, and `true` to allow the request to proceed.
+	 *
+	 * Note: Any requests which are invalidated by the `endpoints` will not be checked by this function.
+	 *
+	 * @example
+	 * ```ts
+	 * ({ request, verification }) => {
+	 * 	return verification.isBot === false;
+	 * }
+	 */
+	validateRequest?: ({
+		request,
+		verification,
+	}: { request: Request; verification: BotIdVerification }) => Promise<boolean>;
+};
+
+export const botId = (options: BotIdOptions) => {
+	/**
+	 * Check if the path is valid for bot detection
+	 */
+	const isValidPath = (path: string) => {
+		if (options.enabled === false) return false;
+		if (options.endpoints === "all" || !options.endpoints?.length) return true;
+
+		const isMatch = wildcardMatch(options.endpoints);
+		return isMatch(path);
+	};
+
+	return {
+		id: "botid",
+		async onRequest(request, ctx) {
+			// Check if the path is valid
+			const basePath = new URL(ctx.baseURL).pathname;
+			const path = new URL(request.url).pathname.replace(basePath, "");
+			if (!isValidPath(path)) return;
+
+			// Check if the request is a bot
+			const verification = await checkBotId();
+			const customValidation = await options.validateRequest?.({
+				request,
+				verification,
+			});
+
+			if (customValidation ?? verification.isBot) {
+				throw new APIError("UNAUTHORIZED", {
+					message: ERROR_CODES.BOT_DETECTED,
+					code: "BOT_DETECTED" as const,
+				});
+			}
+		},
+	} satisfies BetterAuthPlugin;
+};
