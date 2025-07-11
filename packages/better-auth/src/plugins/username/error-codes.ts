523b476e013ef6b2f1d12191985fe5c136458539
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2025-07-11 17:32:11 -0400

    fix: more incorrect values

--- packages/better-auth/src/plugins/username/error-codes.ts
+++ packages/better-auth/src/plugins/username/error-codes.ts
@@ -3,7 +3,7 @@ export const USERNAME_ERROR_CODES = {
 	EMAIL_NOT_VERIFIED: "email not verified",
 	UNEXPECTED_ERROR: "unexpected error",
 	USERNAME_IS_ALREADY_TAKEN: "username is already taken",
-	USERNAME_TOO_SHORT: "username is too short",
-	USERNAME_TOO_LONG: "username is too long",
+	USERNAME_TOO_SHORT: "username too short",
+	USERNAME_TOO_LONG: "username too long",
 	INVALID_USERNAME: "invalid username",
 };
