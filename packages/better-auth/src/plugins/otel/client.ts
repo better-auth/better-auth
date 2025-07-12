06e681fc0906a5460f34f21cfd6068f356bf887f
dagmawibabi <1babidagi@gmail.com>
2025-07-12 06:21:31 +0300

    oTel Plugin

--- packages/better-auth/src/plugins/otel/client.ts
+++ packages/better-auth/src/plugins/otel/client.ts
@@ -1,11 +1,11 @@
 import type { BetterAuthClientPlugin } from "better-auth";
 import type { oTelPlugin } from "./index"; 
  
-type oTelPlugin = typeof oTelPlugin;
+type OTelPlugin = typeof oTelPlugin;
  
 export const oTelClientPlugin = () => {
   return {
     id: "oTelPlugin",
-    $InferServerPlugin: {} as ReturnType<oTelPlugin>,
+    $InferServerPlugin: {} as ReturnType<OTelPlugin>,
   } satisfies BetterAuthClientPlugin;
 };
