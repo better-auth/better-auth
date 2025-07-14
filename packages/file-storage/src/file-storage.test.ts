bfab134de40316414c2b6fcd55ea4c11603354da
ping-maxwell <maxwell.multinite@gmail.com>
2025-07-14 10:31:19 +1000

    add: temp test

---
+++ packages/file-storage/src/file-storage.test.ts
@@ -0,0 +1,8 @@
+import { describe, it, expect } from "vitest";
+import { fileStorage } from ".";
+
+describe("file-storage", () => {
+	it("should be defined", () => {
+		expect(fileStorage).toBeDefined();
+	});
+});
