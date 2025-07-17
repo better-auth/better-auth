4a951a9e4e0d9d4e934512975b49dc5989e068dc
RokitG <pintosdsgn@gmail.com>
2025-07-17 17:17:07 +0200

    run pnpm lint

--- packages/better-auth/src/plugins/siwe/types.ts
+++ packages/better-auth/src/plugins/siwe/types.ts
@@ -11,50 +11,49 @@ export interface WalletAddress {
 	createdAt: Date;
 }
 
-
 interface CacaoHeader {
-	t: 'caip122'
+	t: "caip122";
 }
 
 // Signed Cacao (CAIP-74)
 interface CacaoPayload {
-	domain: string
-	aud: string
-	nonce: string
-	iss: string
-	version?: string
-	iat?: string
-	nbf?: string
-	exp?: string
-	statement?: string
-	requestId?: string
-	resources?: string[]
-	type?: string
+	domain: string;
+	aud: string;
+	nonce: string;
+	iss: string;
+	version?: string;
+	iat?: string;
+	nbf?: string;
+	exp?: string;
+	statement?: string;
+	requestId?: string;
+	resources?: string[];
+	type?: string;
 }
 
 interface Cacao {
-	h: CacaoHeader
-	p: CacaoPayload
+	h: CacaoHeader;
+	p: CacaoPayload;
 	s: {
-	  t: 'eip191' | 'eip1271'
-	  s: string
-	  m?: string
-	}
+		t: "eip191" | "eip1271";
+		s: string;
+		m?: string;
+	};
 }
 
 export interface SIWEVerifyMessageArgs {
-	message: string
-	signature: string
-	address: string
-	chainId: number
-	cacao?: Cacao
+	message: string;
+	signature: string;
+	address: string;
+	chainId: number;
+	cacao?: Cacao;
 }
 
 export interface ENSLookupArgs {
-	walletAddress: string
+	walletAddress: string;
 }
 
 export interface ENSLookupResult {
-	name: string
-	avatar: string
+	name: string;
+	avatar: string;
 }
