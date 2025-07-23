f48e41624d2ebe4dae1ae8c5b7c905b4e592fe30
Dylan Vanmali <dylanvanmali@yahoo.com>
2025-07-23 12:12:23 -0700

    fix: move to sign

--- packages/better-auth/src/plugins/jwt/index.ts
+++ packages/better-auth/src/plugins/jwt/index.ts
@@ -1,7 +1,6 @@
 import type {
 	AuthContext,
 	BetterAuthPlugin,
-	GenericEndpointContext,
 	HookEndpointContext,
 } from "../../types";
 import { schema } from "./schema";
@@ -26,29 +25,6 @@ export const getJwtPlugin = (ctx: AuthContext) => {
 	}
 	return plugin;
 };
-
-/**
- * Backwards compatable version of signJwt
- *
- * @deprecated - prefer signJwt
- *
- * @param ctx - endpoint context
- * @param options - Jwt signing options. If not provided, uses the jwtPlugin options
- */
-export async function getJwtToken(
-	ctx: GenericEndpointContext,
-	options?: JwtPluginOptions,
-) {
-	if (!options) {
-		options = getJwtPlugin(ctx.context).options;
-	}
-
-	const payload = !options?.jwt?.definePayload
-		? ctx.context.session!.user
-		: await options?.jwt.definePayload(ctx.context.session!);
-
-	return await signJwt(ctx, payload, options);
-}
 
 export const jwt = (options?: JwtPluginOptions) => {
 	const endpoints: BetterAuthPlugin["endpoints"] = {};
