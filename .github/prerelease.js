4239fbac9f9be0e845f3c4a9da19a9a2a163a38e
Bereket Engida <Bekacru@gmail.com>
2025-07-26 13:02:33 -0700

    update

--- .github/prerelease.js
+++ .github/prerelease.js
@@ -1,24 +1,5 @@
 import { exec } from "child_process";
-import { promisify } from "util";
 
-const execAsync = promisify(exec);
-
-async function prerelease() {
-	try {
-		console.log("Entering pre-release mode...");
-		await execAsync("npx changeset pre enter next");
-
-		console.log("Updating package versions for pre-release...");
-		await execAsync("npx changeset version");
-
-		console.log("Updating package-lock.json...");
-		await execAsync("npm install");
-
-		console.log("Pre-release versioning completed successfully!");
-	} catch (error) {
-		console.error("Error during pre-release process:", error);
-		process.exit(1);
-	}
-}
-
-prerelease();
+exec("npx changeset pre enter next");
+exec("npx changeset version");
+exec("npm install");
