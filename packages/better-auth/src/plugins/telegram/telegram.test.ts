55d3c2707a0398d1c8a175d393ddf15995b4832d
Re4GD <57919250+Re4GD@users.noreply.github.com>
2025-07-26 19:50:57 +0300

    feat: add test file

---
+++ packages/better-auth/src/plugins/telegram/telegram.test.ts
@@ -0,0 +1,6 @@
+import { describe, expect } from "vitest";
+import { getTestInstance } from "../../test-utils/test-instance";
+import { telegram } from "./index";
+import { telegramClient } from "./client";
+
+describe("telegram", async (it) => {});
