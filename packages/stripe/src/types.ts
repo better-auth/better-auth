a536de91c2d592e8274ae432673026d9b2907c5c
Bereket Engida <86073083+Bekacru@users.noreply.github.com>
2025-07-26 12:07:55 -0700

    fix(stripe): update customer id should also trigger secondary storage update (#3635)
    * fix: update customer id should also update secondary storage
    
    * chore: add changeset

--- packages/stripe/src/types.ts
+++ packages/stripe/src/types.ts
@@ -187,7 +187,6 @@ export interface StripeOptions {
 	 */
 	onCustomerCreate?: (
 		data: {
-			customer: Customer;
 			stripeCustomer: Stripe.Customer;
 			user: User;
 		},
@@ -330,13 +329,4 @@ export interface StripeOptions {
 	schema?: InferOptionSchema<typeof subscriptions & typeof user>;
 }
 
-export interface Customer {
-	id: string;
-	stripeCustomerId?: string;
-	userId: string;
-	createdAt: Date;
-	updatedAt: Date;
-}
-
 export interface InputSubscription extends Omit<Subscription, "id"> {}
-export interface InputCustomer extends Omit<Customer, "id"> {}
