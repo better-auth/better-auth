27bb8a042532630c5c4d0c46a60115cd9ee599da
Bereket Engida <86073083+Bekacru@users.noreply.github.com>
2025-07-27 23:04:21 -0700

    chore: prerelease workflow (#3638)
    * chore: pre-release workflow
    
    * chore: fix
    
    * update

--- .github/changeset-version.js
+++ .github/changeset-version.js
@@ -7,5 +7,6 @@ import { exec } from "child_process";
 // So we also run `npm install`, which does this update.
 // This is a workaround until this is handled automatically by `changeset version`.
 // See https://github.com/changesets/changesets/issues/421.
+exec("npx changeset pre exit"); // exit pre-release mode if it's active
 exec("npx changeset version");
 exec("npm install");
