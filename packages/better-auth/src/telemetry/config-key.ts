ec433734a24fa082802ec85c2f22df24dbf901f2
frectonz <fraol0912@gmail.com>
2025-07-30 18:27:55 +0300

    feat(cli): add commands to enable and disable telemetry

---
+++ packages/better-auth/src/telemetry/config-key.ts
@@ -0,0 +1,1 @@
+export const TELEMETRY_CONFIG_KEY = "telemetry";
--- packages/cli/src/commands/telemetry.ts
+++ packages/cli/src/commands/telemetry.ts
@@ -1,4 +1,6 @@
 import { Command } from "commander";
+import { GlobalConfig } from "../../../better-auth/src/config";
+import { TELEMETRY_CONFIG_KEY } from "../../../better-auth/src/telemetry/config-key";
 
 export const telemetry = new Command("telemetry");
 
@@ -6,6 +8,8 @@ telemetry
 	.command("enable")
 	.description("Enable telemetry")
 	.action(() => {
+		const config = new GlobalConfig();
+		config.set(TELEMETRY_CONFIG_KEY, "true");
 		console.log("Telemetry enabled");
 	});
 
@@ -13,5 +17,7 @@ telemetry
 	.command("disable")
 	.description("Disable telemetry")
 	.action(() => {
+		const config = new GlobalConfig();
+		config.set(TELEMETRY_CONFIG_KEY, "false");
 		console.log("Telemetry disabled");
 	});
