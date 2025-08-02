fbdf2b3a18ab3edfc9fcde5ee299acf3ad96048d
Bereket Engida <Bekacru@gmail.com>
2025-08-01 23:14:16 -0700

    chore: update ignore build script

--- docs/ignore-build.sh
+++ docs/ignore-build.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-# Get the latest commit message
-if ! message=$(git log -1 --pretty=%s 2>/dev/null); then
-    echo "Failed to check commit message" >&2
-    exit 1
-fi
-
 # Check if commit message starts with release or docs prefix
 if [[ ! "$message" == "chore(release): version packages"* ]] && [[ ! "$message" == "docs"* ]]; then
     echo "Skipping build: Not a release or docs commit."
-    exit 0
-fi 
+    exit 1
+fi
