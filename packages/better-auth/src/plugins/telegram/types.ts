647e72904b40f57cf71ae145abf87410e66f2ca9
Re4GD <57919250+Re4GD@users.noreply.github.com>
2025-08-03 01:46:18 +0300

    fix: types

--- packages/better-auth/src/plugins/telegram/types.ts
+++ packages/better-auth/src/plugins/telegram/types.ts
@@ -1,8 +1,9 @@
+// https://core.telegram.org/widgets/login#receiving-authorization-data
+
 export interface TelegramProfile {
 	id: number;
 	first_name?: string;
 	last_name?: string;
 	username?: string;
 	photo_url?: string;
-	// TODO does is_bot, language etc also exist here?
 }
