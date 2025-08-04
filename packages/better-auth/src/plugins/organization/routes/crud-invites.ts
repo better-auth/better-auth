5eca9389338a900149786bb09b4d802a80e4c668
Bereket Engida <86073083+Bekacru@users.noreply.github.com>
2025-08-04 13:12:46 -0700

    feat(organization): add option for requiring email verificaiton (#3785)
    * feat(organization): add option for requiring email verificaiton
    
    * add rejection
    
    * docs: add docs

--- packages/better-auth/src/plugins/organization/routes/crud-invites.ts
+++ packages/better-auth/src/plugins/organization/routes/crud-invites.ts
@@ -419,6 +419,16 @@ export const acceptInvitation = <O extends OrganizationOptions>(options: O) =>
 				});
 			}
 
+			if (
+				ctx.context.orgOptions.requireEmailVerificationOnInvitation &&
+				!session.user.emailVerified
+			) {
+				throw new APIError("FORBIDDEN", {
+					message:
+						ORGANIZATION_ERROR_CODES.EMAIL_VERIFICATION_REQUIRED_BEFORE_ACCEPTING_OR_REJECTING_INVITATION,
+				});
+			}
+
 			const membershipLimit = ctx.context.orgOptions?.membershipLimit || 100;
 			const membersCount = await adapter.countMembers({
 				organizationId: invitation.organizationId,
@@ -578,6 +588,16 @@ export const rejectInvitation = <O extends OrganizationOptions>(options: O) =>
 				throw new APIError("FORBIDDEN", {
 					message:
 						ORGANIZATION_ERROR_CODES.YOU_ARE_NOT_THE_RECIPIENT_OF_THE_INVITATION,
+				});
+			}
+
+			if (
+				ctx.context.orgOptions.requireEmailVerificationOnInvitation &&
+				!session.user.emailVerified
+			) {
+				throw new APIError("FORBIDDEN", {
+					message:
+						ORGANIZATION_ERROR_CODES.EMAIL_VERIFICATION_REQUIRED_BEFORE_ACCEPTING_OR_REJECTING_INVITATION,
 				});
 			}
 
