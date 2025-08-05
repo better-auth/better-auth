2324a6a3816ba4391cea59edddc1fb85b26be0a3
Kinfe123 <kinfishtech@gmail.com>
2025-08-05 15:20:35 +0300

    update

--- docs/components/landing/section.tsx
+++ docs/components/landing/section.tsx
@@ -1,25 +1,6 @@
 import type React from "react";
 import SectionSvg from "./section-svg";
 
-const Icon = ({
-	className,
-	...rest
-}: { className?: string; [key: string]: any }) => {
-	return (
-		<svg
-			xmlns="http://www.w3.org/2000/svg"
-			fill="none"
-			viewBox="0 0 24 24"
-			strokeWidth="1.5"
-			stroke="currentColor"
-			className={className}
-			{...rest}
-		>
-			<path strokeLinecap="round" strokeLinejoin="round" d="M12 6v12m6-6H6" />
-		</svg>
-	);
-};
-
 const Section = ({
 	className,
 	id,
