210a43593b8a527da37615a177ca6d974535033a
Dusan Misic <dmisic@inventage.com>
2025-08-08 18:03:51 +0200

    fix: prematurely deleting active sessions in secondary storage when deleting user with internalAdapter.deleteUser

--- packages/better-auth/src/db/internal-adapter.ts
+++ packages/better-auth/src/db/internal-adapter.ts
@@ -223,10 +223,6 @@ export const createInternalAdapter = (
 			return total;
 		},
 		deleteUser: async (userId: string) => {
-			if (secondaryStorage) {
-				await secondaryStorage.delete(`active-sessions-${userId}`);
-			}
-
 			if (!secondaryStorage || options.session?.storeSessionInDatabase) {
 				await deleteManyWithHooks(
 					[
