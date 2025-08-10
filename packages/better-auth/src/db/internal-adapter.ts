d367af99f243779ec1e9c9764122171c5202676b
Dusan Misic <dmisic@inventage.com>
2025-08-10 19:00:56 +0200

    fix: remove dangling active-sessions-[userId] key in secondary storage after deleting all sessions from that list

--- packages/better-auth/src/db/internal-adapter.ts
+++ packages/better-auth/src/db/internal-adapter.ts
@@ -628,12 +628,22 @@ export const createInternalAdapter = (
 					for (const session of sessions) {
 						await secondaryStorage.delete(session.token);
 					}
+					await secondaryStorage.delete(`active-sessions-${userIdOrSessionTokens}`);
 				} else {
 					for (const sessionToken of userIdOrSessionTokens) {
 						const session = await secondaryStorage.get(sessionToken);
 						if (session) {
 							await secondaryStorage.delete(sessionToken);
 						}
+					}
+					const sessionToken = userIdOrSessionTokens[0];
+					const session = await secondaryStorage.get(sessionToken);
+					if (session) {
+						const parsedSession = JSON.parse(session) as {
+							session: Session;
+							user: User;
+						};
+						await secondaryStorage.delete(`active-sessions-${parsedSession.user.id}`);
 					}
 				}
 
