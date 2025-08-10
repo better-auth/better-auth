140ba9016d47bb350e9d85cd6b9b418cc2bb9efd
Dusan Misic <dmisic@inventage.com>
2025-08-10 19:07:51 +0200

    fix: format code

--- packages/better-auth/src/db/internal-adapter.ts
+++ packages/better-auth/src/db/internal-adapter.ts
@@ -578,7 +578,9 @@ export const createInternalAdapter = (
 					for (const session of sessions) {
 						await secondaryStorage.delete(session.token);
 					}
-					await secondaryStorage.delete(`active-sessions-${userIdOrSessionTokens}`);
+					await secondaryStorage.delete(
+						`active-sessions-${userIdOrSessionTokens}`,
+					);
 				} else {
 					for (const sessionToken of userIdOrSessionTokens) {
 						const session = await secondaryStorage.get(sessionToken);
@@ -593,7 +595,9 @@ export const createInternalAdapter = (
 							session: Session;
 							user: User;
 						};
-						await secondaryStorage.delete(`active-sessions-${parsedSession.user.id}`);
+						await secondaryStorage.delete(
+							`active-sessions-${parsedSession.user.id}`,
+						);
 					}
 				}
 
