9e07d0227bb46ddb2359814189a920c6054df9b3
Dylan Vanmali <dylanvanmali@yahoo.com>
2025-08-18 23:22:43 -0700

    feat: move seconds parsing into time util

--- packages/better-auth/src/plugins/jwt/utils.ts
+++ packages/better-auth/src/plugins/jwt/utils.ts
@@ -1,82 +1,12 @@
 import { subtle, getRandomValues } from "@better-auth/utils";
 import { base64 } from "@better-auth/utils/base64";
-
-const minute = 60
-const hour = minute * 60
-const day = hour * 24
-const week = day * 7
-const year = day * 365.25
-
-const REGEX =
-  /^(\+|\-)? ?(\d+|\d+\.\d+) ?(seconds?|secs?|s|minutes?|mins?|m|hours?|hrs?|h|days?|d|weeks?|w|years?|yrs?|y)(?: (ago|from now))?$/i
-
-/**
- * Converts string into seconds using the same method as jose package
- * 
- * See https://github.com/panva/jose/blob/main/src/lib/secs.ts
- */
-function secs(str: string) {
-	  const matched = REGEX.exec(str)
-
-  if (!matched || (matched[4] && matched[1])) {
-    throw new TypeError('Invalid time period format')
-  }
-
-  const value = parseFloat(matched[2])
-  const unit = matched[3].toLowerCase()
-
-  let numericDate: number
-
-  switch (unit) {
-    case 'sec':
-    case 'secs':
-    case 'second':
-    case 'seconds':
-    case 's':
-      numericDate = Math.round(value)
-      break
-    case 'minute':
-    case 'minutes':
-    case 'min':
-    case 'mins':
-    case 'm':
-      numericDate = Math.round(value * minute)
-      break
-    case 'hour':
-    case 'hours':
-    case 'hr':
-    case 'hrs':
-    case 'h':
-      numericDate = Math.round(value * hour)
-      break
-    case 'day':
-    case 'days':
-    case 'd':
-      numericDate = Math.round(value * day)
-      break
-    case 'week':
-    case 'weeks':
-    case 'w':
-      numericDate = Math.round(value * week)
-      break
-    // years matched
-    default:
-      numericDate = Math.round(value * year)
-      break
-  }
-
-  if (matched[1] === '-' || matched[4] === 'ago') {
-    return -numericDate
-  }
-
-  return numericDate
-}
+import { joseSecs } from "../../utils/time";
 
 /**
  * Converts an expirationTime to ISO seconds expiration time (the format of JWT exp)
  *
  * See https://github.com/panva/jose/blob/main/src/lib/jwt_claims_set.ts#L245
- * 
+ *
  * @param expirationTime - see options.jwt.expirationTime
  * @param iat - the iat time to consolidate on
  * @returns
@@ -85,12 +15,12 @@ export function toExpJWT(
 	expirationTime: number | Date | string,
 	iat: number,
 ): number {
-	if (typeof expirationTime === 'number') {
-		return expirationTime
+	if (typeof expirationTime === "number") {
+		return expirationTime;
 	} else if (expirationTime instanceof Date) {
-		return Math.floor(expirationTime.getTime() / 1000)
+		return Math.floor(expirationTime.getTime() / 1000);
 	} else {
-		return iat + secs(expirationTime)
+		return iat + joseSecs(expirationTime);
 	}
 }
 
