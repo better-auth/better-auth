20bf7cdb65e97ae9c37329440f45da6a4d5b612a
Alex Yang <himself65@outlook.com>
2025-08-21 15:38:50 -0700

    demo: fix domain issue on preview env

--- demo/nextjs/lib/auth.ts
+++ demo/nextjs/lib/auth.ts
@@ -48,8 +48,19 @@ const PLUS_PRICE_ID = {
 	default: "price_1RoxnJHmTADgihIthZTLmrPn",
 	annual: "price_1Roxo5HmTADgihItEbJu5llL",
 };
+
+const baseURL: string | undefined =
+	process.env.VERCEL === "1"
+		? process.env.VERCEL_ENV === "production"
+			? process.env.BETTER_AUTH_URL
+			: process.env.VERCEL_ENV === "preview"
+				? `https://${process.env.VERCEL_URL}`
+				: undefined
+		: undefined;
+
 export const auth = betterAuth({
 	appName: "Better Auth Demo",
+	baseURL,
 	database: {
 		dialect: libsql,
 		type: "sqlite",
