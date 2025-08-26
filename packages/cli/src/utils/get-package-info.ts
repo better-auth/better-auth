e1e7b0ded2b817842d2506637053f871821364bd
Alex Yang <himself65@outlook.com>
2025-08-25 17:42:22 -0700

    chore: remove unused deps

--- packages/cli/src/utils/get-package-info.ts
+++ packages/cli/src/utils/get-package-info.ts
@@ -5,5 +5,5 @@ export function getPackageInfo(cwd?: string) {
 	const packageJsonPath = cwd
 		? path.join(cwd, "package.json")
 		: path.join("package.json");
-	return JSON.parse(fs.readFileSync(packageJsonPath, 'utf-8'));
+	return JSON.parse(fs.readFileSync(packageJsonPath, "utf-8"));
 }
