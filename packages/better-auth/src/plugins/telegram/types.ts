caa37b4aeb734e3dd9593bd291b125782cc48798
Re4GD <57919250+Re4GD@users.noreply.github.com>
2025-08-30 13:09:55 +0300

    feat: export login widget callback types

--- packages/better-auth/src/plugins/telegram/types.ts
+++ packages/better-auth/src/plugins/telegram/types.ts
@@ -7,3 +7,15 @@ export interface TelegramProfile {
 	username?: string;
 	photo_url?: string;
 }
+
+export type TelegramPayloadData = {
+	id: number;
+	first_name?: string;
+	last_name?: string;
+	username?: string;
+	photo_url?: string;
+	auth_date: number;
+	hash: string;
+};
+
+export type TelegramPayload = false | TelegramPayloadData;
