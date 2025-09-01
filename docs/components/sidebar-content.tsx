2db142465ecf29575bb1edf4e59173daf7ecd498
Bereket Engida <86073083+Bekacru@users.noreply.github.com>
2025-09-01 13:15:23 -0700

    feat: add last login method plugin (#4347)

--- docs/components/sidebar-content.tsx
+++ docs/components/sidebar-content.tsx
@@ -1705,6 +1705,24 @@ C0.7,239.6,62.1,0.5,62.2,0.4c0,0,54,13.8,119.9,30.8S302.1,62,302.2,62c0.2,0,0.2,
 				icon: () => <p className="text-xs">';--</p>,
 			},
 			{
+				title: "Last Login Method",
+				href: "/docs/plugins/last-login-method",
+				icon: () => (
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="1.2em"
+						height="1.2em"
+						viewBox="0 0 256 256"
+					>
+						<path
+							fill="currentColor"
+							d="m141.66 133.66l-40 40A8 8 0 0 1 88 168v-32H24a8 8 0 0 1 0-16h64V88a8 8 0 0 1 13.66-5.66l40 40a8 8 0 0 1 0 11.32M200 32h-64a8 8 0 0 0 0 16h56v160h-56a8 8 0 0 0 0 16h64a8 8 0 0 0 8-8V40a8 8 0 0 0-8-8"
+						/>
+					</svg>
+				),
+				isNew: true,
+			},
+			{
 				title: "Multi Session",
 				icon: () => (
 					<svg
