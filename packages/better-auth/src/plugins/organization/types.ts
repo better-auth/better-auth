b401592aa33dd9a577b008866eac0e05f23dfb10
Allie Fitter <afitter@dustidentity.com>
2025-09-03 10:15:09 -0500

    feat(organization): Make membership optional

--- packages/better-auth/src/plugins/organization/types.ts
+++ packages/better-auth/src/plugins/organization/types.ts
@@ -52,6 +52,12 @@ export interface OrganizationOptions {
 	 */
 	ac?: AccessControl;
 	/**
+	 * Require membership in an organization to perform CRUD operations.
+	 *
+	 * @default true
+	 */
+	requireMembership?: boolean;
+	/**
 	 * Custom permissions for roles.
 	 */
 	roles?: {
