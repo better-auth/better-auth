7283fe1f6b2d63b98cbef414d4520b0b2a3679f1
Baris Palaska <barispalaska@gmail.com>
2025-09-03 18:12:57 +0100

    add new label in docs

--- docs/components/sidebar-content.tsx
+++ docs/components/sidebar-content.tsx
@@ -998,6 +998,7 @@ C0.7,239.6,62.1,0.5,62.2,0.4c0,0,54,13.8,119.9,30.8S302.1,62,302.2,62c0.2,0,0.2,
 			{
 				title: "Strava",
 				href: "/docs/authentication/strava",
+				isNew: true,
 				icon: () => (
 					<svg
 						xmlns="http://www.w3.org/2000/svg"
