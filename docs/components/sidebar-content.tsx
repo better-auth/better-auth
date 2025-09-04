963dfe0af065e587e02693f673198625888f2299
Bereket Engida <Bekacru@gmail.com>
2025-09-03 18:35:12 -0700

    docs: update new status

--- docs/components/sidebar-content.tsx
+++ docs/components/sidebar-content.tsx
@@ -758,7 +758,7 @@ export const contents: Content[] = [
 			{
 				title: "Slack",
 				href: "/docs/authentication/slack",
-				isNew: true,
+
 				icon: () => (
 					<svg
 						xmlns="http://www.w3.org/2000/svg"
@@ -778,7 +778,7 @@ export const contents: Content[] = [
 			{
 				title: "Notion",
 				href: "/docs/authentication/notion",
-				isNew: true,
+
 				icon: () => (
 					<svg
 						xmlns="http://www.w3.org/2000/svg"
@@ -1527,7 +1527,6 @@ C0.7,239.6,62.1,0.5,62.2,0.4c0,0,54,13.8,119.9,30.8S302.1,62,302.2,62c0.2,0,0.2,
 			{
 				title: "Sign In With Ethereum",
 				href: "/docs/plugins/siwe",
-				isNew: true,
 				icon: () => (
 					<svg
 						xmlns="http://www.w3.org/2000/svg"
@@ -1901,7 +1900,6 @@ C0.7,239.6,62.1,0.5,62.2,0.4c0,0,54,13.8,119.9,30.8S302.1,62,302.2,62c0.2,0,0.2,
 			{
 				title: "Autumn Billing",
 				href: "/docs/plugins/autumn",
-				isNew: true,
 				icon: () => (
 					<svg
 						xmlns="http://www.w3.org/2000/svg"
@@ -1919,7 +1917,6 @@ C0.7,239.6,62.1,0.5,62.2,0.4c0,0,54,13.8,119.9,30.8S302.1,62,302.2,62c0.2,0,0.2,
 			{
 				title: "Dodo Payments",
 				href: "/docs/plugins/dodopayments",
-				isNew: true,
 				icon: () => (
 					<svg
 						width="1.2em"
