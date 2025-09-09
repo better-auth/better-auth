56fd3b592fd48c0975ed51a065227a94f8528db1
Joel Solano <solano.joel@gmx.de>
2025-09-09 12:58:47 +0200

    Revert "fix(mongodb): pass fresh session for transactions"
    This reverts commit ea1c8449183616640aceaec873f36935fe18a7c1.

--- packages/better-auth/src/adapters/mongodb-adapter/mongodb-adapter.ts
+++ packages/better-auth/src/adapters/mongodb-adapter/mongodb-adapter.ts
@@ -1,11 +1,6 @@
 import { ObjectId, type Db, type ClientSession } from "mongodb";
 import type { Adapter, BetterAuthOptions, Where } from "../../types";
-import {
-	createAdapter,
-	type AdapterDebugLogs,
-	type CreateAdapterOptions,
-	type CreateCustomAdapter,
-} from "../create-adapter";
+import { createAdapter, type AdapterDebugLogs } from "../create-adapter";
 
 export interface MongoDBAdapterConfig {
 	/**
@@ -46,9 +41,83 @@ export const mongodbAdapter = (db: Db, config?: MongoDBAdapterConfig) => {
 		return undefined;
 	};
 
-	const createCustomAdapter =
-		(db: Db, session?: ClientSession): CreateCustomAdapter =>
-		({ options, getFieldName, schema, getDefaultModelName }) => {
+	let lazyAdapter: ((options: BetterAuthOptions) => Adapter) | null = null;
+	lazyAdapter = createAdapter({
+		config: {
+			adapterId: "mongodb-adapter",
+			adapterName: "MongoDB Adapter",
+			usePlural: config?.usePlural ?? false,
+			debugLogs: config?.debugLogs ?? false,
+			mapKeysTransformInput: {
+				id: "_id",
+			},
+			mapKeysTransformOutput: {
+				_id: "id",
+			},
+			supportsNumericIds: false,
+			transaction:
+				config?.session && (config?.transaction ?? true)
+					? (cb) => {
+							const adapter = lazyAdapter!(lazyOptions!);
+							if (!config.session) {
+								return cb(adapter);
+							}
+							return config.session.withTransaction(async () => {
+								return cb(adapter);
+							});
+						}
+					: false,
+			customTransformInput({
+				action,
+				data,
+				field,
+				fieldAttributes,
+				schema,
+				model,
+				options,
+			}) {
+				const customIdGen = getCustomIdGenerator(options);
+
+				if (field === "_id" || fieldAttributes.references?.field === "id") {
+					if (customIdGen) {
+						return data;
+					}
+					if (action === "update") {
+						return data;
+					}
+					if (Array.isArray(data)) {
+						return data.map((v) => new ObjectId());
+					}
+					if (typeof data === "string") {
+						try {
+							return new ObjectId(data);
+						} catch (error) {
+							return new ObjectId();
+						}
+					}
+					return new ObjectId();
+				}
+				return data;
+			},
+			customTransformOutput({ data, field, fieldAttributes }) {
+				if (field === "id" || fieldAttributes.references?.field === "id") {
+					if (data instanceof ObjectId) {
+						return data.toHexString();
+					}
+					if (Array.isArray(data)) {
+						return data.map((v) => {
+							if (v instanceof ObjectId) {
+								return v.toHexString();
+							}
+							return v;
+						});
+					}
+					return data;
+				}
+				return data;
+			},
+		},
+		adapter: ({ options, getFieldName, schema, getDefaultModelName }) => {
 			const customIdGen = getCustomIdGenerator(options);
 
 			function serializeID({
@@ -188,19 +257,25 @@ export const mongodbAdapter = (db: Db, config?: MongoDBAdapterConfig) => {
 
 			return {
 				async create({ model, data: values }) {
-					const res = await db.collection(model).insertOne(values, { session });
+					const res = await db
+						.collection(model)
+						.insertOne(values, { session: config?.session });
 					const insertedData = { _id: res.insertedId.toString(), ...values };
 					return insertedData as any;
 				},
 				async findOne({ model, where, select }) {
 					const clause = convertWhereClause({ where, model });
-					const res = await db.collection(model).findOne(clause, { session });
+					const res = await db
+						.collection(model)
+						.findOne(clause, { session: config?.session });
 					if (!res) return null;
 					return res as any;
 				},
 				async findMany({ model, where, limit, offset, sortBy }) {
 					const clause = where ? convertWhereClause({ where, model }) : {};
-					const cursor = db.collection(model).find(clause, { session });
+					const cursor = db
+						.collection(model)
+						.find(clause, { session: config?.session });
 					if (limit) cursor.limit(limit);
 					if (offset) cursor.skip(offset);
 					if (sortBy)
@@ -214,7 +289,7 @@ export const mongodbAdapter = (db: Db, config?: MongoDBAdapterConfig) => {
 				async count({ model }) {
 					const res = await db
 						.collection(model)
-						.countDocuments(undefined, { session });
+						.countDocuments(undefined, { session: config?.session });
 					return res;
 				},
 				async update({ model, where, update: values }) {
@@ -224,7 +299,7 @@ export const mongodbAdapter = (db: Db, config?: MongoDBAdapterConfig) => {
 						clause,
 						{ $set: values as any },
 						{
-							session,
+							session: config?.session,
 							returnDocument: "after",
 						},
 					);
@@ -239,109 +314,26 @@ export const mongodbAdapter = (db: Db, config?: MongoDBAdapterConfig) => {
 						{
 							$set: values as any,
 						},
-						{ session },
+						{ session: config?.session },
 					);
 					return res.modifiedCount;
 				},
 				async delete({ model, where }) {
 					const clause = convertWhereClause({ where, model });
-					await db.collection(model).deleteOne(clause, { session });
+					await db
+						.collection(model)
+						.deleteOne(clause, { session: config?.session });
 				},
 				async deleteMany({ model, where }) {
 					const clause = convertWhereClause({ where, model });
 					const res = await db
 						.collection(model)
-						.deleteMany(clause, { session });
+						.deleteMany(clause, { session: config?.session });
 					return res.deletedCount;
 				},
 			};
-		};
-
-	let lazyAdapter: ((options: BetterAuthOptions) => Adapter) | null = null;
-	let adapterOptions: CreateAdapterOptions | null = null;
-	adapterOptions = {
-		config: {
-			adapterId: "mongodb-adapter",
-			adapterName: "MongoDB Adapter",
-			usePlural: config?.usePlural ?? false,
-			debugLogs: config?.debugLogs ?? false,
-			mapKeysTransformInput: {
-				id: "_id",
-			},
-			mapKeysTransformOutput: {
-				_id: "id",
-			},
-			supportsNumericIds: false,
-			transaction:
-				config?.session && (config?.transaction ?? true)
-					? (cb) => {
-							const adapter = lazyAdapter!(lazyOptions!);
-							if (!config.session) {
-								return cb(adapter);
-							}
-							return config.session.withTransaction(async (newSession) => {
-								const adapter = createAdapter({
-									config: adapterOptions!.config,
-									adapter: createCustomAdapter(db, newSession),
-								})(lazyOptions!);
-								return cb(adapter);
-							});
-						}
-					: false,
-			customTransformInput({
-				action,
-				data,
-				field,
-				fieldAttributes,
-				schema,
-				model,
-				options,
-			}) {
-				const customIdGen = getCustomIdGenerator(options);
-
-				if (field === "_id" || fieldAttributes.references?.field === "id") {
-					if (customIdGen) {
-						return data;
-					}
-					if (action === "update") {
-						return data;
-					}
-					if (Array.isArray(data)) {
-						return data.map((v) => new ObjectId());
-					}
-					if (typeof data === "string") {
-						try {
-							return new ObjectId(data);
-						} catch (error) {
-							return new ObjectId();
-						}
-					}
-					return new ObjectId();
-				}
-				return data;
-			},
-			customTransformOutput({ data, field, fieldAttributes }) {
-				if (field === "id" || fieldAttributes.references?.field === "id") {
-					if (data instanceof ObjectId) {
-						return data.toHexString();
-					}
-					if (Array.isArray(data)) {
-						return data.map((v) => {
-							if (v instanceof ObjectId) {
-								return v.toHexString();
-							}
-							return v;
-						});
-					}
-					return data;
-				}
-				return data;
-			},
 		},
-		adapter: createCustomAdapter(db, config?.session),
-	};
-
-	lazyAdapter = createAdapter(adapterOptions);
+	});
 
 	return (options: BetterAuthOptions): Adapter => {
 		lazyOptions = options;
