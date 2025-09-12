7c51e96d4aa9c4ddfd846b14f46dcdff78d59221
Alex Yang <himself65@outlook.com>
2025-09-12 00:27:12 -0700

    Revert "feat: support cross-domain cookie (#4482)"
    This reverts commit 61b6a874354cf080d4b6812c62346ae235b4c986.

--- e2e/integration/vanilla-node/src/main.ts
+++ e2e/integration/vanilla-node/src/main.ts
@@ -2,10 +2,9 @@ import { createAuthClient } from "better-auth/client";
 
 const search = new URLSearchParams(window.location.search);
 const port = search.get("port");
-const https = search.get("https");
 
 const client = createAuthClient({
-	baseURL: `http${https ? "s" : ""}://localhost:${port ?? 3000}`,
+	baseURL: `http://localhost:${port ?? 3000}`,
 });
 
 declare global {
