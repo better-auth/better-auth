7f8e80ce6758401a9c50fe679b72c6193d2fa5a8
ping-maxwell <maxwell.multinite@gmail.com>
2025-09-19 23:58:41 +1000

    chore: lint

--- packages/better-auth/src/db/internal-adapter.ts
+++ packages/better-auth/src/db/internal-adapter.ts
@@ -54,7 +54,9 @@ export const createInternalAdapter = (
 				if (!parsed) return;
 
 				const sessionTTL = Math.max(
-					Math.floor((new Date(parsed.session.expiresAt).getTime() - now) / 1000),
+					Math.floor(
+						(new Date(parsed.session.expiresAt).getTime() - now) / 1000,
+					),
 					0,
 				);
 
