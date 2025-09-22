1ca2a412171799e098e1cc43babe3234bcb065de
Kinfe123 <kinfishtech@gmail.com>
2025-09-22 23:52:05 +0300

    update

--- docs/components/mcp-install-button.tsx
+++ docs/components/mcp-install-button.tsx
@@ -1,8 +1,5 @@
-"use client";
+"use clien
 
-import { useTheme } from "next-themes";
-import Image from "next/image";
-import Link from "next/link";
 import { Button } from "./ui/button";
 
 interface McpInstallButtonProps {
