15cc7a9e410668888a7edbc84b7213d3804f94d9
Alex Yang <himself65@outlook.com>
2025-09-24 15:41:50 -0700

    fix(nuxt): avoid load env base url for SSR (#4887)

--- packages/better-auth/src/client/client-ssr.test.ts
+++ packages/better-auth/src/client/client-ssr.test.ts
@@ -2,17 +2,16 @@
 import { expect, it, vi } from "vitest";
 import { createAuthClient as createVueClient } from "./vue";
 
-it("should call '/api/auth' if no baseURL is provided", async () => {
+it("should call '/api/auth' for vue client", async () => {
 	const customFetchImpl = vi.fn(async (url: string | Request | URL) => {
 		expect(url).toBe("/api/auth/get-session");
 		return new Response();
 	});
-	const originalEnv = process.env;
-	process.env = {};
+	process.env.BETTER_AUTH_URL = "http://localhost:3000";
 	// use DisposableStack when Node.js 24 is the minimum requirement
 	using _ = {
 		[Symbol.dispose]() {
-			process.env = originalEnv;
+			process.env.BETTER_AUTH_URL = undefined;
 		},
 	};
 	const client = createVueClient({
