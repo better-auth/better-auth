41a59ff68c60926a23c02be91d80c2479fd1d58d
Re4GD <57919250+Re4GD@users.noreply.github.com>
2025-09-24 23:39:46 +0300

    fix: sidebar icon size

--- docs/components/sidebar-content.tsx
+++ docs/components/sidebar-content.tsx
@@ -1341,6 +1341,7 @@ C0.7,239.6,62.1,0.5,62.2,0.4c0,0,54,13.8,119.9,30.8S302.1,62,302.2,62c0.2,0,0.2,
 						width="1.2em"
 						height="1.2em"
 						viewBox="0 0 101 88"
+						className="p-0.5"
 					>
 						<path
 							fill="currentColor"
