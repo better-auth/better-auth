5812e99d5e39e2f71f7db8988ad309068fab6247
Alex Yang <himself65@outlook.com>
2025-09-24 15:29:30 -0700

    test: add

--- packages/better-auth/src/client/client-ssr.test.ts
+++ packages/better-auth/src/client/client-ssr.test.ts
@@ -2,19 +2,11 @@
 import { expect, it, vi } from "vitest";
 import { createAuthClient as createVueClient } from "./vue";
 
-it("should call '/api/auth' if no baseURL is provided", async () => {
+it("should call '/api/auth' for vue client", async () => {
 	const customFetchImpl = vi.fn(async (url: string | Request | URL) => {
 		expect(url).toBe("/api/auth/get-session");
 		return new Response();
 	});
-	const originalEnv = process.env;
-	process.env = {};
-	// use DisposableStack when Node.js 24 is the minimum requirement
-	using _ = {
-		[Symbol.dispose]() {
-			process.env = originalEnv;
-		},
-	};
 	const client = createVueClient({
 		fetchOptions: {
 			customFetchImpl,
