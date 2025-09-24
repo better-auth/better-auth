60ab8475e6ecbb40e98d153bf970de50b7c17f11
Re4GD <57919250+Re4GD@users.noreply.github.com>
2025-09-24 23:58:29 +0300

    feat: tests file

---
+++ packages/better-auth/src/plugins/siws/siws.test.ts
@@ -0,0 +1,6 @@
+import { describe, expect } from "vitest";
+import { getTestInstance } from "../../test-utils/test-instance";
+import { siws } from "./index";
+import { siwsClient } from "./client";
+
+describe("siws", async (it) => {});
