76b6baef332b9ce8949b0181408d78b867336751
Bereket Engida <Bekacru@gmail.com>
2025-09-28 13:01:41 -0700

    chore: remove log

--- packages/expo/src/index.ts
+++ packages/expo/src/index.ts
@@ -112,7 +112,6 @@ export const expo = (options?: ExpoOptions) => {
 						ctx.context.secret,
 						stateCookie.attributes,
 					);
-					console.log({ url });
 					return ctx.redirect(ctx.query.authorizationURL);
 				},
 			),
