c272d12e6e4000623c7c34c5f7c245dee7d33339
Alex Yang <himself65@outlook.com>
2025-09-30 16:10:46 -0700

    fix: restrict export

--- packages/core/src/utils/index.ts
+++ packages/core/src/utils/index.ts
@@ -1,22 +1,30 @@
-export * from "./await-object";
-export * from "./boolean";
-export * from "./clone";
-export * from "./color-depth";
-export * from "./colors";
-export * from "./constants";
-export * from "./date";
-export * from "./ensure-utc";
-export * from "./error-codes";
-export * from "./hashing";
-export * from "./hide-metadata";
-export * from "./id";
-export * from "./is-atom";
-export * from "./is-promise";
-export * from "./json";
-export * from "./logger";
-export * from "./merger";
-export * from "./middleware-response";
-export * from "./misc";
-export * from "./plugin-helper";
-export * from "./time";
-export * from "./wildcard";
+export { toBoolean } from "./boolean";
+export { clone } from "./clone";
+export { getColorDepth } from "./color-depth";
+export { colors } from "./colors";
+export { DEFAULT_SECRET } from "./constants";
+export { getDate } from "./date";
+export { ensureUTC } from "./ensure-utc";
+export { defineErrorCodes } from "./error-codes";
+export { toChecksumAddress } from "./hashing";
+export { HIDE_METADATA } from "./hide-metadata";
+export { generateId } from "./id";
+export { isAtom } from "./is-atom";
+export { isPromise } from "./is-promise";
+export { safeJSONParse } from "./json";
+export {
+	createLogger,
+	logger,
+	shouldPublishLog,
+	levels,
+	type Logger,
+	type LogHandlerParams,
+	type LogLevel,
+	type InternalLogger,
+} from "./logger";
+export { merge } from "./merger";
+export { middlewareResponse } from "./middleware-response";
+export { capitalizeFirstLetter } from "./misc";
+export { getEndpointResponse } from "./plugin-helper";
+export { joseSecs } from "./time";
+export { wildcardMatch } from "./wildcard";
