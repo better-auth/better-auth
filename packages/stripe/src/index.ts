c5798f0cafd38ce214476ef1ce4d5fa40d15b903
Ebalo <12586380+ebalo55@users.noreply.github.com>
2025-10-01 17:14:07 +0200

    Fix variable naming for customer creation options

--- packages/stripe/src/index.ts
+++ packages/stripe/src/index.ts
@@ -1299,10 +1299,9 @@ export const stripe = <O extends StripeOptions>(options: O) => {
 							create: {
 								async after(user, ctx) {
 									if (ctx && options.createCustomerOnSignUp) {
-
-										let options_override = {};
+										let optionsOverride = {};
 										if (options.getCustomerCreateParams) {
-											options_override = await options.getCustomerCreateParams(
+											optionsOverride = await options.getCustomerCreateParams(
 												{ user, session: null },
 												ctx,
 											);
@@ -1314,7 +1313,7 @@ export const stripe = <O extends StripeOptions>(options: O) => {
 											metadata: {
 												userId: user.id,
 											},
-											...options_override,
+											...optionsOverride,
 										});
 										await ctx.context.internalAdapter.updateUser(user.id, {
 											stripeCustomerId: stripeCustomer.id,
