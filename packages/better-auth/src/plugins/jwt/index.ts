ee2c0ea249dff80806f08c5e94d153dab02f05bf
Dylan Vanmali <dylanvanmali@yahoo.com>
2025-10-02 16:18:29 -0700

    fix: remove unneccesary export

--- packages/better-auth/src/plugins/jwt/index.ts
+++ packages/better-auth/src/plugins/jwt/index.ts
@@ -16,7 +16,6 @@ import type { JwtOptions } from "./types";
 import { createJwk } from "./utils";
 export type * from "./types";
 export { generateExportedKeyPair, createJwk } from "./utils";
-export { jwtClient } from "./client";
 
 export const jwt = (options?: JwtOptions) => {
 	// Remote url must be set when using signing function
