fea9a22b46a417c9b4096c69b7e5fd4aa7b53b2c
ping-maxwell <maxwell.multinite@gmail.com>
2025-10-02 21:54:01 +1000

    fix(adapter): foreign keys that are nullable on number ids can return string of `null`
    When using `useNumberId` then setting `null` to a foreign key field using the adapter, it's possible it will set it to a string `null` (even when returned from the adapter)
    This PR fixes this and adds tests.
    
    Closes https://github.com/better-auth/better-auth/issues/3001

--- packages/better-auth/src/adapters/tests/normal.ts
+++ packages/better-auth/src/adapters/tests/normal.ts
@@ -1,6 +1,6 @@
 import { expect } from "vitest";
 import { createTestSuite } from "../create-test-suite";
-import type { User } from "../../types";
+import type { BetterAuthPlugin, Session, User } from "../../types";
 
 /**
  * This test suite tests the basic CRUD operations of the adapter.
@@ -97,6 +97,37 @@ export const getNormalTestSuiteTests = ({
 				where: [{ field: "id", value: res.id }],
 			});
 			expect(findResult).toEqual(res);
+		},
+		"create - should not return string for nullable foreign keys": async () => {
+			await modifyBetterAuthOptions(
+				{
+					plugins: [
+						{
+							id: "test",
+							schema: {
+								testModel: {
+									fields: {
+										nullableReference: {
+											type: "string",
+											references: { field: "id", model: "user" },
+											required: false,
+										},
+									},
+								},
+							},
+						} satisfies BetterAuthPlugin,
+					],
+				},
+				true,
+			);
+			const { nullableReference } = await adapter.create<{
+				nullableReference: string | null;
+			}>({
+				model: "testModel",
+				data: { nullableReference: null },
+				forceAllowId: true,
+			});
+			expect(nullableReference).toBeNull();
 		},
 		"findOne - should find a model": async () => {
 			const [user] = await insertRandom("user");
