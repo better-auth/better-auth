3e915d774975db7afc92ae0228bcfcc84e0a2f37
Alex Yang <himself65@outlook.com>
2025-10-02 11:53:38 -0700

    refactor: move verification schema to core

--- packages/core/src/db/index.ts
+++ packages/core/src/db/index.ts
@@ -10,6 +10,7 @@ export { coreSchema } from "./schema/shared";
 export { userSchema, type User } from "./schema/user";
 export { accountSchema, type Account } from "./schema/account";
 export { sessionSchema, type Session } from "./schema/session";
+export { verificationSchema, type Verification } from "./schema/verification";
 
 export type {
 	DBFieldAttribute,
