b878d2365eb3c2daf1640674a1d33c9864092e6b
Alex Yang <himself65@outlook.com>
2025-10-06 15:16:20 -0700

    refactor: move SecondaryStorage type to core (#5132)

--- packages/core/src/db/index.ts
+++ packages/core/src/db/index.ts
@@ -7,6 +7,7 @@ import type {
 } from "./type";
 import type { BetterAuthPluginDBSchema } from "./plugin";
 export type { BetterAuthPluginDBSchema } from "./plugin";
+export type { SecondaryStorage } from "./type";
 export { coreSchema } from "./schema/shared";
 export { userSchema, type User } from "./schema/user";
 export { accountSchema, type Account } from "./schema/account";
