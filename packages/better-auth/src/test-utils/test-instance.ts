1d1397ca510e18a18f3af65d4931e1ea39b135a2
Alex Yang <himself65@outlook.com>
2025-10-09 00:27:31 -0700

    refactor: move client plugin types to core

--- packages/better-auth/src/test-utils/test-instance.ts
+++ packages/better-auth/src/test-utils/test-instance.ts
@@ -2,7 +2,8 @@ import { AsyncLocalStorage } from "node:async_hooks";
 import { afterAll } from "vitest";
 import { betterAuth } from "../auth";
 import { createAuthClient } from "../client/vanilla";
-import type { ClientOptions, Session, User } from "../types";
+import type { Session, User } from "../types";
+import type { BetterAuthClientOptions } from "@better-auth/core";
 import { getMigrations } from "../db/get-migration";
 import { parseSetCookieHeader, setCookieToHeader } from "../cookies";
 import type { SuccessContext } from "@better-fetch/fetch";
@@ -33,7 +34,7 @@ afterAll(async () => {
 
 export async function getTestInstance<
 	O extends Partial<BetterAuthOptions>,
-	C extends ClientOptions,
+	C extends BetterAuthClientOptions,
 >(
 	options?: O,
 	config?: {
