1dea9974040dd7859df3189faee642aa2b9a15f2
Dylan Vanmali <dylanvanmali@yahoo.com>
2025-10-09 15:16:19 -0700

    feat: CRUD for oauth client, client.verifyAccessToken, organization support
    feat: move verifyAccessToken into client plugin
    feat: create two client side definitions for verifyAccessToken
    fix: remove customClaims functionality not necessary, advertisedMetadata.claims_supported still supported
    feat: add organization to oauth client
    fix: remake the custom claims functions as objects instead of param based
    feat: introspect accepts optional resource param
    feat: custom claims available for introspected access tokens
    feat: get multiple oauthClients by user_id or organization_id SERVER_ONLY
    feat: client secret storage verify function suggested in #5224
    fix: tie oauthClient to a reference not just an organization
    feat: select_account prompt
    fix: send shallow copy of client to secret deletionon config
    feat: client_secret_expires_at can be set via the create endpoint
    fix: registration reference_id doesn't use activeOrganizationId but instead clientRegistrationReference function
    fix: separate forbidden and internal server error for verifyAccessToken
    fix: ensure default scopes are set for all public endpoints, restricts to server-only endpoints can set undefined scopes

--- packages/better-auth/src/plugins/oauth-provider/consent.ts
+++ packages/better-auth/src/plugins/oauth-provider/consent.ts
@@ -2,24 +2,21 @@ import { APIError } from "../../api";
 import type { OAuthConsent, OAuthOptions, VerificationValue } from "./types";
 import type { GenericEndpointContext } from "@better-auth/core";
 import type { Verification } from "../../types";
-import { generateRandomString } from "../../crypto";
-import { formatErrorURL } from "./authorize";
+import { authorizeEndpoint, formatErrorURL } from "./authorize";
 import { storeToken } from "./utils";
 
 export async function consentEndpoint(
 	ctx: GenericEndpointContext,
 	opts: OAuthOptions,
 ) {
-	const { name: cookieName } = ctx.context.createAuthCookie(
-		"oauth_consent_prompt",
-	);
+	const { name: cookieName } = ctx.context.createAuthCookie("oauth_consent");
 	const storedCode = await ctx.getSignedCookie(cookieName, ctx.context.secret);
 	ctx.setCookie(cookieName, "", {
 		maxAge: 0,
 	});
 	if (!storedCode) {
 		throw new APIError("UNAUTHORIZED", {
-			error_description: "No consent prompt found",
+			error_description: "No consent code found",
 			error: "invalid_request",
 		});
 	}
@@ -74,11 +71,29 @@ export async function consentEndpoint(
 			error: "invalid_request",
 		});
 	}
-	if (!verificationValue.redirectUri) {
+	if (!verificationValue.query) {
 		throw new APIError("UNAUTHORIZED", {
-			error_description: "Missing redirect uri",
+			error_description: "Missing query",
 			error: "invalid_request",
 		});
+	}
+	if (!verificationValue.query.scope) {
+		throw new APIError("UNAUTHORIZED", {
+			error_description: "Missing orginal requested scopes",
+			error: "invalid_request",
+		});
+	}
+
+	// Check scopes if received (can only be equal or lesser than originally requested scopes)
+	const requestedScopes = (ctx.body.scope as string | undefined)?.split(" ");
+	if (requestedScopes) {
+		const originalRequestedScopes = verificationValue.query.scope;
+		if (!requestedScopes.every((sc) => originalRequestedScopes?.includes(sc))) {
+			throw new APIError("BAD_REQUEST", {
+				error_description: "Scope not originally requested",
+				error: "invalid_request",
+			});
+		}
 	}
 
 	// Consent not accepted (ensure it's strictly boolean true)
@@ -87,50 +102,80 @@ export async function consentEndpoint(
 		await ctx.context.internalAdapter.deleteVerificationValue(verification.id);
 		return ctx.json({
 			redirect_uri: formatErrorURL(
-				verificationValue.redirectUri,
+				verificationValue.query.redirect_uri,
 				"access_denied",
 				"User denied access",
-				verificationValue.state,
+				verificationValue.query?.state,
 			),
 		});
 	}
 
 	// Consent accepted
-	const code = generateRandomString(32, "a-z", "A-Z", "0-9");
+	const foundConsent = await ctx.context.adapter
+		.findOne<OAuthConsent>({
+			model: opts.schema?.oauthConsent?.modelName ?? "oauthConsent",
+			where: [
+				{
+					field: "clientId",
+					value: verificationValue.query.client_id,
+				},
+				{
+					field: "userId",
+					value: verificationValue.userId,
+				},
+			],
+		})
+		.then((res) => {
+			if (!res) return undefined;
+			return {
+				...res,
+				scopes: (res.scopes as unknown as string)?.split(" "),
+			} as OAuthConsent & { id: string };
+		});
 	const iat = Math.floor(Date.now() / 1000);
-	const exp = iat + (opts.codeExpiresIn ?? 600);
-
-	await ctx.context.internalAdapter.updateVerificationValue(verification.id, {
-		value: JSON.stringify({
-			...verificationValue,
-			type: "authorization_code",
-		}),
-		identifier: await storeToken(opts.storeTokens, code, "authorization_code"),
-		expiresAt: new Date(exp * 1000),
-	});
 	const consent: OAuthConsent = {
-		clientId: verificationValue.clientId,
+		clientId: verificationValue.query.client_id,
 		userId: verificationValue.userId,
-		scopes: verificationValue.scopes.split(" "),
+		scopes: requestedScopes ?? verificationValue.query.scope.split(" "),
 		consentGiven: true,
 		createdAt: new Date(iat * 1000),
 		updatedAt: new Date(iat * 1000),
 	};
-	await ctx.context.adapter.create({
-		model: opts.schema?.oauthConsent?.modelName ?? "oauthConsent",
-		data: {
-			...consent,
-			scopes: consent.scopes.join(" "),
-		},
-	});
-	const redirectUri = new URL(verificationValue.redirectUri ?? opts.loginPage);
-	redirectUri.searchParams.set("code", code);
-	if (verificationValue.state) {
-		redirectUri.searchParams.set("state", verificationValue.state);
-	}
+	foundConsent?.id
+		? await ctx.context.adapter.update({
+				model: opts.schema?.oauthConsent?.modelName ?? "oauthConsent",
+				where: [
+					{
+						field: "id",
+						value: foundConsent.id,
+					},
+				],
+				update: {
+					scopes: consent.scopes.join(" "),
+					updatedAt: new Date(iat * 1000),
+				},
+			})
+		: await ctx.context.adapter.create({
+				model: opts.schema?.oauthConsent?.modelName ?? "oauthConsent",
+				data: {
+					...consent,
+					scopes: consent.scopes.join(" "),
+				},
+			});
 
-	// Redirect back to application
-	return ctx.json({
-		redirect_uri: redirectUri.toString(),
-	});
+	// Return authorization code
+	const query = {
+		...verificationValue.query,
+		scope: consent.scopes.join(" "),
+	};
+	ctx?.headers?.set("accept", "application/json");
+	if (query.prompt === "consent") {
+		query.prompt = undefined;
+	}
+	ctx.context.verification_id = verification.id;
+	ctx.query = query;
+	const { url } = await authorizeEndpoint(ctx, opts);
+	return {
+		redirect_uri: url,
+	};
 }
