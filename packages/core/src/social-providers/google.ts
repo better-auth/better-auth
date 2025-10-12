e4752230f4866d525b7af47c108c40db3f4217a2
Alex Yang <himself65@outlook.com>
2025-10-12 04:04:05 -0700

    chore: use `tsdown` to bundle package

--- packages/core/src/social-providers/google.ts
+++ packages/core/src/social-providers/google.ts
@@ -1,13 +1,10 @@
 import { betterFetch } from "@better-fetch/fetch";
 import { decodeJwt } from "jose";
 import { BetterAuthError } from "../error";
-import type { OAuthProvider, ProviderOptions } from "@better-auth/core/oauth2";
-import {
-	createAuthorizationURL,
-	validateAuthorizationCode,
-} from "@better-auth/core/oauth2";
-import { logger } from "@better-auth/core/env";
-import { refreshAccessToken } from "@better-auth/core/oauth2";
+import type { OAuthProvider, ProviderOptions } from "../oauth2";
+import { createAuthorizationURL, validateAuthorizationCode } from "../oauth2";
+import { logger } from "../env";
+import { refreshAccessToken } from "../oauth2";
 
 export interface GoogleProfile {
 	aud: string;
