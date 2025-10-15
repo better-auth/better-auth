b19591c949456d43963760ead2a776ecd51b846d
Gideon Ofori Addo <gideonaddoofori@gmail.com>
2025-10-15 14:23:51 +0000

    feat(two-factor): add `twoFactorPage` option to `twoFactorClient` plugin and its logic to handle redirecting users to the specified page when 2FA verification is required

--- packages/better-auth/src/plugins/two-factor/client.ts
+++ packages/better-auth/src/plugins/two-factor/client.ts
@@ -5,13 +5,20 @@ export const twoFactorClient = (
 	options?:
 		| {
 				/**
+				 * the page to redirect if a user needs to verify
+				 * their two factor
+				 *
+				 * @warning This causes a full page reload when used.
+				 */
+				twoFactorPage?: string;
+				/**
 				 * a redirect function to call if a user needs to verify
 				 * their two factor
 				 */
 				onTwoFactorRedirect?: () => void | Promise<void>;
 		  }
 		| undefined,
-) => {
+): BetterAuthClientPlugin => {
 	return {
 		id: "two-factor",
 		$InferServerPlugin: {} as ReturnType<typeof twoFa>,
@@ -34,6 +41,13 @@ export const twoFactorClient = (
 				hooks: {
 					async onSuccess(context) {
 						if (context.data?.twoFactorRedirect) {
+							if (options?.twoFactorPage) {
+								if (typeof window !== "undefined" && window.location) {
+									window.location.href = options.twoFactorPage;
+									return;
+								}
+							}
+
 							if (options?.onTwoFactorRedirect) {
 								await options.onTwoFactorRedirect();
 							}
