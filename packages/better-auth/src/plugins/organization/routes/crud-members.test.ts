49aa55fc28593c94af621ef5f8408c8d0884f000
GautamBytes <manchandanigautam@gmail.com>
2025-10-26 17:12:45 +0000

    lint workflow fix
    Signed-off-by: GautamBytes <manchandanigautam@gmail.com>

--- packages/better-auth/src/plugins/organization/routes/crud-members.test.ts
+++ packages/better-auth/src/plugins/organization/routes/crud-members.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, it, vi, beforeEach } from "vitest";
+import { beforeEach, describe, expect, it, vi } from "vitest";
 import { createAuthClient } from "../../../client";
 import { getTestInstance } from "../../../test-utils/test-instance";
 import { organizationClient } from "../client";
