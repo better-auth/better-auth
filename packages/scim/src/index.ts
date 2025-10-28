e915f65b1b1665a599a112a6223e27a438b31bfb
Jonathan Samines <jn.samines@gmail.com>
2025-10-28 11:00:20 -0600

    chore(scim): create database schemas

---
+++ packages/scim/src/index.ts
@@ -0,0 +1,23 @@
+import type { BetterAuthPlugin } from "better-auth";
+
+export const scim = () => {
+    return {
+        id: "scim",
+        schema: {
+            scimProvider: {
+                fields: {
+                    providerId: {
+                        type: "string",
+                        required: true,
+                        unique: true,
+                    },
+                    scimToken: {
+                        type: "string",
+                        required: true,
+                        unique: true,
+                    },
+                },
+            },
+        }
+    } satisfies BetterAuthPlugin
+}
