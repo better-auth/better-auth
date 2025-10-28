5d1c18ba138d11c54f99b59e44f42ed892b1aa22
Jeffrey Lind <124309394+JeffreyLind3@users.noreply.github.com>
2025-10-28 17:25:08 -0400

    docs: delete leading and trailing whitespaces in className strings (#5650)

--- docs/components/ui/background-beams.tsx
+++ docs/components/ui/background-beams.tsx
@@ -65,7 +65,7 @@ export const BackgroundBeams = React.memo(
 				)}
 			>
 				<svg
-					className=" z-0 h-full w-full pointer-events-none absolute "
+					className="z-0 h-full w-full pointer-events-none absolute"
 					width="100%"
 					height="100%"
 					viewBox="0 0 696 316"
