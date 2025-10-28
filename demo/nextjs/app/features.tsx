a86edb337f4c250aaa0ec64485f5c910e7072796
JeffreyLind3 <jeffrey@jeffreylind.dev>
2025-10-28 15:56:14 -0400

    fix import order

--- demo/nextjs/app/features.tsx
+++ demo/nextjs/app/features.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import { Logo } from "@/components/logo";
 import { AnimatePresence, motion } from "framer-motion";
 import React from "react";
+import { Logo } from "@/components/logo";
 
 export function Features() {
 	return (
