41f2fd648b1e05162efd4c07b17fc2555a4a0929
Jeffrey Lind <124309394+JeffreyLind3@users.noreply.github.com>
2025-10-28 17:25:08 -0400

    docs: delete leading and trailing whitespaces in className strings (#5650)

--- docs/app/page.tsx
+++ docs/app/page.tsx
@@ -33,13 +33,13 @@ export default async function HomePage() {
 					<div className="w-full h-full">
 						<div className="flex flex-col md:flex-row items-center justify-center h-12">
 							<span className="font-medium flex gap-2 text-sm text-zinc-700 dark:text-zinc-300">
-								<span className=" text-zinc-900 dark:text-white/90 hover:text-zinc-950 text-xs md:text-sm dark:hover:text-zinc-100 transition-colors">
+								<span className="text-zinc-900 dark:text-white/90 hover:text-zinc-950 text-xs md:text-sm dark:hover:text-zinc-100 transition-colors">
 									Introducing{" "}
 									<span className="font-semibold">
 										Better Auth Infrastructure
 									</span>
 								</span>
-								<span className=" text-zinc-400 hidden md:block">|</span>
+								<span className="text-zinc-400 hidden md:block">|</span>
 								<Link
 									href="https://better-auth.build"
 									className="font-semibold text-blue-600 dark:text-blue-400 hover:text-blue-700 hidden dark:hover:text-blue-300 transition-colors md:block"
