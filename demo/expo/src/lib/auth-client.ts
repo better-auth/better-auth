5de660a7da5fd42d71cacda686c6fcff629eb3fe
Alex Yang <himself65@outlook.com>
2025-10-30 20:52:52 -0700

    fix: deps

--- demo/expo/src/lib/auth-client.ts
+++ demo/expo/src/lib/auth-client.ts
@@ -2,6 +2,8 @@ import { expoClient } from "@better-auth/expo/client";
 import { createAuthClient } from "better-auth/client";
 import * as SecureStore from "expo-secure-store";
 
+console.log('hello')
+
 export const authClient = createAuthClient({
 	baseURL: "http://localhost:8081",
 	disableDefaultFetchPlugins: true,
