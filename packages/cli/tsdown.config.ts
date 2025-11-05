41cd5ce8b5bea47f8c58089a1cc1d00f3819fdb4
Alex Yang <himself65@outlook.com>
2025-11-05 14:15:37 -0800

    fix: lint

--- packages/cli/tsdown.config.ts
+++ packages/cli/tsdown.config.ts
@@ -3,12 +3,6 @@ import { defineConfig } from "tsdown";
 export default defineConfig({
 	dts: false,
 	format: ["esm"],
-	outExtension({ format }) {
-		if (format === "esm") {
-			return { js: ".mjs", dts: ".d.mts" };
-		}
-		return { js: ".js", dts: ".d.ts" };
-	},
 	entry: ["./src/index.ts"],
 	external: ["better-auth", "better-call"],
 });
