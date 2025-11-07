2ec9485637b38d8e69c57e8c6c8b9373c372e71b
Alex Yang <himself65@outlook.com>
2025-11-07 11:49:37 -0800

    fix: export

--- packages/better-auth/src/index.ts
+++ packages/better-auth/src/index.ts
@@ -24,4 +24,6 @@ export {
 export type { JSONWebKeySet, JWTPayload } from "jose";
 export type * from "zod";
 export type * from "zod/v4";
+// @ts-expect-error
+export type * from "zod/v4/core";
 export { APIError } from "./api";
