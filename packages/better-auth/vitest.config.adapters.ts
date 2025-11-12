7baeed56fb4e18ea8d381b4ecb1ed2abb5496289
ping-maxwell <maxwell.multinite@gmail.com>
2025-11-12 11:30:30 +1000

    fix: adapter vitest config

--- packages/better-auth/vitest.config.adapters.ts
+++ packages/better-auth/vitest.config.adapters.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
 	test: {
-		setupFiles: ["./vitest.setup.ts"],
 		poolOptions: {
 			forks: {
 				execArgv: ["--expose-gc"],
