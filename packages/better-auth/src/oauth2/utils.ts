e293957d651494ad554bd79fab7d735b1accbd14
Firdaus <fai14@pitt.edu>
2025-11-16 05:12:57 -0500

    add 'enc:' to differentiate between encrypted and non-encrypted key

--- packages/better-auth/src/oauth2/utils.ts
+++ packages/better-auth/src/oauth2/utils.ts
@@ -3,24 +3,25 @@ import { symmetricDecrypt, symmetricEncrypt } from "../crypto";
 
 export function decryptOAuthToken(token: string, ctx: AuthContext) {
 	if (!token) return token;
-	if (ctx.options.account?.encryptOAuthTokens) {
+	if (ctx.options.account?.encryptOAuthTokens && token.startsWith('enc:')) {
 		return symmetricDecrypt({
 			key: ctx.secret,
-			data: token,
+			data: token.slice(4),
 		});
 	}
 	return token;
 }
 
-export function setTokenUtil(
+export async function setTokenUtil(
 	token: string | null | undefined,
 	ctx: AuthContext,
 ) {
 	if (ctx.options.account?.encryptOAuthTokens && token) {
-		return symmetricEncrypt({
+		const encryptedToken = await symmetricEncrypt({
 			key: ctx.secret,
 			data: token,
 		});
+		return 'enc:' + encryptedToken
 	}
 	return token;
 }
