8a27a773bb7037f5cc358658826ec309259b64cf
Alex Yang <himself65@outlook.com>
2025-11-17 14:39:24 -0800

    test: use vitest proejcts

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+	test: {
+		projects: ["./packages/*", "./test"],
+	},
 	ssr: {
 		resolve: {
 			// we resolve from source files for unit testing
