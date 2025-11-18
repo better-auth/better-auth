25f2176f8c3fb78c36256b37729cd9157e11a9bc
Blackmamoth <blackmamoth83@gmail.com>
2025-11-18 14:24:16 +0530

    add SessionWithSoftDelete import back in internal-adapter.ts

--- packages/better-auth/src/db/internal-adapter.ts
+++ packages/better-auth/src/db/internal-adapter.ts
@@ -10,7 +10,13 @@ import {
 } from "@better-auth/core/context";
 import type { DBAdapter, Where } from "@better-auth/core/db/adapter";
 import type { InternalLogger } from "@better-auth/core/env";
-import type { Account, Session, User, Verification } from "../types";
+import type {
+	Account,
+	Session,
+	SessionWithSoftDelete,
+	User,
+	Verification,
+} from "../types";
 import { generateId } from "../utils";
 import { getDate } from "../utils/date";
 import { getIp } from "../utils/get-request-ip";
