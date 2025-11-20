23f98db8af82999563bc07d45c6fe99767edcf5f
Mi4BT <tom.bedino@mi4.fr>
2025-11-20 16:18:49 +0100

    feat(hubspot): add hubspot provider

---
+++ packages/core/src/social-providers/hubspot.ts
@@ -0,0 +1,124 @@
+import { decodeJwt } from "jose";
+import { logger } from "../env";
+import { BetterAuthError } from "../error";
+import type { OAuthProvider, ProviderOptions } from "../oauth2";
+import {
+	createAuthorizationURL,
+	refreshAccessToken,
+	validateAuthorizationCode,
+} from "../oauth2";
+
+export interface HubspotProfile {
+	app_id: number;
+	expires_in: number;
+	hub_domain: string;
+	hub_id: number;
+	is_private_distribution: boolean;
+	scopes: string[];
+	signed_access_token: {
+		appId: number;
+		expiresAt: number;
+		hubId: number;
+		hublet: string;
+		installingUserId: number;
+		isPrivateDistribution: boolean;
+		isServiceAccount: boolean;
+		isUserLevel: boolean;
+		newSignature: string;
+		scopeToScopeGroupPks: string;
+		scopes: string;
+		signature: string;
+		trialScopeToScopeGroupPks: string;
+		trialScopes: string;
+		userId: number;
+	};
+	token: string;
+	token_type: string;
+	user: string;
+	user_id: number;
+}
+
+export interface HubspotOptions extends ProviderOptions<HubspotProfile> {
+	clientId: string;
+	clientSecret: string;
+}
+
+export const hubspot = (options: HubspotOptions) => {
+	return {
+		id: "hubspot",
+		name: "Hubspot",
+		async createAuthorizationURL({
+			state,
+			scopes,
+			codeVerifier,
+			redirectURI,
+			loginHint,
+			display,
+		}) {
+			if (!options.clientId || !options.clientSecret) {
+				logger.error(
+					"Client Id and Client Secret is required for Hubspot. Make sure to provide them in the options.",
+				);
+				throw new BetterAuthError("CLIENT_ID_AND_SECRET_REQUIRED");
+			}
+			if (!codeVerifier) {
+				throw new BetterAuthError("codeVerifier is required for Hubspot");
+			}
+			const _scopes = ["oauth"];
+			if (options.scope) _scopes.push(...options.scope);
+			if (scopes) _scopes.push(...scopes);
+			return createAuthorizationURL({
+				id: "hubspot",
+				options,
+				authorizationEndpoint: "https://app.hubspot.com/oauth/authorize",
+				scopes: _scopes,
+				state,
+				codeVerifier,
+				redirectURI,
+			});
+		},
+		validateAuthorizationCode: async ({ code, codeVerifier, redirectURI }) => {
+			return validateAuthorizationCode({
+				code,
+				codeVerifier,
+				redirectURI,
+				options,
+				tokenEndpoint: "https://api.hubapi.com/oauth/v1/access-tokens",
+			});
+		},
+		refreshAccessToken: options.refreshAccessToken
+			? options.refreshAccessToken
+			: async (refreshToken) => {
+					return refreshAccessToken({
+						refreshToken,
+						options: {
+							clientId: options.clientId,
+							clientKey: options.clientKey,
+							clientSecret: options.clientSecret,
+						},
+						tokenEndpoint: "https://api.hubapi.com/oauth/v1/access-tokens",
+					});
+				},
+		async getUserInfo(token) {
+			if (options.getUserInfo) {
+				return options.getUserInfo(token);
+			}
+			if (!token.idToken) {
+				return null;
+			}
+			const user = decodeJwt(token.idToken) as HubspotProfile;
+			const userMap = await options.mapProfileToUser?.(user);
+			return {
+				user: {
+					id: user.user_id,
+					name: user.user,
+					emailVerified: true,
+					email: user.user,
+					...userMap,
+				},
+				data: user,
+			};
+		},
+		options,
+	} satisfies OAuthProvider<HubspotProfile>;
+};
