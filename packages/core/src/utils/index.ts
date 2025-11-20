c89ebb892f7455b7f9f2464dc5fa6d06fb4d8eea
Nikhil Saraf <nsaraf98@gmail.com>
2025-11-20 23:31:11 +0530

    add invariant util

--- packages/core/src/utils/index.ts
+++ packages/core/src/utils/index.ts
@@ -1,1 +1,14 @@
+import { APIError } from "better-call";
+
 export { defineErrorCodes } from "./error-codes";
+
+export function invariant(
+	condition: boolean,
+	message: string,
+): asserts condition {
+	if (!condition) {
+		throw new APIError("PRECONDITION_FAILED", {
+			message,
+		});
+	}
+}
