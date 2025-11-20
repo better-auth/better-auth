4b87b8f4dd55cd7cf22899de9bebd7b01c37a660
Alex Yang <himself65@outlook.com>
2025-11-19 16:33:32 -0800

    fix(oidc-provider): redirect to consent when scope changed

--- test/vitest.config.ts
+++ test/vitest.config.ts
@@ -1,3 +1,17 @@
+import path from "path";
 import { defineProject } from "vitest/config";
 
-export default defineProject({});
+export default defineProject({
+	resolve: {
+		alias: {
+			"better-auth/plugins": path.resolve(
+				__dirname,
+				"../packages/better-auth/src/plugins/index.ts",
+			),
+			"better-auth": path.resolve(
+				__dirname,
+				"../packages/better-auth/src/index.ts",
+			),
+		},
+	},
+});
