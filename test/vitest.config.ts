d3baa72d6a3e399429f26676c612378e5b3e131b
Alex Yang <himself65@outlook.com>
2025-11-19 18:54:56 -0800

    ?

--- test/vitest.config.ts
+++ test/vitest.config.ts
@@ -1,10 +1,3 @@
 import { defineProject } from "vitest/config";
 
-export default defineProject({
-  ssr: {
-    resolve: {
-      // we resolve from source files for unit testing
-      conditions: ["dev-source"],
-    },
-  },
-});
+export default defineProject({});
