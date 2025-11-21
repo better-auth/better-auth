f034383acd7d94e5ac9b0000cbae762e0f36da76
Alex Yang <himself65@outlook.com>
2025-11-20 17:44:44 -0800

    chore: add CORS supports

--- demo/nextjs/app/.well-known/openid-configuration/route.ts
+++ demo/nextjs/app/.well-known/openid-configuration/route.ts
@@ -1,7 +1,13 @@
 import { auth } from "@/lib/auth";
 
 export async function GET() {
-	return auth.api.getOpenIdConfig({
+	const response = await auth.api.getOpenIdConfig({
 		asResponse: true,
 	});
+
+	response.headers.set("Access-Control-Allow-Origin", "*");
+	response.headers.set("Access-Control-Allow-Methods", "GET");
+	response.headers.set("Access-Control-Allow-Headers", "Content-Type");
+
+	return response;
 }
