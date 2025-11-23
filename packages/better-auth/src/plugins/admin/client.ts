77dcf9bd8147d6e58ce553ab6fed4a969dc46731
jslno <joelsolano@jsolano.de>
2025-11-23 13:55:59 +0100

    chore: fix linting

--- packages/better-auth/src/plugins/admin/client.ts
+++ packages/better-auth/src/plugins/admin/client.ts
@@ -6,90 +6,91 @@ import type { admin } from "./admin";
 import { hasPermission } from "./has-permission";
 
 interface AdminClientOptions {
-  ac?: AccessControl | undefined;
-  roles?:
-    | {
-        [key in string]: Role;
-      }
-    | undefined;
+	ac?: AccessControl | undefined;
+	roles?:
+		| {
+				[key in string]: Role;
+		  }
+		| undefined;
 }
 
 export const adminClient = <O extends AdminClientOptions>(
-  options?: O | undefined,
+	options?: O | undefined,
 ) => {
-  type DefaultStatements = typeof defaultStatements;
-  type Statements =
-    O["ac"] extends AccessControl<infer S> ? S : DefaultStatements;
-  type PermissionType = {
-    [key in keyof Statements]?: Array<
-      Statements[key] extends readonly unknown[]
-        ? Statements[key][number]
-        : never
-    >;
-  };
-  type PermissionExclusive =
-    | {
-        /**
-         * @deprecated Use `permissions` instead
-         */
-        permission: PermissionType;
-        permissions?: never | undefined;
-      }
-    | {
-        permissions: PermissionType;
-        permission?: never | undefined;
-      };
+	type DefaultStatements = typeof defaultStatements;
+	type Statements = O["ac"] extends AccessControl<infer S>
+		? S
+		: DefaultStatements;
+	type PermissionType = {
+		[key in keyof Statements]?: Array<
+			Statements[key] extends readonly unknown[]
+				? Statements[key][number]
+				: never
+		>;
+	};
+	type PermissionExclusive =
+		| {
+				/**
+				 * @deprecated Use `permissions` instead
+				 */
+				permission: PermissionType;
+				permissions?: never | undefined;
+		  }
+		| {
+				permissions: PermissionType;
+				permission?: never | undefined;
+		  };
 
-  const roles = {
-    admin: adminAc,
-    user: userAc,
-    ...options?.roles,
-  };
+	const roles = {
+		admin: adminAc,
+		user: userAc,
+		...options?.roles,
+	};
 
-  return {
-    id: "admin-client",
-    $InferServerPlugin: {} as ReturnType<
-      typeof admin<{
-        ac: O["ac"] extends AccessControl
-          ? O["ac"]
-          : AccessControl<DefaultStatements>;
-        roles: O["roles"] extends Record<string, Role>
-          ? O["roles"]
-          : {
-              admin: Role;
-              user: Role;
-            };
-      }>
-    >,
-    getActions: () => ({
-      admin: {
-        checkRolePermission: <
-          R extends O extends { roles: any }
-            ? keyof O["roles"]
-            : "admin" | "user",
-        >(
-          data: PermissionExclusive & {
-            role: R;
-          },
-        ) => {
-          const isAuthorized = hasPermission({
-            role: data.role as string,
-            options: {
-              ac: options?.ac,
-              roles: roles,
-            },
-            permissions: (data.permissions ?? data.permission) as any,
-          });
-          return isAuthorized;
-        },
-      },
-    }),
-    pathMethods: {
-      "/admin/list-users": "GET",
-      "/admin/stop-impersonating": "POST",
-    },
-  } satisfies BetterAuthClientPlugin;
+	return {
+		id: "admin-client",
+		$InferServerPlugin: {} as ReturnType<
+			typeof admin<{
+				ac: O["ac"] extends AccessControl
+					? O["ac"]
+					: AccessControl<DefaultStatements>;
+				roles: O["roles"] extends Record<string, Role>
+					? O["roles"]
+					: {
+							admin: Role;
+							user: Role;
+						};
+			}>
+		>,
+		getActions: () => ({
+			admin: {
+				checkRolePermission: <
+					R extends O extends { roles: any }
+						? keyof O["roles"]
+						: "admin" | "user",
+				>(
+					data: PermissionExclusive & {
+						role: R;
+					},
+				) => {
+					const isAuthorized = hasPermission({
+						role: data.role as string,
+						options: {
+							ac: options?.ac,
+							roles: roles,
+						},
+						permissions: (data.permissions ?? data.permission) as any,
+					});
+					return isAuthorized;
+				},
+			},
+		}),
+		pathMethods: {
+			"/admin/list-users": "GET",
+			"/admin/stop-impersonating": "POST",
+		},
+	} satisfies BetterAuthClientPlugin;
 };
 
-export * from "./types";
 export * from "./access";
+export * from "./types";
