9b5a969ed24780db807d71026627ff2cb823745f
ping-maxwell <maxwell.multinite@gmail.com>
2025-11-28 07:36:21 +1000

    fix: type issues

--- packages/cli/src/commands/init/utility/database.ts
+++ packages/cli/src/commands/init/utility/database.ts
@@ -1,11 +1,16 @@
-import type { DatabaseAdapter } from "../configs/databases.config";
+import type {
+	DatabaseAdapter,
+	DatabasesConfig,
+} from "../configs/databases.config";
 import { databasesConfig } from "../configs/databases.config";
 
-export const getDatabaseCode = (adapter: DatabaseAdapter | null) => {
-	if (!adapter) return null;
+export const getDatabaseCode = <A extends DatabaseAdapter | null>(
+	adapter: A,
+): A extends DatabaseAdapter ? DatabasesConfig : null => {
+	if (!adapter) return null as any;
 	const database = databasesConfig.find(
 		(database) => database.adapter === adapter,
 	)!;
 
-	return database;
+	return database as any;
 };
