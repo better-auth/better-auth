56299a8bcdb2a5a43befa99d234f020f2af6a28b
jslno <joelsolano@jsolano.de>
2025-11-29 18:46:19 +0100

    chore: cleanup

--- demo/nextjs/components/wrapper.tsx
+++ demo/nextjs/components/wrapper.tsx
@@ -7,7 +7,7 @@ import { ThemeToggle } from "./theme-toggle";
 
 export function Wrapper(props: { children: React.ReactNode }) {
 	return (
-		<div className="min-h-screen w-full dark:bg-black bg-white  dark:bg-grid-small-white/[0.2] bg-grid-small-black/[0.2] relative flex justify-center">
+		<div className="min-h-screen w-full dark:bg-black bg-white  dark:bg-grid-small-white/2 bg-grid-small-black/2 relative flex justify-center">
 			<div className="absolute pointer-events-none inset-0 md:flex items-center justify-center dark:bg-black bg-white mask-[radial-gradient(ellipse_at_center,transparent_20%,black)] hidden"></div>
 			<div className="bg-white dark:bg-black border-b py-2 flex justify-between items-center border-border absolute z-50 w-full lg:w-8/12 px-4 md:px-1">
 				<Link href="/">
