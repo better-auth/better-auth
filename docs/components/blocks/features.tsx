cc3ebba946d4292d4f6a5cf5752e5b07eb3a34c6
jslno <joelsolano@jsolano.de>
2025-11-29 15:38:44 +0100

    chore: apply remaining canonical class names

--- docs/components/blocks/features.tsx
+++ docs/components/blocks/features.tsx
@@ -78,8 +78,8 @@ export const Grid = ({
 		[Math.floor(Math.random() * 4) + 7, Math.floor(Math.random() * 6) + 1],
 	];
 	return (
-		<div className="pointer-events-none absolute left-1/2 top-0  -ml-20 -mt-2 h-full w-full [mask-image:linear-gradient(white,transparent)]">
-			<div className="absolute inset-0 bg-gradient-to-r  [mask-image:radial-gradient(farthest-side_at_top,white,transparent)] dark:from-zinc-900/30 from-zinc-100/30 to-zinc-300/30 dark:to-zinc-900/30 opacity-100">
+		<div className="pointer-events-none absolute left-1/2 top-0  -ml-20 -mt-2 h-full w-full mask-[linear-gradient(white,transparent)]">
+			<div className="absolute inset-0 bg-linear-to-r mask-[radial-gradient(farthest-side_at_top,white,transparent)] dark:from-zinc-900/30 from-zinc-100/30 to-zinc-300/30 dark:to-zinc-900/30 opacity-100">
 				<GridPattern
 					width={size ?? 20}
 					height={size ?? 20}
