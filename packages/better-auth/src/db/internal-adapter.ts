49a7c300f70af022c3e5078d070ebf8125118621
Ridhim-RR <ridhimraizada.rr@gmail.com>
2025-11-30 19:28:26 +0530

    chore(lint): linting issue resolved

--- packages/better-auth/src/db/internal-adapter.ts
+++ packages/better-auth/src/db/internal-adapter.ts
@@ -272,10 +272,7 @@ export const createInternalAdapter = (
 			const headers = ctx?.headers || ctx?.request?.headers;
 			const { id: _, ...rest } = override || {};
 			//we're parsing default values for session additional fields
-			const defaultAdditionalFields = parseSessionInput(
-				options,
-				{},
-			);
+			const defaultAdditionalFields = parseSessionInput(options, {});
 			const data: Omit<Session, "id"> = {
 				ipAddress:
 					ctx?.request || ctx?.headers
