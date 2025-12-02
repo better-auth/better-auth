e6eccc30a823a16b6ffea1daed98af9bb2730007
jslno <joelsolano@jsolano.de>
2025-12-02 15:41:25 +0100

    docs: add sidebar tab for canary docs

--- docs/components/side-bar.tsx
+++ docs/components/side-bar.tsx
@@ -27,12 +27,16 @@ export default function ArticleLayout() {
 	const [group, setGroup] = useState("docs");
 
 	useEffect(() => {
-		const grp = pathname.includes("examples") ? "examples" : "docs";
+		const grp = pathname.includes("examples")
+			? "examples"
+			: hasSubdomain("canary")
+				? "docs-canary"
+				: "docs";
 		setGroup(grp);
 		setCurrentOpen(getDefaultValue());
 	}, [pathname]);
 
-	const cts = group === "docs" ? contents : examples;
+	const cts = group.startsWith("docs") ? contents : examples;
 
 	return (
 		<div className={cn("fixed start-0 top-0")}>
@@ -175,6 +179,30 @@ const tabs = [
 		description: "get started, concepts, and plugins",
 	},
 	{
+		value: "docs-canary",
+		icon: (
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				width="1.4em"
+				height="1.4em"
+				viewBox="0 0 24 24"
+			>
+				<path
+					fill="currentColor"
+					d="M4.727 2.733c.306-.308.734-.508 1.544-.618C7.105 2.002 8.209 2 9.793 2h4.414c1.584 0 2.688.002 3.522.115c.81.11 1.238.31 1.544.618c.305.308.504.74.613 1.557c.112.84.114 1.955.114 3.552V18H7.426c-1.084 0-1.462.006-1.753.068c-.513.11-.96.347-1.285.667c-.11.108-.164.161-.291.505A1.3 1.3 0 0 0 4 19.7V7.842c0-1.597.002-2.711.114-3.552c.109-.816.308-1.249.613-1.557"
+					opacity=".5"
+				></path>
+				<path
+					fill="currentColor"
+					d="M20 18H7.426c-1.084 0-1.462.006-1.753.068c-.513.11-.96.347-1.285.667c-.11.108-.164.161-.291.505s-.107.489-.066.78l.022.15c.11.653.31.998.616 1.244c.307.246.737.407 1.55.494c.837.09 1.946.092 3.536.092h4.43c1.59 0 2.7-.001 3.536-.092c.813-.087 1.243-.248 1.55-.494c.2-.16.354-.362.467-.664H8a.75.75 0 0 1 0-1.5h11.975c.018-.363.023-.776.025-1.25M7.25 7A.75.75 0 0 1 8 6.25h8a.75.75 0 0 1 0 1.5H8A.75.75 0 0 1 7.25 7M8 9.75a.75.75 0 0 0 0 1.5h5a.75.75 0 0 0 0-1.5z"
+				></path>
+			</svg>
+		),
+		title: "Docs",
+		badge: "Canary",
+		description: "get started, concepts, and plugins",
+	},
+	{
 		value: "examples",
 		icon: (
 			<svg
@@ -208,6 +236,26 @@ const tabs = [
 	},
 ];
 
+function hasSubdomain(expected: string) {
+	const parts = window.location.hostname.split(".");
+	if (parts.length < 3) return false;
+
+	return parts[0] === expected;
+}
+
+function changeSubdomain(newSubdomain: string | null) {
+	const parts = window.location.hostname.split(".");
+	if (parts.length < 2) {
+		// For dev we dont change anything
+		return "";
+	}
+	const domain = parts.slice(-2).join(".");
+	if (newSubdomain !== null) {
+		newSubdomain += ".";
+	}
+	return `${window.location.protocol}//${newSubdomain}${domain}`;
+}
+
 function SidebarTab({
 	group,
 	setGroup,
@@ -223,8 +271,11 @@ function SidebarTab({
 			value={group}
 			onValueChange={(val) => {
 				setGroup(val);
+				const isCanary = hasSubdomain("canary");
 				if (val === "docs") {
-					router.push("/docs");
+					router.push(`${!isCanary ? changeSubdomain("canary") : ""}/docs`);
+				} else if (val === "docs-canary") {
+					router.push(`${isCanary ? changeSubdomain(null) : ""}/docs`);
 				} else {
 					router.push("/docs/examples");
 				}
@@ -236,6 +287,14 @@ function SidebarTab({
 						<div className="flex items-center gap-1 -ml-0.5">
 							{selected.icon}
 							{selected.title}
+							{selected.badge ? (
+								<Badge
+									variant="outline"
+									className="ms-1.5 text-[0.65rem] text-muted-foreground"
+								>
+									{selected.badge}
+								</Badge>
+							) : null}
 						</div>
 						<p className="text-xs text-muted-foreground">
 							{selected.description}
@@ -253,6 +312,14 @@ function SidebarTab({
 						<div className="flex items-center gap-1">
 							{tab.icon}
 							{tab.title}
+							{tab.badge ? (
+								<Badge
+									variant="outline"
+									className="ms-1.5 text-[0.65rem] text-muted-foreground"
+								>
+									{tab.badge}
+								</Badge>
+							) : null}
 						</div>
 						<p className="text-xs text-muted-foreground">{tab.description}</p>
 					</SelectItem>
