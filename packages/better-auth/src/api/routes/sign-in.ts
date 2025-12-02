021327ee95c9bf053f44239c0c6c0929b8d1f97c
Paola Estefanía de Campos <paolaestefania.decampos@gmail.com>
2025-12-02 15:25:39 -0300

    fix: lint

--- packages/better-auth/src/api/routes/sign-in.ts
+++ packages/better-auth/src/api/routes/sign-in.ts
@@ -372,25 +372,25 @@ export const signInEmail = <O extends BetterAuthOptions>() =>
 					.default(true)
 					.optional(),
 			}),
-		metadata: {
-			allowedMediaTypes: [
-				"application/x-www-form-urlencoded",
-				"application/json",
-			],
-			$Infer: {
-				body: {} as {
-					email: string;
-					password: string;
-					callbackURL?: string | undefined;
-					rememberMe?: boolean | undefined;
+			metadata: {
+				allowedMediaTypes: [
+					"application/x-www-form-urlencoded",
+					"application/json",
+				],
+				$Infer: {
+					body: {} as {
+						email: string;
+						password: string;
+						callbackURL?: string | undefined;
+						rememberMe?: boolean | undefined;
+					},
+					returned: {} as {
+						redirect: boolean;
+						token: string;
+						url?: string | undefined;
+						user: InferUser<O>;
+					},
 				},
-				returned: {} as {
-					redirect: boolean;
-					token: string;
-					url?: string | undefined;
-					user: InferUser<O>;
-				},
-			},
 				openapi: {
 					operationId: "signInEmail",
 					description: "Sign in with email and password",
