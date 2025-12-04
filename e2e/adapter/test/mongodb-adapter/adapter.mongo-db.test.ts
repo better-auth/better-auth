ee7ae3ad18d8c34196534d2031539489d972fdf0
Alex Yang <himself65@outlook.com>
2025-12-03 16:30:07 -0800

    refactor: separate adapters into different packages

---
+++ e2e/adapter/test/mongodb-adapter/adapter.mongo-db.test.ts
@@ -0,0 +1,41 @@
+import { mongodbAdapter } from "@better-auth/mongo-adapter";
+import { MongoClient, ObjectId } from "mongodb";
+import { testAdapter } from "../test-adapter";
+import {
+	authFlowTestSuite,
+	joinsTestSuite,
+	normalTestSuite,
+	transactionsTestSuite,
+} from "../tests";
+
+const dbClient = async (connectionString: string, dbName: string) => {
+	const client = new MongoClient(connectionString);
+	await client.connect();
+	const db = client.db(dbName);
+	return { db, client };
+};
+
+const { db, client } = await dbClient(
+	"mongodb://127.0.0.1:27017",
+	"better-auth",
+);
+
+const { execute } = await testAdapter({
+	adapter: (options) => {
+		return mongodbAdapter(db, {
+			transaction: false,
+		});
+	},
+	runMigrations: async (betterAuthOptions) => {},
+	tests: [
+		normalTestSuite(),
+		authFlowTestSuite(),
+		transactionsTestSuite(),
+		joinsTestSuite(),
+		// numberIdTestSuite(), // no support
+		// uuidTestSuite() // no support
+	],
+	customIdGenerator: () => new ObjectId().toHexString(),
+});
+
+execute();
