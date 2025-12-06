fa8ce158a538f9ebded41c242a77eaf117ea8e43
jslno <joelsolano@jsolano.de>
2025-12-06 17:57:58 +0100

    chore: fix linting

--- packages/better-auth/src/plugins/admin/routes.ts
+++ packages/better-auth/src/plugins/admin/routes.ts
@@ -1513,9 +1513,8 @@ const userHasPermissionBodySchema = z
  */
 export const userHasPermission = <O extends AdminOptions>(opts: O) => {
 	type DefaultStatements = typeof defaultStatements;
-	type Statements = O["ac"] extends AccessControl<infer S>
-		? S
-		: DefaultStatements;
+	type Statements =
+		O["ac"] extends AccessControl<infer S> ? S : DefaultStatements;
 
 	type PermissionType = {
 		[key in keyof Statements]?: Array<
