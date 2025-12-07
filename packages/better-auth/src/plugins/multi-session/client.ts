46b1d07dc7f52593d67a6b2c0d0f1a3cc7ab7328
jslno <joelsolano@jsolano.de>
2025-12-07 17:16:44 +0100

    feat: update client

--- packages/better-auth/src/plugins/multi-session/client.ts
+++ packages/better-auth/src/plugins/multi-session/client.ts
@@ -1,10 +1,30 @@
 import type { BetterAuthClientPlugin } from "@better-auth/core";
+import type { DBFieldAttribute } from "@better-auth/core/db";
 import type { multiSession } from ".";
 
-export const multiSessionClient = () => {
+export type MultiSessionClientOptions = {
+	schema?:
+		| {
+				user?:
+					| {
+							additionalFields?: Record<string, DBFieldAttribute> | undefined;
+					  }
+					| undefined;
+				session?:
+					| {
+							additionalFields?: Record<string, DBFieldAttribute> | undefined;
+					  }
+					| undefined;
+		  }
+		| undefined;
+};
+
+export const multiSessionClient = <O extends MultiSessionClientOptions>(
+	options?: O | undefined,
+) => {
 	return {
 		id: "multi-session",
-		$InferServerPlugin: {} as ReturnType<typeof multiSession>,
+		$InferServerPlugin: {} as ReturnType<typeof multiSession<O>>,
 		atomListeners: [
 			{
 				matcher(path) {
