d5d5c3b1c9b972e6d3daf1857f5e031d638b0e40
ping-maxwell <maxwell.multinite@gmail.com>
2025-12-09 06:03:35 +1000

    fix: chore spelling

--- packages/better-auth/src/adapters/kysely-adapter/kysely-adapter.ts
+++ packages/better-auth/src/adapters/kysely-adapter/kysely-adapter.ts
@@ -607,7 +607,7 @@ export const kyselyAdapter = (
 					: true,
 			supportsJSON:
 				config?.type === "postgres"
-					? true // even if there is JSON support, only pg supports passing direct jsons, all others must stringify
+					? true // even if there is JSON support, only pg supports passing direct json, all others must stringify
 					: false,
 			supportsArrays: false, // Even if field supports JSON, we must pass stringified arrays to the database.
 			supportsUUIDs: config?.type === "postgres" ? true : false,
