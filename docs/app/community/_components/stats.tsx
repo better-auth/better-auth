db1a4d96cdbc83675f6a17a37b208873aab624d5
Taesu <bytaesu@gmail.com>
2025-12-09 01:04:39 +0900

    chore: apply current changes

--- docs/app/community/_components/stats.tsx
+++ docs/app/community/_components/stats.tsx
@@ -28,8 +28,8 @@ export default function Stats({
 						fill="none"
 						stroke="#ffffff"
 						strokeLinecap="round"
-						strokeLinejoin="round"
-						strokeWidth="2"
+						stroke-linejoin="round"
+						stroke-width="2"
 						d="M9 8a3 3 0 1 0 6 0M3 16.8V7.2c0-1.12 0-1.68.218-2.108c.192-.377.497-.682.874-.874C4.52 4 5.08 4 6.2 4h11.6c1.12 0 1.68 0 2.107.218c.377.192.683.497.875.874c.218.427.218.987.218 2.105v9.607c0 1.118 0 1.677-.218 2.104a2 2 0 0 1-.875.874c-.427.218-.986.218-2.104.218H6.197c-1.118 0-1.678 0-2.105-.218a2 2 0 0 1-.874-.874C3 18.48 3 17.92 3 16.8"
 					/>
 				</svg>
