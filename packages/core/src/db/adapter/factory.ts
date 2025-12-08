9a2a898f2ed1b9a306183820fab195d1f301a892
ping-maxwell <maxwell.multinite@gmail.com>
2025-12-08 12:08:23 +1000

    Update factory.ts

--- packages/core/src/db/adapter/factory.ts
+++ packages/core/src/db/adapter/factory.ts
@@ -220,6 +220,7 @@ export const createAdapterFactory =
 					try {
 						value = new Date(value);
 					} catch {
+						const logger = createLogger(options.logger);
 						logger.error("[Adapter Factory] Failed to convert string to date", {
 							value,
 							field,
