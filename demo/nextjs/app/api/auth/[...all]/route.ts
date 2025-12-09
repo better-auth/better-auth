48c8a214d6dc83e797d59c237c46b6d3df844a85
Jonathan Samines <jn.samines@gmail.com>
2025-12-08 21:33:35 -0600

    chore(demo): add basic SCIM demo

--- demo/nextjs/app/api/auth/[...all]/route.ts
+++ demo/nextjs/app/api/auth/[...all]/route.ts
@@ -1,4 +1,4 @@
 import { toNextJsHandler } from "better-auth/next-js";
 import { auth } from "@/lib/auth";
 
-export const { GET, POST } = toNextJsHandler(auth);
+export const { POST, GET, PUT, PATCH, DELETE } = toNextJsHandler(auth);
