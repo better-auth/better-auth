862bba4c652e11e3ca8cfebcb40da8dd4fb4b756
yacobmole <me@jacobmole.xyz>
2025-12-11 10:35:21 +1100

    chore: lint & format

--- packages/better-auth/src/auth/custom.test.ts
+++ packages/better-auth/src/auth/custom.test.ts
@@ -3,25 +3,25 @@ import type { Auth } from "../types";
 import { betterAuth } from "./custom";
 
 describe("auth-custom", () => {
-  test("default auth type should be okay (no interceptor)", () => {
-    const auth = betterAuth({});
-    type T = typeof auth;
-    expectTypeOf<T>().toEqualTypeOf<Auth>();
-  });
-  test("default auth type should be okay (with interceptor)", () => {
-    const auth = betterAuth({}, (ctx) => ctx);
-    type T = typeof auth;
-    expectTypeOf<T>().toEqualTypeOf<Auth>();
-  });
-  test("an invalid interceptor type should throw", () => {
-    expect(() => betterAuth({}, "not an interceptor" as any)).toThrow();
-  });
-  test("context should be modified all the way down", async () => {
-    const auth = betterAuth({}, (ctx) => {
-      ctx.appName = "My Different App Name";
-      return ctx;
-    });
-    const newContext = await auth.$context;
-    expect(newContext.appName).toBe("My Different App Name");
-  });
+	test("default auth type should be okay (no interceptor)", () => {
+		const auth = betterAuth({});
+		type T = typeof auth;
+		expectTypeOf<T>().toEqualTypeOf<Auth>();
+	});
+	test("default auth type should be okay (with interceptor)", () => {
+		const auth = betterAuth({}, (ctx) => ctx);
+		type T = typeof auth;
+		expectTypeOf<T>().toEqualTypeOf<Auth>();
+	});
+	test("an invalid interceptor type should throw", () => {
+		expect(() => betterAuth({}, "not an interceptor" as any)).toThrow();
+	});
+	test("context should be modified all the way down", async () => {
+		const auth = betterAuth({}, (ctx) => {
+			ctx.appName = "My Different App Name";
+			return ctx;
+		});
+		const newContext = await auth.$context;
+		expect(newContext.appName).toBe("My Different App Name");
+	});
 });
