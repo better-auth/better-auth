5b07d8cc9e7a29ebf8def517f8265a1c2f533502
Joél Solano <joelsolano@jsolano.de>
2025-12-10 10:09:48 +0100

    refactor: allow inferring plain schemas using `inferAdditionalFields`

--- packages/better-auth/src/plugins/additional-fields/client.ts
+++ packages/better-auth/src/plugins/additional-fields/client.ts
@@ -5,82 +5,98 @@ import type {
 } from "@better-auth/core";
 import type { DBFieldAttribute } from "@better-auth/core/db";
 
-export const inferAdditionalFields = <
-	T,
-	S extends {
-		user?:
-			| {
-					[key: string]: DBFieldAttribute;
-			  }
-			| undefined;
-		session?:
-			| {
-					[key: string]: DBFieldAttribute;
-			  }
-			| undefined;
-	} = {},
->(
-	schema?: S | undefined,
-) => {
-	type Opts = T extends BetterAuthOptions
-		? T
-		: T extends {
-					options: BetterAuthOptions;
-				}
-			? T["options"]
-			: never;
+type SchemaInput = {
+	user?:
+		| {
+				[key: string]: DBFieldAttribute;
+		  }
+		| undefined;
+	session?:
+		| {
+				[key: string]: DBFieldAttribute;
+		  }
+		| undefined;
+};
 
-	type Plugin = Opts extends never
-		? S extends {
-				user?:
-					| {
-							[key: string]: DBFieldAttribute;
-					  }
-					| undefined;
-				session?:
-					| {
-							[key: string]: DBFieldAttribute;
-					  }
-					| undefined;
+type InferOpts<T> = T extends BetterAuthOptions
+	? T
+	: T extends {
+				options: BetterAuthOptions;
 			}
+		? T["options"]
+		: never;
+
+type InferSchema<T, S extends SchemaInput> = InferOpts<T> extends infer Opts
+	? S extends SchemaInput
+		? {
+				user: {
+					fields: S["user"] extends object ? S["user"] : {};
+				};
+				session: {
+					fields: S["session"] extends object ? S["session"] : {};
+				};
+			}
+		: Opts extends BetterAuthOptions
 			? {
-					id: "additional-fields-client";
-					schema: {
-						user: {
-							fields: S["user"] extends object ? S["user"] : {};
-						};
-						session: {
-							fields: S["session"] extends object ? S["session"] : {};
-						};
+					user: {
+						fields: Opts["user"] extends {
+							additionalFields: infer U;
+						}
+							? U
+							: {};
+					};
+					session: {
+						fields: Opts["session"] extends {
+							additionalFields: infer U;
+						}
+							? U
+							: {};
 					};
 				}
 			: never
-		: Opts extends BetterAuthOptions
-			? {
-					id: "additional-fields";
-					schema: {
-						user: {
-							fields: Opts["user"] extends {
-								additionalFields: infer U;
-							}
-								? U
-								: {};
-						};
-						session: {
-							fields: Opts["session"] extends {
-								additionalFields: infer U;
-							}
-								? U
-								: {};
-						};
-					};
-				}
-			: never;
+	: never;
+
+type InferServerPlugin<T, S extends SchemaInput> = InferOpts<T> extends never
+	? S extends SchemaInput
+		? {
+				id: "additional-fields-client";
+				schema: InferSchema<T, S>;
+			}
+		: never
+	: InferOpts<T> extends BetterAuthOptions
+		? {
+				id: "additional-fields";
+				schema: InferSchema<T, S>;
+			}
+		: never;
+
+type InferPlugin<T, S extends SchemaInput> = {
+	id: "additional-fields-client";
+	$InferServerPlugin: InferServerPlugin<T, S> extends BetterAuthPlugin
+		? InferServerPlugin<T, S>
+		: undefined;
+};
+
+export function inferAdditionalFields<T, S extends SchemaInput = {}>(
+	schema?: S | undefined,
+): InferPlugin<T, S>;
+export function inferAdditionalFields<T, S extends SchemaInput = {}>(
+	flag: "asPlugin",
+	schema?: S | undefined,
+): InferPlugin<T, S>;
+export function inferAdditionalFields<T, S extends SchemaInput = {}>(
+	flag: "asSchema",
+	schema?: S | undefined,
+): InferSchema<T, S>;
+export function inferAdditionalFields<T, S extends SchemaInput>(
+	schema?: string | SchemaInput | undefined,
+) {
+	if (schema === "asSchema") {
+		return {} as InferSchema<T, S>;
+	}
 
 	return {
 		id: "additional-fields-client",
-		$InferServerPlugin: {} as Plugin extends BetterAuthPlugin
-			? Plugin
-			: undefined,
-	} satisfies BetterAuthClientPlugin;
-};
+		$InferServerPlugin: {},
+	} as InferPlugin<T, S> satisfies BetterAuthClientPlugin;
+}
