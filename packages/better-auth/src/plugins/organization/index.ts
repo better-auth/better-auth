af7a90cc97e0df6d5eb1a5c69f815ac6524a464a
Josh Sullivan <jaysche.sullivan@gmail.com>
2025-12-12 22:28:16 -0500

    lint fix

--- packages/better-auth/src/plugins/organization/index.ts
+++ packages/better-auth/src/plugins/organization/index.ts
@@ -1,5 +1,5 @@
 export type * from "./access";
-export * from './adapter';
+export * from "./adapter";
 export * from "./organization";
 export type * from "./schema";
 export type * from "./types";
