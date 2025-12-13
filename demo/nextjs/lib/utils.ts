33ce18634e357af419592ee233035b04c0929a0d
Taesu <bytaesu@gmail.com>
2025-12-13 14:09:23 +0900

    separate util

--- demo/nextjs/lib/utils.ts
+++ demo/nextjs/lib/utils.ts
@@ -5,3 +5,12 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
 }
+
+export async function convertImageToBase64(file: File): Promise<string> {
+	return new Promise((resolve, reject) => {
+		const reader = new FileReader();
+		reader.onloadend = () => resolve(reader.result as string);
+		reader.onerror = reject;
+		reader.readAsDataURL(file);
+	});
+}
