2025b4b73342138cc2f0c1d3ca2ee7331f7556ac
Bereket Engida <Bekacru@gmail.com>
2025-12-15 14:15:24 -0800

    add logs

--- packages/better-auth/src/context/create-context.ts
+++ packages/better-auth/src/context/create-context.ts
@@ -104,6 +104,12 @@ export async function createAuthContext(
 	const logger = createLogger(options.logger);
 	const baseURL = getBaseURL(options.baseURL, options.basePath);
 
+	let displayError = false;
+	if(!baseURL && !displayError) {
+		logger.error(`[better-auth] Base URL could not be determined. Please set a valid base URL using the baseURL config option or the BETTER_AUTH_BASE_URL environment variable. Without this, callbacks and redirects may not work correctly.`);
+		displayError = true;
+	}
+
 	const secret =
 		options.secret ||
 		env.BETTER_AUTH_SECRET ||
