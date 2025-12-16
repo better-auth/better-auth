8cfbccddd3a48f419682b35936dbeda6322520ed
Taesu <bytaesu@gmail.com>
2025-12-16 11:35:28 +0900

    button asChild

--- demo/nextjs/components/entry-button.tsx
+++ demo/nextjs/components/entry-button.tsx
@@ -16,8 +16,8 @@ export default EntryButton;
 
 const SignInButton = () => {
 	return (
-		<Link href="/sign-in" passHref>
-			<Button className="gap-2" variant="default">
+		<Button className="gap-2" variant="default" asChild>
+			<Link href="/sign-in">
 				<svg
 					xmlns="http://www.w3.org/2000/svg"
 					width="1.2em"
@@ -27,28 +27,28 @@ const SignInButton = () => {
 					<path
 						fill="currentColor"
 						d="M5 3H3v4h2V5h14v14H5v-2H3v4h18V3zm12 8h-2V9h-2V7h-2v2h2v2H3v2h10v2h-2v2h2v-2h2v-2h2z"
-					></path>
+					/>
 				</svg>
 				<span>Sign In</span>
-			</Button>
-		</Link>
+			</Link>
+		</Button>
 	);
 };
 
 const DashboardButton = () => {
 	return (
-		<Link href="/dashboard" passHref>
-			<Button className="gap-2" variant="default">
+		<Button className="gap-2" variant="default" asChild>
+			<Link href="/dashboard">
 				<svg
 					xmlns="http://www.w3.org/2000/svg"
 					width="1.2em"
 					height="1.2em"
 					viewBox="0 0 24 24"
 				>
-					<path fill="currentColor" d="M2 3h20v18H2zm18 16V7H4v12z"></path>
+					<path fill="currentColor" d="M2 3h20v18H2zm18 16V7H4v12z" />
 				</svg>
 				<span>Dashboard</span>
-			</Button>
-		</Link>
+			</Link>
+		</Button>
 	);
 };
