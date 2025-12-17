fdd730c66db19fd8b7cb501e686a359305fc70f2
Paola Estefanía de Campos <paolaestefania.decampos@gmail.com>
2025-12-17 18:33:19 -0300

    ignore ex port

--- packages/sso/src/saml/assertions.ts
+++ packages/sso/src/saml/assertions.ts
@@ -7,7 +7,7 @@ export interface AssertionCounts {
 	total: number;
 }
 
-/** @knip used in tests */
+/** @lintignore used in tests */
 export function countAssertions(xml: string): AssertionCounts {
 	let parsed: unknown;
 	try {
