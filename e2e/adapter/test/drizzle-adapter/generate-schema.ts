6602c7fb03b656a3d2cf88cbc514e96418b7d3fc
Alex Yang <himself65@outlook.com>
2025-12-18 14:58:58 +0800

    fix: imports

--- e2e/adapter/test/drizzle-adapter/generate-schema.ts
+++ e2e/adapter/test/drizzle-adapter/generate-schema.ts
@@ -42,7 +42,7 @@ export const generateDrizzleSchema = async (
 	};
 
 	const { generateSchema } = (await i(
-		join(import.meta.dirname, "../../../../packages/cli/src/generators/index"),
+		join(import.meta.dirname, "./../../../../../packages/cli/src/generators/index"),
 	)) as {
 		generateSchema: (opts: {
 			adapter: DBAdapter<BetterAuthOptions>;
