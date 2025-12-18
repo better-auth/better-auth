21638ecf3afe2a7d5be648ce2ecb4dfda4137009
Kasper Sprengers <Diabl0570@hotmail.com>
2025-12-18 10:53:35 +0100

    fix: use operator in list members where clause

--- packages/better-auth/src/plugins/organization/adapter.ts
+++ packages/better-auth/src/plugins/organization/adapter.ts
@@ -135,6 +135,8 @@ export const getOrgAdapter = <O extends OrganizationOptions>(
 									{
 										field: data.filter?.field,
 										value: data.filter?.value,
+										...(data?.filter?.operator ? { operator: data.filter?.operator } : {}),
+
 									},
 								]
 							: []),
@@ -154,6 +156,7 @@ export const getOrgAdapter = <O extends OrganizationOptions>(
 									{
 										field: data.filter?.field,
 										value: data.filter?.value,
+										...(data?.filter?.operator ? { operator: data.filter?.operator } : {}),
 									},
 								]
 							: []),
