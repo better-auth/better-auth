8d42a06ed55d94d9369986973326269c96444dde
Dylan Vanmali <dylanvanmali@yahoo.com>
2025-12-19 01:04:51 -0800

    docs: suggestions and update schema

--- docs/components/sidebar-content.tsx
+++ docs/components/sidebar-content.tsx
@@ -1712,15 +1712,14 @@ C0.7,239.6,62.1,0.5,62.2,0.4c0,0,54,13.8,119.9,30.8S302.1,62,302.2,62c0.2,0,0.2,
 						<circle cx="7" cy="25" r="1" fill="currentColor"></circle>
 					</svg>
 				),
-				isNew: true,
 			},
 			{
 				title: "OAuth Provider",
 				href: "/docs/plugins/oauth-provider",
 				icon: () => (
 					<svg
-						width="32"
-						height="32"
+						width="1.2em"
+						height="1.2em"
 						viewBox="0 0 32 32"
 						fill="none"
 						xmlns="http://www.w3.org/2000/svg"
