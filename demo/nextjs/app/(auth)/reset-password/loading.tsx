19a611854febeb875cff3eaf11f8df518e62ee25
Alex Yang <himself65@outlook.com>
2025-12-20 15:26:54 +0800

    chore(demo): add loading component for `reset-password`

---
+++ demo/nextjs/app/(auth)/reset-password/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+	return (
+		<div className="flex flex-col items-center justify-center min-h-[calc(100vh-10rem)]">
+			<div className="w-87.5 h-50 bg-muted/50 animate-pulse rounded-lg" />
+		</div>
+	);
+}
