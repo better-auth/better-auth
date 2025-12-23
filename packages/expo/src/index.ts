ec38c2293868fd4290a4eed890edf3a1c180d612
Taesu <166604494+bytaesu@users.noreply.github.com>
2025-12-23 12:06:53 +0900

    fix(expo): add missing matcher paths (#6939)

--- packages/expo/src/index.ts
+++ packages/expo/src/index.ts
@@ -47,7 +47,9 @@ export const expo = (options?: ExpoOptions | undefined) => {
 					matcher(context) {
 						return !!(
 							context.path?.startsWith("/callback") ||
-							context.path?.startsWith("/oauth2/callback")
+							context.path?.startsWith("/oauth2/callback") ||
+							context.path?.startsWith("/magic-link/verify") ||
+							context.path?.startsWith("/verify-email")
 						);
 					},
 					handler: createAuthMiddleware(async (ctx) => {
