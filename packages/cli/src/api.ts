dba3f8bb5900e1c528c87fd9eba6c7ae9f952314
Alex Yang <himself65@outlook.com>
2026-01-02 15:37:33 +0800

    chore: lint fix

--- packages/cli/src/api.ts
+++ packages/cli/src/api.ts
@@ -1,6 +1,9 @@
-export { generateDrizzleSchema } from "./generators/drizzle";
-export { generatePrismaSchema } from "./generators/prisma";
-export { generateKyselySchema } from "./generators/kysely";
-export { generateSchema, adapters } from "./generators";
-export type { SchemaGenerator, SchemaGeneratorResult } from "./generators/types";
 export type { DBAdapter } from "@better-auth/core/db/adapter";
+export { adapters, generateSchema } from "./generators";
+export { generateDrizzleSchema } from "./generators/drizzle";
+export { generateKyselySchema } from "./generators/kysely";
+export { generatePrismaSchema } from "./generators/prisma";
+export type {
+	SchemaGenerator,
+	SchemaGeneratorResult,
+} from "./generators/types";
