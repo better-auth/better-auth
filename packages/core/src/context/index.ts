b5626209bea071f985b3c7c3e17867a9c156bde3
Alex Yang <himself65@outlook.com>
2026-01-02 16:58:34 +0800

    fix(core): dual module error

--- packages/core/src/context/index.ts
+++ packages/core/src/context/index.ts
@@ -19,3 +19,34 @@ export {
 	runWithAdapter,
 	runWithTransaction,
 } from "./transaction";
+
+const glo: any =
+	typeof globalThis !== "undefined"
+		? globalThis
+		: typeof window !== "undefined"
+			? window
+			: typeof global !== "undefined"
+				? global
+				: {};
+
+const importIdentifier = "__ $BETTER_AUTH$ __";
+
+if (glo[importIdentifier] === true) {
+	/**
+	 * Dear reader of this message. Please take this seriously.
+	 *
+	 * If you see this message, make sure that you only import one version of Better Auth. In many cases,
+	 * your package manager installs two versions of Better Auth that are used by different packages within your project.
+	 * Another reason for this message is that some parts of your project use the commonjs version of Better Auth
+	 * and others use the EcmaScript version of Better Auth.
+	 *
+	 * This often leads to issues that are hard to debug. We often need to ensure async local storage instance,
+	 * If you imported different versions of Better Auth, it is impossible for us to
+	 * do status synchronization per request anymore - which might break the states.
+	 *
+	 */
+	console.error(
+		"Better Auth was already imported. This breaks async local storage instance and will lead to issues!",
+	);
+}
+glo[importIdentifier] = true;
