8bed383279545db69dbd058159084cd0ef5966c4
ping-maxwell <maxwell.multinite@gmail.com>
2026-01-03 20:28:00 +1000

    fix: CI wip

--- packages/drizzle-adapter/test/adapter.drizzle.mysql.test.ts
+++ packages/drizzle-adapter/test/adapter.drizzle.mysql.test.ts
@@ -47,7 +47,7 @@ const { execute } = await testAdapter({
 			"mysql",
 		);
 
-		const command = `npx drizzle-kit@beta push --dialect=mysql --schema=${fileName}.ts --url=mysql://user:password@localhost:3306/${dbName}`;
+		const command = `npx drizzle-kit push --dialect=mysql --schema=${fileName}.ts --url=mysql://user:password@localhost:3306/${dbName}`;
 		console.log(`Running: ${command}`);
 		console.log(`Options:`, betterAuthOptions);
 		try {
