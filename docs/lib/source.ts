5b633d6157fde2a34f4c770312188cd3af8c14a8
Taesu <bytaesu@gmail.com>
2026-01-05 03:20:39 +0900

    chore: include fumadocs-mdx in tsconfig path

--- docs/lib/source.ts
+++ docs/lib/source.ts
@@ -1,4 +1,8 @@
-import { blogCollection, changelogCollection, docs } from "collections/server";
+import {
+	blogCollection,
+	changelogCollection,
+	docs,
+} from "fumadocs-mdx:collections/server";
 import { loader } from "fumadocs-core/source";
 import { toFumadocsSource } from "fumadocs-mdx/runtime/server";
 import { getPageTree } from "@/components/sidebar-content";
