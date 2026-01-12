7fa0571c6e54179b9665e777293778bba9dbaf1f
ping-maxwell <maxwell.multinite@gmail.com>
2026-01-13 02:52:16 +1000

    chore: code-split type definitions

---
+++ packages/better-auth/src/plugins/organization-new/types/infer-schemas.ts
@@ -0,0 +1,84 @@
+import type { Prettify } from "@better-auth/core";
+import type { InferAdditionalFieldsFromPluginOptions } from "../../../db";
+import type { InferOrganizationRolesFromOption } from "../access";
+import type { Organization } from "../schema";
+import type { ResolvedOrganizationOptions } from "./organization-options";
+
+export type InferMember<
+	O extends ResolvedOrganizationOptions,
+	isClientSide extends boolean = true,
+> = Prettify<
+	(O["use"][number] extends {
+		id: "teams";
+	}
+		? {
+				id: string;
+				organizationId: string;
+				role: InferOrganizationRolesFromOption<O>;
+				createdAt: Date;
+				userId: string;
+				teamId?: string | undefined;
+				user: {
+					id: string;
+					email: string;
+					name: string;
+					image?: string | undefined;
+				};
+			}
+		: {
+				id: string;
+				organizationId: string;
+				role: InferOrganizationRolesFromOption<O>;
+				createdAt: Date;
+				userId: string;
+				user: {
+					id: string;
+					email: string;
+					name: string;
+					image?: string | undefined;
+				};
+			}) &
+		InferAdditionalFieldsFromPluginOptions<"member", O, isClientSide>
+>;
+
+export type InferOrganization<
+	O extends ResolvedOrganizationOptions,
+	isClientSide extends boolean = true,
+> = Prettify<
+	Organization &
+		InferAdditionalFieldsFromPluginOptions<"organization", O, isClientSide>
+> &
+	(O["disableSlugs"] extends false ? { slug: string } : never);
+
+export type InferInvitation<
+	O extends ResolvedOrganizationOptions,
+	isClientSide extends boolean = true,
+> = Prettify<
+	(O["use"][number] extends {
+		id: "teams";
+	}
+		? {
+				id: string;
+				organizationId: string;
+				email: string;
+				role: InferOrganizationRolesFromOption<O>;
+				status: InvitationStatus;
+				inviterId: string;
+				expiresAt: Date;
+				createdAt: Date;
+				teamId?: string | undefined;
+			}
+		: {
+				id: string;
+				organizationId: string;
+				email: string;
+				role: InferOrganizationRolesFromOption<O>;
+				status: InvitationStatus;
+				inviterId: string;
+				expiresAt: Date;
+				createdAt: Date;
+			}) &
+		InferAdditionalFieldsFromPluginOptions<"invitation", O, isClientSide>
+>;
+
+export type InvitationStatus = "pending" | "accepted" | "rejected" | "canceled";
---
+++ packages/better-auth/src/plugins/organization-new/types/organization-addons.ts
@@ -0,0 +1,5 @@
+export interface OrganizationAddons {
+	id: string;
+	endpoints?: Record<string, any>;
+	schema?: Record<string, any>;
+}
