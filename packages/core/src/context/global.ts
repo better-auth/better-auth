251ed5fda626f727cdd87a27e8e57fa5b4020a5c
Alex Yang <himself65@outlook.com>
2026-01-13 09:26:13 -0800

    chore(core): use global for multiple instances of BetterAuth

---
+++ packages/core/src/context/global.ts
@@ -0,0 +1,43 @@
+import type { AsyncLocalStorage } from "@better-auth/core/async_hooks";
+
+interface BetterAuthGlobal {
+	/**
+	 * Used to track the number of BetterAuth instances in the same process.
+	 *
+	 * Debugging purposes only.
+	 */
+	epoch: number;
+	/**
+	 * Stores the AsyncLocalStorage instances for each context.
+	 */
+	context: Record<string, AsyncLocalStorage<unknown>>;
+}
+
+const symbol = Symbol.for("better-auth:global");
+let bind: BetterAuthGlobal | null = null;
+
+const context: Record<string, AsyncLocalStorage<unknown>> = {};
+
+/**
+ * We store context instance in the globalThis.
+ *
+ * The reason we do this is that some bundlers, web framework, or package managers might
+ * create multiple copies of BetterAuth in the same process intentionally or unintentionally.
+ *
+ * For example, yarn v1, Next.js, SSR, Vite...
+ */
+export function getBetterAuthGlobal(): BetterAuthGlobal {
+	if (!(globalThis as any)[symbol]) {
+		(globalThis as any)[symbol] = {
+			epoch: 1,
+			context,
+		};
+	} else {
+		// add counter to track
+		if (!bind) {
+			bind = (globalThis as any)[symbol] as BetterAuthGlobal;
+			bind.epoch++;
+		}
+	}
+	return (globalThis as any)[symbol] as BetterAuthGlobal;
+}
