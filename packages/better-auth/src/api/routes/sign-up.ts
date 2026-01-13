159413c8b98101102458ab970b90cf72b1b389c0
Alex Yang <himself65@outlook.com>
2026-01-13 14:41:06 -0800

    fix: missing

--- packages/better-auth/src/api/routes/sign-up.ts
+++ packages/better-auth/src/api/routes/sign-up.ts
@@ -12,7 +12,7 @@ import { isAPIError } from "../../utils/is-api-error";
 import { formCsrfMiddleware } from "../middlewares/origin-check";
 import { createEmailVerificationToken } from "./email-verification";
 
-const _signUpEmailBodySchema = z
+const signUpEmailBodySchema = z
 	.object({
 		name: z.string().nonempty(),
 		email: z.email(),
