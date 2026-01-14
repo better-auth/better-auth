7f3566ed46bd73a54a610f1510b3eb2dde1597c6
jslno <joelsolano@jsolano.de>
2026-01-14 06:33:50 +0100

    chore: fix linting

--- docs/lib/rss.ts
+++ docs/lib/rss.ts
@@ -2,7 +2,7 @@ import { Feed } from "feed";
 import { blogs } from "./source";
 
 export function getRSS() {
-  const baseUrl = "https://better-auth.com/";
+	const baseUrl = "https://better-auth.com/";
 	const feed = new Feed({
 		title: "Better Auth Blog",
 		description: "Latest updates, articles, and insights about Better Auth",
