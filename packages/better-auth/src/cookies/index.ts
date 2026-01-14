d4a6614165a77aa8a3b41815a16ef2029e0c2165
Tomas Sivicki <tomas.sivicki@gmail.com>
2026-01-15 00:49:32 +0100

    test: adds additional secure prefix tests and considers '.' priority

--- packages/better-auth/src/cookies/index.ts
+++ packages/better-auth/src/cookies/index.ts
@@ -383,8 +383,8 @@ export const getSessionCookie = (
 		parsedCookie.get(name) ||
 		parsedCookie.get(`${SECURE_COOKIE_PREFIX}${name}`);
 	return (
-		tryGet(`${prefix}-${cookieName}`) ||
 		tryGet(`${prefix}.${cookieName}`) ||
+		tryGet(`${prefix}-${cookieName}`) ||
 		null
 	);
 };
