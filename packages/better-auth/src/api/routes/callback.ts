c918ee658258a5622f794b3182dc154603ef0728
Taesu <166604494+bytaesu@users.noreply.github.com>
2026-01-15 01:09:54 +0900

    fix: correctly handle OAuth callback and Apple email field (#7181)

--- packages/better-auth/src/api/routes/callback.ts
+++ packages/better-auth/src/api/routes/callback.ts
@@ -68,7 +68,14 @@ export const callbackOAuth = createAuthEndpoint(
 			throw c.redirect(`${defaultErrorURL}?error=invalid_callback_request`);
 		}
 
-		const { code, error, state, error_description, device_id } = queryOrBody;
+		const {
+			code,
+			error,
+			state,
+			error_description,
+			device_id,
+			user: userData,
+		} = queryOrBody;
 
 		if (!state) {
 			c.context.logger.error("State not found", error);
@@ -131,10 +138,24 @@ export const callbackOAuth = createAuthEndpoint(
 			c.context.logger.error("", e);
 			throw redirectOnError("invalid_code");
 		}
+		const parsedUserData = userData
+			? safeJSONParse<{
+					name?: {
+						firstName?: string;
+						lastName?: string;
+					};
+					email?: string;
+				}>(userData)
+			: null;
+
 		const userInfo = await provider
 			.getUserInfo({
 				...tokens,
-				user: c.body?.user ? safeJSONParse<any>(c.body.user) : undefined,
+				/**
+				 * The user object from the provider
+				 * This is only available for some providers like Apple
+				 */
+				user: parsedUserData ?? undefined,
 			})
 			.then((res) => res?.user);
 
