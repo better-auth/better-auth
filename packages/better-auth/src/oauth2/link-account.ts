eb297c2b7f55743137bf528e572628f8b36c53ef
Oscar Cornish <35607967+OscarCornish@users.noreply.github.com>
2026-01-19 20:24:32 +0000

    refactor: return linked account in findOAuthUser (#7331)
    Co-authored-by: Maxwell <145994855+ping-maxwell@users.noreply.github.com>

--- packages/better-auth/src/oauth2/link-account.ts
+++ packages/better-auth/src/oauth2/link-account.ts
@@ -37,12 +37,14 @@ export async function handleOAuthUserInfo(
 	const isRegister = !user;
 
 	if (dbUser) {
-		const hasBeenLinked = dbUser.accounts.find(
-			(a) =>
-				a.providerId === account.providerId &&
-				a.accountId === account.accountId,
-		);
-		if (!hasBeenLinked) {
+		const linkedAccount =
+			dbUser.linkedAccount ??
+			dbUser.accounts.find(
+				(acc) =>
+					acc.providerId === account.providerId &&
+					acc.accountId === account.accountId,
+			);
+		if (!linkedAccount) {
 			const trustedProviders =
 				c.context.options.account?.accountLinking?.trustedProviders;
 			const isTrustedProvider =
@@ -111,14 +113,14 @@ export async function handleOAuthUserInfo(
 
 			if (c.context.options.account?.storeAccountCookie) {
 				await setAccountCookie(c, {
-					...hasBeenLinked,
+					...linkedAccount,
 					...freshTokens,
 				});
 			}
 
 			if (Object.keys(freshTokens).length > 0) {
 				await c.context.internalAdapter.updateAccount(
-					hasBeenLinked.id,
+					linkedAccount.id,
 					freshTokens,
 				);
 			}
