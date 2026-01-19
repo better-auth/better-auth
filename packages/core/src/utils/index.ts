ae0e280587cb810f53858e686cc0d69e4e129b2c
Alex Yang <himself65@outlook.com>
2026-01-19 15:30:38 -0800

    fix(rate-limit): support IPv6 address normalization and subnet (#7470)

--- packages/core/src/utils/index.ts
+++ packages/core/src/utils/index.ts
@@ -1,5 +1,7 @@
 export { deprecate } from "./deprecate";
 export { defineErrorCodes } from "./error-codes";
 export { generateId } from "./id";
+export { createRateLimitKey, isValidIP, normalizeIP } from "./ip";
 export { safeJSONParse } from "./json";
 export { capitalizeFirstLetter } from "./string";
+export { normalizePathname } from "./url";
