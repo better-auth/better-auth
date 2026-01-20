0131f7a45655e32ebb51ab1b62f9871fde57cf5c
ping-maxwell <maxwell.multinite@gmail.com>
2026-01-20 16:54:26 +1000

    Update schema.ts

--- packages/better-auth/src/plugins/organization-new/addons/dynamic-access-control/schema.ts
+++ packages/better-auth/src/plugins/organization-new/addons/dynamic-access-control/schema.ts
@@ -1,13 +1,12 @@
-// import { generateId } from "@better-auth/core/utils/id";
-// import * as z from "zod/v4";
+import { generateId } from "@better-auth/core/utils/id";
+import * as z from "zod/v4";
 
-// TODO: when needed uncomment
-// export const organizationRoleSchema = z.object({
-// 	id: z.string().default(generateId),
-// 	organizationId: z.string(),
-// 	role: z.string(),
-// 	permission: z.record(z.string(), z.array(z.string())),
-// 	createdAt: z.date().default(() => new Date()),
-// 	updatedAt: z.date().optional(),
-// });
-// export type OrganizationRole = z.infer<typeof organizationRoleSchema>;
+const organizationRoleSchema = z.object({
+	id: z.string().default(generateId),
+	organizationId: z.string(),
+	role: z.string(),
+	permission: z.record(z.string(), z.array(z.string())),
+	createdAt: z.date().default(() => new Date()),
+	updatedAt: z.date().optional(),
+});
+export type OrganizationRole = z.infer<typeof organizationRoleSchema>;
