3c7220f86b0b9be034c60358e8ec056319012dc4
Taesu <166604494+bytaesu@users.noreply.github.com>
2026-01-23 15:54:53 -0800

    docs: move errors content to reference section (#7581)

--- docs/components/sidebar-content.tsx
+++ docs/components/sidebar-content.tsx
@@ -14,6 +14,7 @@ import {
 	Phone,
 	ScanFace,
 	ShieldCheck,
+	TriangleAlertIcon,
 	UserCircle,
 	UserSquare2,
 	Users2,
@@ -33,6 +34,7 @@ interface Content {
 		group?: boolean;
 		separator?: boolean;
 		isNew?: boolean;
+		hasSubpages?: boolean;
 	}[];
 }
 
@@ -2514,6 +2516,12 @@ C0.7,239.6,62.1,0.5,62.2,0.4c0,0,54,13.8,119.9,30.8S302.1,62,302.2,62c0.2,0,0.2,
 						></path>
 					</svg>
 				),
+			},
+			{
+				title: "Errors",
+				href: "/docs/reference/errors",
+				icon: () => <TriangleAlertIcon className="w-4 h-4 text-current" />,
+				hasSubpages: true,
 			},
 			{
 				title: "Contributing",
