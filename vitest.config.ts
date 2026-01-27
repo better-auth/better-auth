f4b6043a7207eb460f27590a04a165c78fbe6f5d
Alex Yang <himself65@outlook.com>
2026-01-27 14:18:44 -0800

    chore: vitest coverage performance

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,10 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
 	test: {
 		projects: ["./packages/*", "./test", "./e2e/*"],
+		coverage: {
+			include: ["src/**/*"],
+			exclude: [],
+		}
 	},
 	ssr: {
 		resolve: {
