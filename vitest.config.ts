fee3aa6814594302d0222b1aeb138f04da74c2d3
Alex Yang <himself65@outlook.com>
2026-01-27 15:19:50 -0800

    ci: parallel test for ci.yml

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,8 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
 	test: {
 		projects: ["./packages/*", "./test", "./e2e/*"],
+		slowTestThreshold: 10_000,
+		passWithNoTests: true,
 	},
 	ssr: {
 		resolve: {
