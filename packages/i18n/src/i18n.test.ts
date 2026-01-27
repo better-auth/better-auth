fb4958b22f2009285e6b5c2931368a8c8f45b929
Alex Yang <himself65@outlook.com>
2026-01-27 12:18:51 -0800

    fix: remove unused

--- packages/i18n/src/i18n.test.ts
+++ packages/i18n/src/i18n.test.ts
@@ -245,7 +245,6 @@ describe("i18n plugin", async () => {
 					INVALID_EMAIL_OR_PASSWORD: "Email ou mot de passe invalide",
 				},
 				de: {
-					DEF: "",
 					USER_NOT_FOUND: "Benutzer nicht gefunden",
 					INVALID_EMAIL_OR_PASSWORD: "Ungültige E-Mail oder Passwort",
 				},
