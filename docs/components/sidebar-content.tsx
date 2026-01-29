eb82e1736141d265bbf4e518d134872d1387955a
ping-maxwell <maxwell.multinite@gmail.com>
2026-01-29 15:01:00 +1000

    docs: add old org docs back

--- docs/components/sidebar-content.tsx
+++ docs/components/sidebar-content.tsx
@@ -1721,9 +1721,14 @@ C0.7,239.6,62.1,0.5,62.2,0.4c0,0,54,13.8,119.9,30.8S302.1,62,302.2,62c0.2,0,0.2,
 			},
 			{
 				title: "Organization",
+				href: "/docs/plugins/organization-old",
+				icon: () => <Users2 className="w-4 h-4" />,
+			},
+			{
+				title: "Organization v2",
 				icon: () => <Users2 className="w-4 h-4" />,
 				href: "/docs/plugins/organization",
-				isUpdated: true,
+				isNew: true,
 				children: [
 					{
 						title: "APIs",
@@ -2622,7 +2627,7 @@ C0.7,239.6,62.1,0.5,62.2,0.4c0,0,54,13.8,119.9,30.8S302.1,62,302.2,62c0.2,0,0.2,
 				title: "Errors",
 				href: "/docs/reference/errors",
 				icon: () => <TriangleAlertIcon className="w-4 h-4 text-current" />,
-				hasSubpages: true,
+				group: true,
 			},
 			{
 				title: "Contributing",
