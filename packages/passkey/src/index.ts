2089f5da39d44fa72920325ac8313e37d0513b4e
Taesu <bytaesu@gmail.com>
2026-02-01 04:01:23 +0900

    refactor: move const to outer scope

--- packages/passkey/src/index.ts
+++ packages/passkey/src/index.ts
@@ -21,6 +21,8 @@ declare module "@better-auth/core" {
 	}
 }
 
+const MAX_AGE_IN_SECONDS = 60 * 5; // 5 minutes
+
 export const passkey = (options?: PasskeyOptions | undefined) => {
 	const opts = {
 		origin: null,
@@ -30,18 +32,17 @@ export const passkey = (options?: PasskeyOptions | undefined) => {
 			...options?.advanced,
 		},
 	};
-	const maxAgeInSeconds = 60 * 5; // 5 minutes
 
 	return {
 		id: "passkey",
 		endpoints: {
 			generatePasskeyRegistrationOptions: generatePasskeyRegistrationOptions(
 				opts,
-				{ maxAgeInSeconds },
+				{ maxAgeInSeconds: MAX_AGE_IN_SECONDS },
 			),
 			generatePasskeyAuthenticationOptions:
 				generatePasskeyAuthenticationOptions(opts, {
-					maxAgeInSeconds,
+					maxAgeInSeconds: MAX_AGE_IN_SECONDS,
 				}),
 			verifyPasskeyRegistration: verifyPasskeyRegistration(opts),
 			verifyPasskeyAuthentication: verifyPasskeyAuthentication(opts),
