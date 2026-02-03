a002b8f4c8694660056d6c4d76f04667f0e7135c
Charles Bennett <bleudebennett@proton.me>
2026-02-03 04:38:32 +0100

    fix(types): added account to session type, context & auth

--- packages/core/src/types/context.ts
+++ packages/core/src/types/context.ts
@@ -123,6 +123,7 @@ export interface InternalAdapter<
 	): Promise<Session>;
 
 	findSession(token: string): Promise<{
+		account: Account & Record<string, any>;
 		session: Session & Record<string, any>;
 		user: User & Record<string, any>;
 	} | null>;
@@ -296,15 +297,18 @@ export type AuthContext<Options extends BetterAuthOptions = BetterAuthOptions> =
 			 * by `setNewSession` method.
 			 */
 			newSession: {
+				account: Account & Record<string, any>;
 				session: Session & Record<string, any>;
 				user: User & Record<string, any>;
 			} | null;
 			session: {
+				account: Account & Record<string, any>;
 				session: Session & Record<string, any>;
 				user: User & Record<string, any>;
 			} | null;
 			setNewSession: (
 				session: {
+					account: Account & Record<string, any>;
 					session: Session & Record<string, any>;
 					user: User & Record<string, any>;
 				} | null,
