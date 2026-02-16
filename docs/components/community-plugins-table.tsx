76a9588861c2bb53743a77bb6312e24a9a8d53aa
Olivier Bazoud <olivier.bazoud@gmail.com>
2026-02-16 10:09:23 +0100

    docs: add better-email to community plugins
    * Add better-email to the community plugins table
    * It si a new Better Auth plugin that centralizes all email sending callbacks through a single ESP-agnostic provider and template renderer.

--- docs/components/community-plugins-table.tsx
+++ docs/components/community-plugins-table.tsx
@@ -323,6 +323,16 @@ export const communityPlugins: CommunityPlugin[] = [
 			avatar: "https://github.com/C-W-D-Harshit.png",
 		},
 	},
+	{
+		name: "@nuntly/better-email",
+		url: "https://github.com/nuntly/better-email",
+		description: "Better Auth emails made simple with Better Email.",
+		author: {
+			name: "obazoud",
+			github: "nuntly",
+			avatar: "https://github.com/obazoud.png",
+		},
+	},
 ];
 export function CommunityPluginsTable() {
 	const [sorting, setSorting] = useState<SortingState>([]);
