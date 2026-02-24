e3463c63b620116c812e990cac26474ddb7a905d
ping-maxwell <maxwell.multinite@gmail.com>
2026-02-24 15:25:49 -0800

    fix: Api-key plugin

--- packages/better-auth/src/plugins/organization/index.ts
+++ packages/better-auth/src/plugins/organization/index.ts
@@ -1,4 +1,5 @@
 export { getOrgAdapter } from "./adapter";
+export { hasPermission } from "./has-permission";
 export * from "./organization";
 export type * from "./schema";
 export type * from "./types";
