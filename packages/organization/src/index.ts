6d7b2d096f3667b21f5667f3c3e61d6bc48bf9cb
ping-maxwell <maxwell.multinite@gmail.com>
2026-02-24 12:18:45 -0800

    fix: adapter tests

--- packages/organization/src/index.ts
+++ packages/organization/src/index.ts
@@ -3,4 +3,5 @@ export * from "./addons";
 export * from "./helpers/error-codes";
 export * from "./helpers/get-org-adapter";
 export * from "./organization";
+export * from "./schema";
 export * from "./types";
