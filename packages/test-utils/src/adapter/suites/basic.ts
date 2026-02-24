a740c844747a9e8b57a7865084e9cf1a2b660a9c
ping-maxwell <maxwell.multinite@gmail.com>
2026-02-24 15:06:21 -0800

    fix: CI

--- packages/test-utils/src/adapter/suites/basic.ts
+++ packages/test-utils/src/adapter/suites/basic.ts
@@ -2559,9 +2559,7 @@ export const getNormalTestSuiteTests = (
 		"findOne - multiple joins should return result even when some joined tables have no matching rows":
 			async () => {
 				await modifyBetterAuthOptions(
-					{
-						plugins: [organization({ use: [teams()] })],
-					},
+					{ plugins: [organization({ use: [teams()] })] },
 					true,
 				);
 
