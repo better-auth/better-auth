6beb0a1360bd47e548070efa63428091bf0884e9
Taesu <166604494+bytaesu@users.noreply.github.com>
2026-02-27 13:32:49 +0900

    fix(cookies): export `getAccountCookie` from `better-auth/cookies` (#8181)

--- packages/better-auth/src/cookies/index.ts
+++ packages/better-auth/src/cookies/index.ts
@@ -581,4 +581,8 @@ export const getCookieCache = async <
 };
 
 export * from "./cookie-utils";
-export { createSessionStore, getChunkedCookie } from "./session-store";
+export {
+	createSessionStore,
+	getAccountCookie,
+	getChunkedCookie,
+} from "./session-store";
