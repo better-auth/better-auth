20d7edfb25ca74cfd9cca1a9fb8c942f2922fb8c
Alex Yang <himself65@outlook.com>
2026-03-01 16:48:39 +0900

    chore: improve cli bundler

--- packages/cli/tsdown.config.ts
+++ packages/cli/tsdown.config.ts
@@ -1,9 +1,14 @@
-import { defineConfig } from "tsdown";
+import { defineConfig } from 'tsdown'
 
-export default defineConfig({
-	dts: { build: true },
-	format: ["esm"],
-	entry: ["./src/index.ts", "./src/api.ts"],
-	external: ["better-auth", "better-call"],
-	sourcemap: true,
-});
+export default defineConfig([
+  {
+    dts: { build: true },
+    format: ['esm'],
+    entry: ['./src/api.ts'],
+    sourcemap: true
+  }, {
+    format: ['esm'],
+    entry: ['./src/index.ts'],
+    sourcemap: true
+  }
+])
