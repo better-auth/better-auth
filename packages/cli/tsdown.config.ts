f246cd0ca8cfc712db1ab3601a428e2a6c3aaabb
Alex Yang <himself65@outlook.com>
2026-03-01 16:50:38 +0900

    chore: lint fix

--- packages/cli/tsdown.config.ts
+++ packages/cli/tsdown.config.ts
@@ -1,14 +1,15 @@
-import { defineConfig } from 'tsdown'
+import { defineConfig } from "tsdown";
 
 export default defineConfig([
-  {
-    dts: { build: true },
-    format: ['esm'],
-    entry: ['./src/api.ts'],
-    sourcemap: true
-  }, {
-    format: ['esm'],
-    entry: ['./src/index.ts'],
-    sourcemap: true
-  }
-])
+	{
+		dts: { build: true },
+		format: ["esm"],
+		entry: ["./src/api.ts"],
+		sourcemap: true,
+	},
+	{
+		format: ["esm"],
+		entry: ["./src/index.ts"],
+		sourcemap: true,
+	},
+]);
