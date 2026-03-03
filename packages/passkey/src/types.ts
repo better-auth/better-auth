56a1ca15badf0e449305b1346d4c0967d81d7330
EugeneDraitsev <eugene.draitsev@yubico.com>
2026-03-03 12:51:52 +0100

    refactor(passkey): rename lastUsedAt to updatedAt

--- packages/passkey/src/types.ts
+++ packages/passkey/src/types.ts
@@ -70,6 +70,6 @@ export type Passkey = {
 	backedUp: boolean;
 	transports?: string | undefined;
 	createdAt: Date;
-	lastUsedAt?: Date | undefined;
+	updatedAt?: Date | undefined;
 	aaguid?: string | undefined;
 };
