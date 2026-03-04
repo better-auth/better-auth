14cb487ce9eaf7082a15a8a5be58c30f32c3a655
Joel Solano <joelsolano@jsolano.de>
2026-03-05 00:31:44 +0100

    chore: update test

--- packages/better-auth/src/plugins/oidc-provider/oidc.test.ts
+++ packages/better-auth/src/plugins/oidc-provider/oidc.test.ts
@@ -515,7 +515,7 @@ describe("oidc", async () => {
 			});
 
 			const location = response.headers.get("Location") || "";
-			expect(location).not.toContain("malicious.com");
+			expect(location === null || location === "").not.toContain("malicious.com");
 			expect([400, 302]).toContain(response.status);
 		});
 
