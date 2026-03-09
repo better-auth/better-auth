210baed9a3b64236f3ff7237653d087564a8f2a0
Ivan <cernja@protonmail.com>
2026-03-09 12:03:56 +0100

    docs: add new badge to Encore integration

--- landing/components/sidebar-content.tsx
+++ landing/components/sidebar-content.tsx
@@ -1502,6 +1502,7 @@ C0.7,239.6,62.1,0.5,62.2,0.4c0,0,54,13.8,119.9,30.8S302.1,62,302.2,62c0.2,0,0.2,
 				title: "Encore",
 				icon: Icons.encore,
 				href: "/docs/integrations/encore",
+				isNew: true,
 			},
 			{
 				title: "Express",
