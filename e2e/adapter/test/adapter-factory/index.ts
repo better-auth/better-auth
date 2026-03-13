53b8c45c4ffd7f6961c86a999556e8d2be8e5417
Jonathan Samines <jn.samines@gmail.com>
2026-03-13 09:51:27 -0600

    chore(e2e): fix lint issues

--- e2e/adapter/test/adapter-factory/index.ts
+++ e2e/adapter/test/adapter-factory/index.ts
@@ -1,10 +1,10 @@
 export {
 	authFlowTestSuite,
+	caseInsensitiveTestSuite,
 	getNormalTestSuiteTests,
 	joinsTestSuite,
 	normalTestSuite,
 	numberIdTestSuite,
 	transactionsTestSuite,
-	caseInsensitiveTestSuite,
 	uuidTestSuite,
 } from "@better-auth/test-utils/adapter";
