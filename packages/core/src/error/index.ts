b463d59d3cd19f39654e7a2debbce71f5ef564d6
stephen <98430908+sleepe229@users.noreply.github.com>
2026-03-22 23:46:49 +0300

    fix(core): declare inherited APIError properties to fix TypeScript type resolution

--- packages/core/src/error/index.ts
+++ packages/core/src/error/index.ts
@@ -11,7 +11,15 @@ export class BetterAuthError extends Error {
 
 export { type APIErrorCode, BASE_ERROR_CODES } from "./codes";
 
+type BaseAPIErrorInstance = InstanceType<typeof BaseAPIError>;
+
 export class APIError extends BaseAPIError {
+	declare status: BaseAPIErrorInstance["status"];
+	declare body: BaseAPIErrorInstance["body"];
+	declare headers: BaseAPIErrorInstance["headers"];
+	declare statusCode: BaseAPIErrorInstance["statusCode"];
+	declare message: string;
+
 	constructor(...args: ConstructorParameters<typeof BaseAPIError>) {
 		super(...args);
 	}
