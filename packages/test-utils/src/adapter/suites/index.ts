89c60a0979edc8eba4ec36ee9bec22785098f691
Jonathan Samines <jn.samines@gmail.com>
2026-03-23 12:07:56 -0600

    chore(test-utils): move case-insensitive suite to test-utils

--- packages/test-utils/src/adapter/suites/index.ts
+++ packages/test-utils/src/adapter/suites/index.ts
@@ -2,5 +2,6 @@ export * from "./auth-flow";
 export * from "./basic";
 export * from "./joins";
 export * from "./number-id";
+export * from "./case-insensitive";
 export * from "./transactions";
 export * from "./uuid";
