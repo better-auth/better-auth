dfa805c3eaca884fa02d7ebcca114ba3602a19d8
Manuel Lautenschlager <ml@webrender.de>
2026-07-31 10:30:42 +0200

    feat(oauth-provider): export verifyOAuthQueryParams

--- packages/oauth-provider/src/index.ts
+++ packages/oauth-provider/src/index.ts
@@ -28,6 +28,7 @@ export type {
 } from "./oauth-endpoint";
 export { createResourceServerChallenge } from "./resource-challenge";
 export { getOAuthProviderApi } from "./token";
+export { verifyOAuthQueryParams } from "./utils";
 export type * from "./types";
 export type { OAuthClient, ResourceServerMetadata } from "./types/oauth";
 export type { OAuthClientMetadata } from "./types/zod";
